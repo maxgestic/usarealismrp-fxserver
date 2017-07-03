58f66096c3d9189c70316b69cea1f885c46d53f3
Josh Yolles <josh@whaicehockey.com>
2017-07-03 14:12:21 -0700

    alt-chat and bank-robbery work

---
+++ resources/bank-robbery/bank-robbery_cl.lua
@@ -0,0 +1,74 @@
+--local markerX, markerY, markerZ = -106.358, 6474.25, 30.600
+local markerX, markerY, markerZ = 146.346, -1044.58, 28.2778
+
+function getPlayerDistanceFromShop(shopX,shopY,shopZ)
+	-- Get the player coords so that we know where to spawn it
+	local playerCoords = GetEntityCoords(GetPlayerPed(-1) --[[Ped]], false)
+	return GetDistanceBetweenCoords(playerCoords.x,playerCoords.y,playerCoords.z,shopX,shopY,shopZ,false)
+end
+
+RegisterNetEvent("bank:checkRange")
+AddEventHandler("bank:checkRange", function(source)
+
+	if getPlayerDistanceFromShop(markerX,markerY,markerZ) < 4 then
+		TriggerServerEvent("bank:inRange")
+	else
+		TriggerEvent("chatMessage", "SYSTEM", { 0, 141, 155 }, "^3Out of range of bank. No money stolen.")
+	end
+
+end)
+
+function robBank()
+	TriggerServerEvent("bank:isBusy")
+	Menu.hidden = true -- close menu
+end
+
+function bankMenu()
+	MenuTitle = "US Bank"
+	ClearMenu()
+	Menu.addButton("Rob Bank", "robBank")
+end
+
+local playerNotified = false
+
+Citizen.CreateThread(function()
+
+	while true do
+
+		Citizen.Wait(0)
+		DrawMarker(1, markerX, markerY, markerZ, 0, 0, 0, 0, 0, 0, 2.0, 2.0, 1.0, 240, 230, 140, 90, 0, 0, 2, 0, 0, 0, 0)
+		if getPlayerDistanceFromShop(markerX,markerY,markerZ) < 4 and not playerNotified then
+			TriggerEvent("chatMessage", "SYSTEM", { 0, 141, 155 }, "^3Press E then 'enter' to rob the bank.")
+			playerNotified = true
+		end
+		if IsControlJustPressed(1,Keys["E"]) then
+
+			if getPlayerDistanceFromShop(markerX,markerY,markerZ) < 4 then -- dmv shop
+
+				bankMenu()                -- Menu to draw
+				Menu.hidden = not Menu.hidden    -- Hide/Show the menu
+
+			end
+
+		elseif getPlayerDistanceFromShop(markerX,markerY,markerZ) > 4 then
+
+			playerNotified = false
+			Menu.hidden = true
+
+		end
+
+		Menu.renderGUI()     -- Draw menu on each tick if Menu.hidden = false
+
+	end
+end)
+
+RegisterNetEvent("bank-robbery:notify")
+AddEventHandler("bank-robbery:notify", function(msg)
+	DrawCoolLookingNotification(msg)
+end)
+
+function DrawCoolLookingNotification(msg)
+    SetNotificationTextEntry("STRING")
+    AddTextComponentString(msg)
+    DrawNotification(0,1)
+end
---
+++ resources/bank-robbery/bank-robbery_sv.lua
@@ -0,0 +1,124 @@
+local isBusy = "no"
+
+local closed = false
+
+function getPlayerIdentifierEasyMode(source)
+	local rawIdentifiers = GetPlayerIdentifiers(source)
+	if rawIdentifiers then
+		for key, value in pairs(rawIdentifiers) do
+			playerIdentifier = value
+		end
+    else
+		print("IDENTIFIERS DO NOT EXIST OR WERE NOT RETIREVED PROPERLY : " .. source)
+		return false
+	end
+	print("INSIDE getPlayerIdEasyMode about to retrun playerIdentifier = " .. playerIdentifier)
+	return playerIdentifier -- should usually be only 1 identifier according to the wiki
+end
+
+local abletorob = true
+RegisterServerEvent("bank:isBusy")
+AddEventHandler("bank:isBusy", function()
+
+	print("INSIDE isBusy with source = " .. source)
+	local busyStatus = isBusy
+
+    if not closed then
+        print("BANK WAS NOT CLOSED")
+        if not abletorob then
+            TriggerClientEvent('chatMessage', source, 'SYSTEM', { 0, 141, 155 }, "^3The bank has already been robbed!")
+        elseif busyStatus == "no" then
+
+            TriggerClientEvent('chatMessage', source, 'SYSTEM', { 0, 141, 155 }, "^3You are robbing the bank!")
+            TriggerClientEvent('chatMessage', source, 'SYSTEM', { 0, 141, 155 }, "^3Wait ^21 minute^3 to get all the money!")
+			TriggerClientEvent("bank-robbery:notify", source, "~r~Alarm activated!")
+
+            TriggerEvent("bank:beginRobbery", source)
+            abletorob = false
+
+            -- ~ 45 minute cooldown
+            SetTimeout(2700000 , function()
+                abletorob = true
+            end)
+
+        else
+			TriggerClientEvent("bank-robbery:notify", source, "Someone is already robbing the bank!")
+        end
+    else
+		TriggerClientEvent("bank-robbery:notify", source, "The bank has been ~r~closed~w~ by admins.")
+    end
+
+end)
+
+RegisterServerEvent("bank:beginRobbery")
+AddEventHandler("bank:beginRobbery", function(source)
+
+	print("INSIDE beginRobbery with source = " .. source)
+
+	-- make npc busy so only one at a time can rob bank
+    isBusy = "yes"
+
+	TriggerClientEvent('chatMessage', -1, 'NEWS', { 255, 180, 0 }, '^0Someone is robbing the bank!')
+
+	-- wait 45 seconds to 'get' money
+	SetTimeout(60000, function()
+
+		-- need to check player distance from bank point
+		-- if in range: give bag of loot
+		-- if not in rage: cancel robbery
+		TriggerClientEvent("bank:checkRange", source, source)
+		-- make npc not busy so it can be used again
+        isBusy = "no"
+
+	end)
+
+end)
+
+RegisterServerEvent("bank:inRange")
+AddEventHandler("bank:inRange", function()
+	rewardMoney = math.random(5000, 60000)
+	local msg = "You have been given ~g~$" .. comma_value(rewardMoney) .. "~w~ in dirty money!"
+	TriggerClientEvent("bank-robbery:notify", source, msg)
+	TriggerEvent('es:getPlayerFromId', source, function(user)
+		if user then
+			user.addMoney(rewardMoney)
+	        isBusy = "no"
+		end
+	end)
+end)
+
+RegisterServerEvent("bank:outOfRange")
+AddEventHandler("bank:outOfRange", function()
+	TriggerClientEvent("bank-robbery:notify", source, "~y~Out of range! No money was taken.")
+    isBusy = "no"
+    abletorob = true
+end)
+
+TriggerEvent('es:addCommand', 'closebank', function(source, args, user)
+
+    if tonumber(user.permission_level) > 0 then
+        closed = true
+		TriggerClientEvent("bank-robbery:notify", source, "BANK IS NOW ~r~CLOSED")
+    end
+
+end)
+
+TriggerEvent('es:addCommand', 'openbank', function(source, args, user)
+
+    if tonumber(user.permission_level) > 0 then
+        closed = false
+        TriggerClientEvent("bank-robbery:notify", source, "BANK IS NOW ~g~CLOSED")
+    end
+
+end)
+
+function comma_value(amount)
+  local formatted = amount
+  while true do
+    formatted, k = string.gsub(formatted, "^(-?%d+)(%d%d%d)", '%1,%2')
+    if (k==0) then
+      break
+    end
+  end
+  return formatted
+end
