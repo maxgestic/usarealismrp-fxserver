7f4fd93e08a5bfb9e27748ebf67fdc080bbe4333
Josh Yolles <josh@whaicehockey.com>
2017-07-07 13:55:39 -0700

    C:/Program Files/Git/impound should work okay

--- resources/deleteveh/server.lua
+++ resources/deleteveh/server.lua
@@ -1,19 +1,13 @@
 TriggerEvent('es:addCommand','impound', function(source, args, user)
-        local playerJob
-            local userSource = source
-            local idents = GetPlayerIdentifiers(userSource)
-            TriggerEvent('es:exposeDBFunctions', function(usersTable)
-                usersTable.getDocumentByRow("essentialmode", "identifier", idents[1], function(result)
-                    docid = result._id
-                    playerJob = result.job
-                    if playerJob == "sheriff" or playerJob == "ems" or playerJob == "fire" or playerJob == "owner" or playerJob == "admin" or playerJob == "mod" then
-                        -- do stuff here
-                        TriggerClientEvent( 'impoundVehicle', source )
-                    else
-                        TriggerClientEvent("impound:notify", source, "Only ~y~law enforcement~w~,~y~medics~w~, and ~y~admins~w~ can use /impound!")
-                    end
-                end)
-            end)
+    local userSource = source
+    TriggerEvent('es:getPlayerFromId', userSource, function(user)
+        local playerJob = user.getJob()
+        if playerJob == "sheriff" or playerJob == "ems" or playerJob == "fire" or playerJob == "owner" or playerJob == "admin" or playerJob == "mod" then
+            TriggerClientEvent( 'impoundVehicle', source )
+        else
+            TriggerClientEvent("impound:notify", source, "Only ~y~law enforcement~w~,~y~medics~w~, and ~y~admins~w~ can use /impound!")
+        end
+    end)
 end)
 
 
@@ -21,50 +15,35 @@ RegisterServerEvent("impound:impoundVehicle")
 AddEventHandler("impound:impoundVehicle", function(vehicle, plate)
     print("inside of impound:impoundVehicle!")
     local userSource = source
-    local idents = GetPlayerIdentifiers(userSource)
-    TriggerEvent('es:exposeDBFunctions', function(usersTable)
-        usersTable.getDocumentByRow("essentialmode", "identifier", idents[1], function(result)
-            print("inside callback for getDocumentByRow")
-            print("typeof result = " .. type(result))
-            docid = result._id
-            if docid then
-                print("docid = " .. docid)
-            else
-                print("docid did not exist!")
-            end
-            playerJob = result.job
-            if playerJob == "sheriff" or playerJob == "ems" or playerJob == "fire" or playerJob == "owner" or playerJob == "admin" or playerJob == "mod" then
-                print("user's job was sheriff.. calling userTable.getContentsOfView now...")
-                -- basically: go through the vehicle properties on each player's document and see if the plate matches the passed in plate from the client
-                -- if they match, then set the impounded property to true on that specific player's vehicle property of their document
-                usersTable.getContentsOfView('essentialmode', "test", "test-view", function(rows)
-                    for i = 1, #rows do
-                        local result = rows[i]
-                        if result.value then
-                            local playerVehicle = result.value
-                            if playerVehicle.plate == plate then
-                                print("plates matched! setting player vehicle impound status!")
-                                -- TODO: TEST THIS CODE. LOL....
-                                playerVehicle.impounded = true
-                                usersTable.updateDocument("essentialmode", docid ,{vehicle = playerVehicle},function() end)
+    TriggerEvent('es:getPlayerFromId', userSource, function(user)
+        local playerJob = user.getJob()
+        if playerJob == "sheriff" or playerJob == "ems" or playerJob == "fire" or playerJob == "owner" or playerJob == "admin" or playerJob == "mod" then
+            TriggerEvent('es:getPlayers', function(players)
+                    for x = 1, #players do
+                        local player = players[i]
+                        local vehicles = player.getVehicles()
+                        for i = 1, #vehicles do
+                            if vehicles[i].plate == plate then
+                                vehicles[i].impounded = true
+                                print("just impounded " .. GetPlayerName(userSource) .. "'s vehicle!'")
+                                TriggerClientEvent( 'impoundVehicle', userSource )
+                                return
                             end
                         end
                     end
-                end)
-                TriggerClientEvent( 'impoundVehicle', source )
-            else
-                TriggerClientEvent("impound:notify", source, "Only ~y~law enforcement~w~,~y~medics~w~, and ~y~admins~w~ can use /impound!")
-            end
-        end)
-    end)
-    --[[
-    if key == "plate" then
-        if string.match(plate,val) ~= nil then
-            print("found a matching plate to be impounded")
-            v.impounded = true
-            print("v.impounded = " .. tostring(v.impounded))
-            return
+            end)
+        else
+            TriggerClientEvent("impound:notify", userSource, "Only ~y~law enforcement~w~,~y~medics~w~, and ~y~admins~w~ can use /impound!")
         end
-    end
-    ]]
+    end)
 end)
+--[[
+if key == "plate" then
+if string.match(plate,val) ~= nil then
+print("found a matching plate to be impounded")
+v.impounded = true
+print("v.impounded = " .. tostring(v.impounded))
+return
+end
+end
+]]
