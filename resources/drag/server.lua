52297bff1f5992711eb36d0abac630992453e7de
Josh Yolles <josh@whaicehockey.com>
2017-07-08 22:48:45 -0700

    drag now only for cops/ems

--- resources/drag/server.lua
+++ resources/drag/server.lua
@@ -1,21 +1,25 @@
 TriggerEvent('es:addCommand', 'drag', function(source, args, user)
-
-	local argument = args[2] -- player id to check license
-
-	if argument == nil or type(tonumber(argument)) == nil then
-	
-		TriggerClientEvent("chatMessage", source, "SYSTEM", { 0, 141, 155 }, "example: /drag <id>")
-		
-	else 
-			
-		if(GetPlayerName(tonumber(argument)))then
-			if argument == source then
-				TriggerClientEvent('chatMessage', source, "SYSTEM", { 0, 141, 155 }, "You cannot drag yourself!")
+	if user then
+		if user.getJob() == "sheriff" or user.getJob() == "ems" or user.getJob() == "fire" then
+			local argument = args[2] -- player id to check license
+			if argument == nil or type(tonumber(argument)) == nil then
+				TriggerClientEvent("chatMessage", source, "SYSTEM", { 0, 141, 155 }, "example: /drag <id>")
 			else
-				TriggerClientEvent("dr:drag", tonumber(argument), source)
+				if(GetPlayerName(tonumber(argument)))then
+					if argument == source then
+						TriggerClientEvent('chatMessage', source, "SYSTEM", { 0, 141, 155 }, "You cannot drag yourself!")
+					else
+						TriggerClientEvent("dr:drag", tonumber(argument), source)
+					end
+				else
+					TriggerClientEvent('chatMessage', source, "SYSTEM", { 0, 141, 155 }, "Incorrect Player ID")
+				end
 			end
 		else
-			TriggerClientEvent('chatMessage', source, "SYSTEM", { 0, 141, 155 }, "Incorrect Player ID")
-		end		
+			print("wrong job for drag")
+			-- wrong job
+		end
+		--print("user nil for drag")
+		-- user did not exist
 	end
 end)
