1eb4984f5f035de7586a3ae9cf4a2a81a606d16d
Josh Yolles <josh@whaicehockey.com>
2017-08-13 08:55:17 -0700

    Harder to steal NPC vehicles

--- resources/lock-doors/client.lua
+++ resources/lock-doors/client.lua
@@ -1,3 +1,4 @@
+--[[
 -- Shows a notification on the player's screen
 function ShowNotification( text )
     SetNotificationTextEntry( "STRING" )
@@ -56,5 +57,28 @@ Citizen.CreateThread(function()
                 end
             end
         end
+    end
+end)
+
+--]]
+-- test
+
+Citizen.CreateThread(function()
+    while true do
+        if DoesEntityExist(GetVehiclePedIsTryingToEnter(PlayerPedId())) then
+            local veh = GetVehiclePedIsTryingToEnter(PlayerPedId())
+            local lock = GetVehicleDoorLockStatus(veh)
+
+            if lock == 7 then
+                SetVehicleDoorsLocked(veh, 2)
+            end
+
+            local pedd = GetPedInVehicleSeat(veh, -1)
+
+            if pedd then
+                SetPedCanBeDraggedOut(pedd, false)
+            end
+        end
+        Citizen.Wait(0)
     end
 end)
