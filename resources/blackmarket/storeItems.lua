9f99af3c3b8ce2d760e9c8b0c55d71c533fb7cff
Josh Yolles <josh@whaicehockey.com>
2017-08-14 10:58:07 -0700

    More blackmarket weapons

--- resources/blackmarket/storeItems.lua
+++ resources/blackmarket/storeItems.lua
@@ -1,9 +1,14 @@
 storeItems = {
     ["weapons"] = {
-        { name = "Molotov", type = "weapon", hash = 615608432, price = 250, legality = "illegal", quantity = 1 },
+        { name = "Molotov", type = "weapon", hash = 615608432, price = 650, legality = "illegal", quantity = 1 },
+        { name = "Brass Knuckles", type = "weapon", hash = -656458692, price = 650, legality = "illegal", quantity = 1 },
+        { name = "Dagger", type = "weapon", hash = -1834847097, price = 750, legality = "illegal", quantity = 1 },
+        { name = "Switchblade", type = "weapon", hash = -538741184, price = 1000, legality = "illegal", quantity = 1 },
+        { name = "Hatchet", type = "weapon", hash = -102973651, price = 1000, legality = "illegal", quantity = 1 },
         { name = "AP Pistol", type = "weapon", hash = 0x22D8FE39, price = 6550, legality = "illegal", quantity = 1 },
         { name = "Sawn-off", type = "weapon", hash = 0x7846A318, price = 8000, legality = "illegal", quantity = 1 },
         { name = "Micro SMG", type = "weapon", hash = 324215364, price = 9700, legality = "illegal", quantity = 1 },
+        { name = "SMG", type = "weapon", hash = 736523883, price = 13700, legality = "illegal", quantity = 1 },
         { name = "Machine Pistol", type = "weapon", hash = -619010992, price = 9500, legality = "illegal", quantity = 1 },
         { name = "Tommy Gun", type = "weapon", hash = 1627465347, price = 9750, legality = "illegal", quantity = 1 },
         { name = "AK47", type = "weapon", hash = -1074790547, price = 20500, legality = "illegal", quantity = 1 },
