f18083db2b3ef7397391625932b4d2e7ecfc8786
Josh Yolles <josh@whaicehockey.com>
2017-08-29 14:32:08 -0700

    Any police can be k9

--- resources/police-k9/server.lua
+++ resources/police-k9/server.lua
@@ -1,7 +1,6 @@
 TriggerEvent('es:addCommand', 'k9', function(source, args, user)
-	-- tim
-	-- agome
-	if GetPlayerIdentifiers(source)[1] == "steam:110000105e9e6f1" or GetPlayerIdentifiers(source)[1] == "steam:11000010f138f99" then
+	-- m. lentz = steam:110000106d47d14
+	if user.getJob() == "sheriff" or user.getJob() == "police" then
 		TriggerClientEvent("k9:transform", source)
 	else
 		TriggerClientEvent("chatMessage", source, "K9 Keeper", {255, 50, 50}, "You're not authorized to have access to the K9's");
