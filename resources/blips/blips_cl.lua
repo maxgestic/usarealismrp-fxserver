548d75ecb22972a4ea200526b3dcf1b296433486
Josh Yolles <josh@whaicehockey.com>
2017-09-12 15:57:33 -0700

    Changed color of bar blip

--- resources/blips/blips_cl.lua
+++ resources/blips/blips_cl.lua
@@ -47,7 +47,7 @@ local blips = {
 	{ title="FBI", colour = 47, id = 88, x=140.577, y=-768.375, z=45.752 },
 	{ title="Airport", colour = 30, id = 307, x=-1329.32, y=-3045.92, z=13.944 },
 	{ title="Tequilala", colour = 83, id = 93, x=-564.778, y=274.195, z=83.0197 },
-	{ title="Yellow Jack", colour = 83, id = 93, x = 1986.1, y = 3050.57, z = 47.2151 },
+	{ title="Yellow Jack", colour = 46, id = 93, x = 1986.1, y = 3050.57, z = 47.2151 },
 	{ title="Downtown Taxi", colour = 60, id = 198, x=895.563, y=-179.536, z=74.7003 },
 	{ title="Bank", colour = 24, id = 108, x=254.565, y=225.495, z=100.176 },
 	{ title="Seaview Aircraft", colour = 60, id = 251, x=-943.103, y=-2958.14, z=13.9451 }
