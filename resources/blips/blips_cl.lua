62afe0c498bd70b189fb599209fcd9e86da49702
Josh Yolles <josh@whaicehockey.com>
2017-09-16 07:27:07 -0700

    Added 24/7 store marker in sandy shores

--- resources/blips/blips_cl.lua
+++ resources/blips/blips_cl.lua
@@ -34,6 +34,7 @@ local blips = {
 	{ title="General Store", colour = 24 , id = 52, x=1135.6, y=-981.327, z=46.5269	 },
 	{ title="General Store", colour = 24 , id = 52, x=-1223.69, y=-907.691, z=12.3264 },
 	{ title="General Store", colour = 24 , id = 52, x=25.746, y=-1345.59, z=29.497 },
+	{ title="General Store", colour = 24 , id = 52, x=1960.4197998047, y = 3742.9755859375, z = 31.343738555908 },
 	{ title="Police Department", colour = 38 , id = 60, x=451.255, y=-992.41, z=30.6896 }, -- LOS SANTOS
 	{ title="Sheriff's Office", colour = 38 , id = 60, x=1853.2, y=3687.74, z=34.267 }, -- SANDY
 	{ title="Sheriff's Office", colour = 38 , id = 60, x=-447.041, y=6012.97, z=31.7164 }, -- PALETO
