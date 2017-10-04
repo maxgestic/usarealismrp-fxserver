be9b09dbd7caa8af3792c2d8f12779c70d3ee85e
Josh Yolles <josh@whaicehockey.com>
2017-10-03 21:36:35 -0700

    bugfix for 30 minute data save

--- resources/essentialmode/server/main.lua
+++ resources/essentialmode/server/main.lua
@@ -227,7 +227,7 @@ Citizen.CreateThread(function()
 					for id, player in pairs(players) do
 						if player then
 							print("player existed")
-							db.updateUser(player.get('identifier'), {money = player.getMoney(), bank = player.getBank(), model = player.getModel(), inventory = player.getInventory(), weapons = player.getWeapons(), vehicles = player.getVehicles(), insurance = player.getInsurance(), job = player.getJob(), licenses = player.getLicenses(), criminalHistory = player.getCriminalHistory(), characters = player.getCharacters(), jailtime = player.getJailtime(), policeRank = player.getPoliceRank(), policeCharacter = player.getPoliceCharacter(), EMSRank = player.getEMSRank(), securityRank = Users[numberSource].getSecurityRank()}, function()
+							db.updateUser(player.get('identifier'), {money = player.getMoney(), bank = player.getBank(), model = player.getModel(), inventory = player.getInventory(), weapons = player.getWeapons(), vehicles = player.getVehicles(), insurance = player.getInsurance(), job = player.getJob(), licenses = player.getLicenses(), criminalHistory = player.getCriminalHistory(), characters = player.getCharacters(), jailtime = player.getJailtime(), policeRank = player.getPoliceRank(), policeCharacter = player.getPoliceCharacter(), EMSRank = player.getEMSRank(), securityRank = player.getSecurityRank()}, function()
 								print("saved player #" .. id .. "'s data!'")
 							end)
 						end
