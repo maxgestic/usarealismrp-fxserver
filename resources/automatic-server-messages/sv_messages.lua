97f1d7953c10540c50fb5fce00e4b8e1f04f89ad
Josh Yolles <josh@whaicehockey.com>
2017-10-05 15:46:27 -0700

    bugfix for settimeout timer on auto server messages

--- resources/automatic-server-messages/sv_messages.lua
+++ resources/automatic-server-messages/sv_messages.lua
@@ -1,8 +1,9 @@
 -- 12 hours = 43200 seconds
 local restartTimeHours = 12
 local restartTimeSeconds = 12 * 60 * 60
+local restartTimeMilliseconds = restartTimeSeconds * 1000
 function sendRestartNotification()
-    SetTimeout(restartTimeSeconds-90000, function()
+    SetTimeout(restartTimeMilliseconds-90000, function()
         print("****sending 1 minute 30 second warning...****")
         TriggerClientEvent("chatMessage", -1, "", {0, 0, 0}, "SERVER RESTARTING IN 1 MINUTE!!!")
         TriggerClientEvent("chatMessage", -1, "", {0, 0, 0}, "SERVER RESTARTING IN 1 MINUTE!!!")
@@ -10,7 +11,7 @@ function sendRestartNotification()
         TriggerClientEvent("chatMessage", -1, "", {0, 0, 0}, "SERVER RESTARTING IN 1 MINUTE!!!")
         TriggerClientEvent("chatMessage", -1, "", {0, 0, 0}, "SERVER RESTARTING IN 1 MINUTE!!!")
     end)
-    SetTimeout(restartTimeSeconds-30000, function()
+    SetTimeout(restartTimeMilliseconds-30000, function()
         print("****sending 30 second warning...****")
         TriggerClientEvent("chatMessage", -1, "", {0, 0, 0}, "^1SERVER RESTARTING IN 30 SECONDS! ^3DISCONNECT OR RISK DATA LOSS!!!")
         TriggerClientEvent("chatMessage", -1, "", {0, 0, 0}, "^1SERVER RESTARTING IN 30 SECONDS! ^3DISCONNECT OR RISK DATA LOSS!!!")
