de8fda29384b5149709e302e99a31ea0523f2aac
Josh Yolles <josh@whaicehockey.com>
2017-10-07 19:41:08 -0700

    Added debug message for db.retrieve user

--- resources/essentialmode/server/db.lua
+++ resources/essentialmode/server/db.lua
@@ -127,7 +127,7 @@ local function updateDocument(docID, updates, callback)
 					update[i] = updates[i]
 				end
 			end
-			
+
 			for i in pairs(updates)do
 				if update[i] == nil then
 					--update[i] = updates[i]
@@ -193,6 +193,8 @@ end
 function db.retrieveUser(identifier, callback)
 	if identifier ~= nil and type(identifier) == "string" then
 		requestDB('POST', 'essentialmode/_find', {selector = {["identifier"] = identifier}}, {["Content-Type"] = 'application/json'}, function(err, rText, headers)
+			if err ~= 200 or err ~= "200" then print("error inside of db.retrieveUser: " .. err) end
+			if not rText then print("rText value was nil inside of db.retrieveUser") return end
 			local doc =  json.decode(rText).docs[1]
 			if callback then
 				if doc then callback(doc) else callback(false) end
