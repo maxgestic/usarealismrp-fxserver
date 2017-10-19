f99ddc61a70ee6e7b5f7dbf52396d4f20e3a589e
Josh Yolles <josh@whaicehockey.com>
2017-10-18 22:05:22 -0700

    Add server messages to separate thread

--- resources/automatic-server-messages/sv_messages.lua
+++ resources/automatic-server-messages/sv_messages.lua
@@ -3,20 +3,19 @@ local restartTimeHours = 12
 local restartTimeSeconds = 12 * 60 * 60
 local restartTimeMilliseconds = restartTimeSeconds * 1000
 function sendRestartNotification()
-    SetTimeout(restartTimeMilliseconds-90000, function()
-        print("****sending 1 minute 30 second warning...****")
-        TriggerClientEvent("chatMessage", -1, "", {0, 0, 0}, "SERVER RESTARTING IN 1 MINUTE!!!")
-        TriggerClientEvent("chatMessage", -1, "", {0, 0, 0}, "SERVER RESTARTING IN 1 MINUTE!!!")
-        TriggerClientEvent("chatMessage", -1, "", {0, 0, 0}, "SERVER RESTARTING IN 1 MINUTE!!!")
-        TriggerClientEvent("chatMessage", -1, "", {0, 0, 0}, "SERVER RESTARTING IN 1 MINUTE!!!")
-        TriggerClientEvent("chatMessage", -1, "", {0, 0, 0}, "SERVER RESTARTING IN 1 MINUTE!!!")
-    end)
-    SetTimeout(restartTimeMilliseconds-30000, function()
-        print("****sending 30 second warning...****")
-        TriggerClientEvent("chatMessage", -1, "", {0, 0, 0}, "^1SERVER RESTARTING IN 30 SECONDS! ^3DISCONNECT OR RISK DATA LOSS!!!")
-        TriggerClientEvent("chatMessage", -1, "", {0, 0, 0}, "^1SERVER RESTARTING IN 30 SECONDS! ^3DISCONNECT OR RISK DATA LOSS!!!")
-        TriggerClientEvent("chatMessage", -1, "", {0, 0, 0}, "^1SERVER RESTARTING IN 30 SECONDS! ^3DISCONNECT OR RISK DATA LOSS!!!")
-        TriggerClientEvent("chatMessage", -1, "", {0, 0, 0}, "^1SERVER RESTARTING IN 30 SECONDS! ^3DISCONNECT OR RISK DATA LOSS!!!")
+    Citizen.CreateThreadNow(function()
+        SetTimeout(restartTimeMilliseconds-90000, function()
+            print("****sending 1 minute 30 second warning...****")
+            for i = 1, 8 do
+                TriggerClientEvent("chatMessage", -1, "", {0, 0, 0}, "SERVER RESTARTING IN 1 MINUTE!!!")
+            end
+        end)
+        SetTimeout(restartTimeMilliseconds-30000, function()
+            print("****sending 30 second warning...****")
+            for i = 1, 8 do
+                TriggerClientEvent("chatMessage", -1, "", {0, 0, 0}, "^1SERVER RESTARTING IN 30 SECONDS! ^3DISCONNECT OR RISK DATA LOSS!!!")
+            end
+        end)
     end)
 end
 
