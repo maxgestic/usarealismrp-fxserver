35921465acab21d6a1efbc70a6029dd635f0269a
Josh Yolles <josh@whaicehockey.com>
2017-11-16 16:38:56 -0800

    Stop shuffling from passenger seat

--- resources/usa_rp/client.lua
+++ resources/usa_rp/client.lua
@@ -344,3 +344,18 @@ Citizen.CreateThread(function()
         end
     end
 end)
+
+-- stop seat shuffling in vehicles
+local antiShuffleEnabled = true
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		if IsPedInAnyVehicle(GetPlayerPed(-1), false) and antiShuffleEnabled then
+			if GetPedInVehicleSeat(GetVehiclePedIsIn(GetPlayerPed(-1), false), 0) == GetPlayerPed(-1) then
+				if GetIsTaskActive(GetPlayerPed(-1), 165) then
+					SetPedIntoVehicle(GetPlayerPed(-1), GetVehiclePedIsIn(GetPlayerPed(-1), false), 0)
+				end
+			end
+		end
+	end
+end)
