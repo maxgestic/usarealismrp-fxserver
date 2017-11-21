bd097c88866821f877f19852d9585943d64a051c
Josh Yolles <josh@whaicehockey.com>
2017-11-21 14:11:54 -0800

    Can only open police radar when in police car

--- resources/wk_wrs/cl_radar.lua
+++ resources/wk_wrs/cl_radar.lua
@@ -435,9 +435,11 @@ end )
 
 Citizen.CreateThread( function()
     while true do
-        -- LCtrl is pressed and K has just been pressed
+        -- K has just been pressed
         if (IsControlJustPressed( 1, 311 ) and GetLastInputMethod(2)) then
-            TriggerEvent( 'wk:radarRC' )
+            if IsPedInAnyPoliceVehicle(GetPlayerPed(-1)) then
+                TriggerEvent( 'wk:radarRC' )
+            end
         end
 
         -- LCtrl is not being pressed and M has just been pressed
