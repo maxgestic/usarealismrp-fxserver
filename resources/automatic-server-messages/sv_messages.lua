34e12462123dd23f3a293555912f98741bc5ec78
Josh Yolles <josh@whaicehockey.com>
2017-12-05 20:03:27 -0800

    Send restart message earlier

--- resources/automatic-server-messages/sv_messages.lua
+++ resources/automatic-server-messages/sv_messages.lua
@@ -4,16 +4,16 @@ local restartTimeSeconds = 12 * 60 * 60
 local restartTimeMilliseconds = restartTimeSeconds * 1000
 function sendRestartNotification()
     Citizen.CreateThreadNow(function()
-        SetTimeout(restartTimeMilliseconds-90000, function()
+        SetTimeout(restartTimeMilliseconds-120000, function()
             print("****sending 1 minute 30 second warning...****")
             for i = 1, 8 do
-                TriggerClientEvent("chatMessage", -1, "", {0, 0, 0}, "SERVER RESTARTING IN 1 MINUTE!!!")
+                TriggerClientEvent("chatMessage", -1, "", {0, 0, 0}, "SERVER RESTARTING IN 2 MINUTES!!!")
             end
         end)
-        SetTimeout(restartTimeMilliseconds-30000, function()
+        SetTimeout(restartTimeMilliseconds-60000, function()
             print("****sending 30 second warning...****")
             for i = 1, 8 do
-                TriggerClientEvent("chatMessage", -1, "", {0, 0, 0}, "^1SERVER RESTARTING IN 30 SECONDS! ^3DISCONNECT OR RISK DATA LOSS!!!")
+                TriggerClientEvent("chatMessage", -1, "", {0, 0, 0}, "^1SERVER RESTARTING IN 60 SECONDS! ^3DISCONNECT OR RISK DATA LOSS!!!")
             end
         end)
     end)
