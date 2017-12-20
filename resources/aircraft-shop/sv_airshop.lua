a6b6c01e6b3fd36c29428e99b0cba3e41fea2346
Joshua G. Yolles <josh@whaicehockey.com>
2017-12-20 12:57:26 -0800

    Moved some prices to server

--- resources/aircraft-shop/sv_airshop.lua
+++ resources/aircraft-shop/sv_airshop.lua
@@ -1,36 +1,66 @@
+-- TO PREVENT MEMORY EDIT CHEATERS
+local MAX_AIRCRAFT_RETURN_AMOUNT = .25 * 250000
+local MIN_AIRCRAFT_PRICE = 15000
+
+local prices = {
+    ["Frogger"] = 35000,
+    ["Supervolito"] = 45000,
+    ["Swift"] = 55000,
+    ["Swift 2"] = 65000,
+    ["Volatus"] = 75000,
+    ["Cuban 800"] = 20000,
+    ["Dodo"] = 21000,
+    ["Duster"] = 15000,
+    ["Mammatus"] = 30000,
+    ["Stunt"] = 45000,
+    ["Velum"] = 55000,
+    ["Vestra"] = 60000,
+    ["Nimbus"] = 75000,
+    ["Shamal"] = 75000,
+    ["Luxor"] = 200000,
+    ["Luxor 2"] = 250000
+}
+
 RegisterServerEvent("airshop:rentVehicle")
 AddEventHandler("airshop:rentVehicle", function(vehicle)
-    print("vehicle.name = " .. vehicle.name)
-    print("vehicle.price = " .. vehicle.price)
-    print("vehicle.hash = " .. vehicle.hash)
-    local price = vehicle.price
-    local userSource = tonumber(source)
+  print("vehicle.name = " .. vehicle.name)
+  print("vehicle.price = " .. vehicle.price)
+  print("vehicle.hash = " .. vehicle.hash)
+  local price = prices[vehicle.name]
+  local userSource = tonumber(source)
+  if price >= MIN_AIRCRAFT_PRICE then
     TriggerEvent("es:getPlayerFromId", userSource, function(user)
-        print("inside of get player from id")
-        if user then
-            local user_money = user.getActiveCharacterData("money")
-            print("user existed")
-            if user_money >= price then
-                local new_money = user_money - price
-                user.setActiveCharacterData("money", new_money)
-                print("calling spawnAircraft")
-                TriggerClientEvent("airshop:spawnAircraft", userSource, vehicle.hash)
-            else
-                print("player did not have enough money")
-            end
+      print("inside of get player from id")
+      if user then
+        local user_money = user.getActiveCharacterData("money")
+        print("user existed")
+        if user_money >= price then
+          local new_money = user_money - price
+          user.setActiveCharacterData("money", new_money)
+          print("calling spawnAircraft")
+          TriggerClientEvent("airshop:spawnAircraft", userSource, vehicle.hash)
+        else
+          print("player did not have enough money")
         end
+      end
     end)
+  end
 end)
 
 RegisterServerEvent("airshop:returnedVehicle")
 AddEventHandler("airshop:returnedVehicle", function(item)
     local userSource = tonumber(source)
+    local price = prices[item.name]
     TriggerEvent("es:getPlayerFromId", userSource, function(user)
         local user_money = user.getActiveCharacterData("money")
-        local returnAmount = .25*item.price
+        local returnAmount = .25*price
         local rounded = round(returnAmount, 0)
-        local new_money = user_money + rounded
-        user.setActiveCharacterData("money", new_money)
+        if rounded <= MAX_AIRCRAFT_RETURN_AMOUNT then
+          local new_money = user_money + rounded
+          user.setActiveCharacterData("money", new_money)
+        else
+          print("*** PLAYER .. " .. GetPlayerName(userSource) .. " WAS TRYING TO MEMORY CHEAT AIRCRAFT RETURN TO EXPLOIT MONEY! ***")
+        end
     end)
 end)
 
--- resources/boat-shop/sv_boatshop.lua
+++ resources/boat-shop/sv_boatshop.lua
@@ -1,9 +1,28 @@
+local prices = {
+  boats = {
+    ["Nagasaki Dinghy"] = 15000,
+    ["Nagasaki Dinghy 2"] = 15000,
+    ["Dinka Marquis"] = 25000,
+    ["Speedophile Seashark"] = 8000,
+    ["Cuban Jetmax"] = 35000,
+    ["Lampadati Toro"] = 40000,
+    ["Lampadati Toro 2"] = 40000,
+    ["Tug"] = 50000,
+    ["Shitzu Squalo"] = 30000,
+    ["Shitzu Tropic"] = 30000,
+    ["Shitzu Suntrap"] = 20000,
+    ["Submersible"] = 100000,
+    ["Submersible 2"] = 100000
+  }
+}
+
 RegisterServerEvent("boatshop:rentVehicle")
 AddEventHandler("boatshop:rentVehicle", function(vehicle)
     print("vehicle.name = " .. vehicle.name)
     print("vehicle.price = " .. vehicle.price)
     print("vehicle.hash = " .. vehicle.hash)
-    local price = vehicle.price
+    local price = prices.boats[vehicle.name]
+    print("rental boat price = $" .. price)
     local userSource = tonumber(source)
     TriggerEvent("es:getPlayerFromId", userSource, function(user)
         print("inside of get player from id")
@@ -24,8 +43,9 @@ end)
 RegisterServerEvent("boatshop:returnedVehicle")
 AddEventHandler("boatshop:returnedVehicle", function(item)
     local userSource = tonumber(source)
+    local price = prices.boats[item.name]
     TriggerEvent("es:getPlayerFromId", userSource, function(user)
-        local returnAmount = .25*item.price
+        local returnAmount = .25*price
         local rounded = round(returnAmount, 0)
         local user_money = user.getActiveCharacterData("money")
         user.setActiveCharacterData("money", user_money + rounded)
