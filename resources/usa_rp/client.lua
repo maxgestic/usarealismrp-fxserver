2776d32c34d0688f7dc63d79042e5c6f3c0c41d5
Joshua G. Yolles <josh@whaicehockey.com>
2017-12-21 09:22:02 -0800

    Increase tazer gun taze duration

--- resources/usa_rp/client.lua
+++ resources/usa_rp/client.lua
@@ -403,6 +403,18 @@ AddEventHandler('veh:toggleEngine', function(status)
     end
 end)
 
+-- increase tazer gun stun duration
+local tiempo = 8000 -- in miliseconds >> 1000 ms = 1s
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		if IsPedBeingStunned(GetPlayerPed(-1)) then
+		SetPedMinGroundTimeForStungun(GetPlayerPed(-1), tiempo)
+		end
+	end
+end)
+
 -- UTILITY FUNCTIONS
 RegisterNetEvent("usa:notify")
 AddEventHandler("usa:notify", function(msg)
