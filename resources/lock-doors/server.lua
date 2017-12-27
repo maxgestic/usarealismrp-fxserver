823eff0cfcf4ca38ce7ee49a8344d505bf30c99d
mini punch <joshyolles@gmail.com>
2017-12-26 16:01:23 -0800

    Remove old lock-doors resource

--- resources/lock-doors/server.lua
+++
@@ -1,8 +0,0 @@
--- Add a command everyone is able to run. Args is a table with all the arguments, and the user is the user object, containing all the user data.
-TriggerEvent('es:addCommand', 'lock', function(source, args, user)
-
-	print("CALLING LOCK:LOCKDOOR")
-
-	TriggerClientEvent("lock:lockDoor", source)
-
-end)
