a24d76df57564cf1a02a7dfeebdae81f1744fdc5
mini punch <joshyolles@gmail.com>
2017-12-28 21:22:51 -0800

    Change seashark hash

--- resources/boat-shop/items.lua
+++ resources/boat-shop/items.lua
@@ -3,7 +3,7 @@ ITEMS = {
         {name = "Nagasaki Dinghy", price =15000, rent = 5000, hash =  1033245328, stored = false},
         {name = "Nagasaki Dinghy 2", price =15000, rent = 5000, hash =  276773164, stored = false},
         {name = "Dinka Marquis", price = 30000, rent = 9000, hash =  -1043459709, stored = false},
-        {name = "Speedophile Seashark", price = 5000, rent = 3500, hash =  3983945033, stored = false},
+        {name = "Speedophile Seashark", price = 5000, rent = 3500, hash =  -1030275036, stored = false},
         {name = "Cuban Jetmax", price = 35000, rent =  7500, hash =  861409633, stored = false},
         {name = "Lampadati Toro", price = 60000, rent = 15000, hash =  1070967343, stored = false},
         {name = "Lampadati Toro 2", price = 60000, rent = 15000, hash =  908897389, stored = false},
