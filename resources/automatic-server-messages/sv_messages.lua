13df08fb29a4135b339bab3b9a6f04f9af6648ef
mini punch <joshyolles@gmail.com>
2018-01-04 10:02:01 -0800

    Increase warning time for server restart

--- resources/automatic-server-messages/sv_messages.lua
+++ resources/automatic-server-messages/sv_messages.lua
@@ -4,14 +4,14 @@ local restartTimeSeconds = 12 * 60 * 60
 local restartTimeMilliseconds = restartTimeSeconds * 1000
 function sendRestartNotification()
     Citizen.CreateThreadNow(function()
-        SetTimeout(restartTimeMilliseconds-120000, function()
-            print("****sending 1 minute 30 second warning...****")
+        SetTimeout(restartTimeMilliseconds-600000, function()
+            print("****sending 10 minute warning...****")
             for i = 1, 8 do
-                TriggerClientEvent("chatMessage", -1, "", {0, 0, 0}, "SERVER RESTARTING IN 2 MINUTES!!!")
+                TriggerClientEvent("chatMessage", -1, "", {0, 0, 0}, "SERVER RESTARTING IN 10 MINUTES!!!")
             end
         end)
-        SetTimeout(restartTimeMilliseconds-60000, function()
-            print("****sending 30 second warning...****")
+        SetTimeout(restartTimeMilliseconds-90000, function()
+            print("****sending 1.5 minute warning...****")
             for i = 1, 8 do
                 TriggerClientEvent("chatMessage", -1, "", {0, 0, 0}, "^1SERVER RESTARTING IN 60 SECONDS! ^3DISCONNECT OR RISK DATA LOSS!!!")
             end
