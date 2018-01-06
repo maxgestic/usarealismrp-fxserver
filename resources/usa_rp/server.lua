2eaf8eab8eaaf9f513f7a914e24ba28c5e932d37
mini punch <joshyolles@gmail.com>
2018-01-05 23:08:21 -0800

    Add inventory weight check for usa:insertItem

--- resources/usa_rp/server.lua
+++ resources/usa_rp/server.lua
@@ -213,65 +213,72 @@ end)
 
 RegisterServerEvent("usa:insertItem")
 AddEventHandler("usa:insertItem", function(to_insert_item, quantity)
-  -- todo implement support for removing more than 1 at a time (aka quantity parameter above)
   print("inside usa:insertItem!")
   local userSource = tonumber(source)
   TriggerEvent("es:getPlayerFromId", userSource, function(user)
-    if to_insert_item.type == "license" then
-      print("inserting/incrementing license item!")
-      -- insert into licenses
-      local user_licenses = user.getActiveCharacterData("licenses")
-      for i = 1, #user_licenses do
-        local item = user_licenses[i]
-        if item.name == to_insert_item.name then
-          print("quantity increased for license item!")
-          user_licenses[i].quantity = item.quantity + quantity
-          user.setActiveCharacterData("licenses", user_licenses)
-          return
+    to_insert_item.quantity = quantity
+    if user.getCanActiveCharacterHoldItem(to_insert_item) then
+      if to_insert_item.type == "license" then
+        print("inserting/incrementing license item!")
+        -- insert into licenses
+        local user_licenses = user.getActiveCharacterData("licenses")
+        for i = 1, #user_licenses do
+          local item = user_licenses[i]
+          if item.name == to_insert_item.name then
+            print("quantity increased for license item!")
+            user_licenses[i].quantity = user_licenses[i].quantity + quantity
+            user.setActiveCharacterData("licenses", user_licenses)
+            return
+          end
         end
-      end
-      print("license inserted!")
-      -- not in licenses already. insert:
-      table.insert(user_licenses, to_insert_item)
-      user.setActiveCharacterData("licenses", user_licenses)
-    elseif to_insert_item.type == "weapon" then
-      print("inserting/incrementing weapon item!")
-      -- insert into weapons, assuming that we've checked that player had < 3 weapons
-      local user_weapons = user.getActiveCharacterData("weapons")
-      for i = 1, #user_weapons do
-        local item = user_weapons[i]
-        if item.name == to_insert_item.name then
-          print("quantity increased for weapon item!")
-          user_weapons[i].quantity = item.quantity + quantity
-          user.setActiveCharacterData("weapons", user_weapons)
-          return
+        print("license inserted!")
+        -- not in licenses already. insert:
+        to_insert_item.quantity = quantity
+        table.insert(user_licenses, to_insert_item)
+        user.setActiveCharacterData("licenses", user_licenses)
+      elseif to_insert_item.type == "weapon" then
+        print("inserting/incrementing weapon item!")
+        -- insert into weapons, assuming that we've checked that player had < 3 weapons
+        local user_weapons = user.getActiveCharacterData("weapons")
+        for i = 1, #user_weapons do
+          local item = user_weapons[i]
+          if item.name == to_insert_item.name then
+            print("quantity increased for weapon item!")
+            user_weapons[i].quantity = user_weapons[i].quantity + quantity
+            user.setActiveCharacterData("weapons", user_weapons)
+            return
+          end
         end
+        print("weapon inserted!")
+        -- not in weapons already. insert:
+        to_insert_item.quantity = quantity
+        table.insert(user_weapons, to_insert_item)
+        user.setActiveCharacterData("weapons", user_weapons)
+        TriggerClientEvent("usa:equipWeapon", userSource, to_insert_item)
+      else
+        --print("inserting/incrementing inventory item!")
+        --print("to_insert_item.name: " .. to_insert_item.name)
+        -- insert into inventory
+        local user_inventory = user.getActiveCharacterData("inventory")
+        --print("#user_inventory: " .. #user_inventory)
+        for z = 1, #user_inventory do
+          local item = user_inventory[z]
+          --print("checking item name for match: " .. item.name)
+          if item.name == to_insert_item.name then
+            print("quantity increased for inventory item!")
+            user_inventory[z].quantity = user_inventory[z].quantity + quantity
+            user.setActiveCharacterData("inventory", user_inventory)
+            return
+          end
+        end
+        print("inventory item inserted!")
+        -- not in inventory already. insert:
+        to_insert_item.quantity = quantity
+        table.insert(user_inventory, to_insert_item)
+        user.setActiveCharacterData("inventory", user_inventory)
       end
-      print("weapon inserted!")
-      -- not in weapons already. insert:
-      table.insert(user_weapons, to_insert_item)
-      user.setActiveCharacterData("weapons", user_weapons)
-      TriggerClientEvent("usa:equipWeapon", userSource, to_insert_item)
     else
-      --print("inserting/incrementing inventory item!")
-      --print("to_insert_item.name: " .. to_insert_item.name)
-      -- insert into inventory
-      local user_inventory = user.getActiveCharacterData("inventory")
-      --print("#user_inventory: " .. #user_inventory)
-      for z = 1, #user_inventory do
-        local item = user_inventory[z]
-        --print("checking item name for match: " .. item.name)
-        if item.name == to_insert_item.name then
-          print("quantity increased for inventory item!")
-          user_inventory[z].quantity = item.quantity + quantity
-          user.setActiveCharacterData("inventory", user_inventory)
-          return
-        end
-      end
-      print("inventory item inserted!")
-      -- not in inventory already. insert:
-      table.insert(user_inventory, to_insert_item)
-      user.setActiveCharacterData("inventory", user_inventory)
+      TriggerClientEvent("usa:notify", "Inventory full.")
     end
   end)
 end)
