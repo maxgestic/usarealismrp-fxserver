6208fe8c6cecb4d25aa936fe50681a36018dcf56
mini punch <joshyolles@gmail.com>
2018-01-06 18:45:43 -0800

    Able to harvest, process, sell weed for money

--- resources/harvest-process-sell/cl_harvest-process-sell.lua
+++ resources/harvest-process-sell/cl_harvest-process-sell.lua
@@ -1,13 +1,13 @@
 local JOBS = {
   ["Weed"] = {
-    harvest = {x = 2224.04, y = 5577.28, z = 52.7},
-    process = {x = 0000.04, y = 0000.28, z = 00.7}.
-    sell = {x = 0000.04, y = 0000.28, z = 00.7},
-    harvest_time = 10
+    harvest = {x = 2224.04, y = 5577.28, z = 52.7, reward_item_name = "Raw Weed", time = 22},
+    process = {x = 1442.84, y = 6332.9, z = 22.9, time = 25},
+    sell = {x = -1134.04, y = 4948.84, z = 221.35}
   }
 }
 
 local KEY = 38 -- "E"
+local SOUND_ENABLE = true
 
 ------------------------------------------------
 -- See if player is close to any job location --
@@ -15,24 +15,27 @@ local KEY = 38 -- "E"
 Citizen.CreateThread(function()
   while true do
     local player_ped = GetPlayerPed(-1)
-    local player_coords = GetEntityCoords(ped)
+    local player_coords = GetEntityCoords(player_ped)
     for job, places in pairs(JOBS) do
       if Vdist(player_coords, places.harvest.x, places.harvest.y, places.harvest.z) < 9 then
-        print("player is close to job location:  " .. job)
+        print("player is close to harvest job location:  " .. job)
         drawTxt("Press ~g~E~w~ to harvest " .. job .. "!",0,1,0.5,0.8,0.6,255,255,255,255)
         if IsControlJustPressed(1, KEY) then
-          Harvest(job, places.harvest_time)
+          TriggerEvent("usa:notify", "~y~Harvesting: ~w~(1x) " .. places.harvest.reward_item_name)
+          Harvest(job, places.harvest.time)
         end
-      elseif Vdist(player_coords, places.process.x, places.process.y, places.process.z) < 9 then
-        print("player is close to job location:  " .. job)
+      elseif Vdist(player_coords, places.process.x, places.process.y, places.process.z) < 6.5 then
+        print("player is close to process job location:  " .. job)
         drawTxt("Press ~g~E~w~ to process " .. job .. "!",0,1,0.5,0.8,0.6,255,255,255,255)
         if IsControlJustPressed(1, KEY) then
-          --Process(job, places.harvest_time)
+          TriggerServerEvent("HPS:checkItem", job, places.process.time, "Process")
+          Wait(places.process.time * 1000) -- prevent spamming
         end
-      elseif Vdist(player_coords, places.sell.x, places.sell.y, places.sell.z) < 9 then
-        print("player is close to job location:  " .. job)
+      elseif Vdist(player_coords, places.sell.x, places.sell.y, places.sell.z) < 2.0 then
+        print("player is close to sell job location:  " .. job)
         drawTxt("Press ~g~E~w~ to sell " .. job .. "!",0,1,0.5,0.8,0.6,255,255,255,255)
         if IsControlJustPressed(1, KEY) then
+          TriggerServerEvent("HPS:checkItem", job, 0, "Sale")
           --Sell(job, places.harvest_time)
         end
       end
@@ -42,9 +45,30 @@ Citizen.CreateThread(function()
 end)
 
 ------------------
+-- Draw Markers --
+------------------
+Citizen.CreateThread(function()
+  while true do
+    for job, places in pairs(JOBS) do
+      DrawMarker(27, places.sell.x, places.sell.y, places.sell.z, 0, 0, 0, 0, 0, 0, 1.0001, 1.0001, 1.5001, 1555, 0, 0,255, 0, 0, 0,0)
+    end
+    Wait(0)
+  end
+end)
+
+-------------------
+-- CUSTOM EVENTS --
+-------------------
+RegisterNetEvent("HPS:continueProcessing")
+AddEventHandler("HPS:continueProcessing", function(job_name, process_time)
+  TriggerEvent("usa:notify", "~y~Processing: ~w~(1x) " .. string.lower(job_name))
+  Process(job_name, process_time)
+end)
+
+------------------
 -- HARVEST ITEM --
 ------------------
-function Harvest(to_harvest_item_name, anim_play_time)
+function Harvest(to_harvest_item_name, harvest_time)
   -- weed:
   if to_harvest_item_name == "Weed" then
     -- play animation:
@@ -52,13 +76,35 @@ function Harvest(to_harvest_item_name, anim_play_time)
       dict = "anim@move_m@trash",
       name = "pickup"
     }
-    TriggerEvent("usa:playAnimation", anim.name, anim.dict, anim_play_time)
-    --
+    TriggerEvent("usa:playAnimation", anim.name, anim.dict, harvest_time)
+    -- play sound:
+    if SOUND_ENABLE then TriggerServerEvent("InteractSound_SV:PlayWithinDistance", 2.5, "trimming", 0.75) end
   end
   -- pause for time
-  Wait(anim_play_time * 1000)
+  Wait(harvest_time * 1000)
   -- give item harvested item:
-  TriggerServerEvent("HPS:rewardItem", to_harvest_item_name)
+  TriggerServerEvent("HPS:rewardItem", to_harvest_item_name, "Harvest")
+end
+
+------------------
+-- PROCESS ITEM --
+------------------
+function Process(to_process_item_name, process_time)
+  -- weed:
+  if to_process_item_name == "Weed" then
+    -- play animation:
+    local anim = {
+      dict = "timetable@jimmy@ig_1@idle_a",
+      name = "hydrotropic_bud_or_something"
+    }
+    TriggerEvent("usa:playAnimation", anim.name, anim.dict, process_time)
+    -- play sound:
+    if SOUND_ENABLE then TriggerServerEvent("InteractSound_SV:PlayWithinDistance", 1.8, "weed-process", 1.0) end
+  end
+  -- pause for time
+  Wait(process_time * 1000)
+  -- give item harvested item:
+  TriggerServerEvent("HPS:rewardItem", to_process_item_name, "Process")
 end
 
 ----------------------
--- resources/harvest-process-sell/sv_harvest-process-sell.lua
+++ resources/harvest-process-sell/sv_harvest-process-sell.lua
@@ -1,21 +1,104 @@
 local REWARDS = {
   ["Weed"] = {
-    item = {
+    harvest_item = {
       name = "Raw Weed",
       quantity = 1,
       weight = 8.0,
       type = "drug",
-      legality = "legal"
-    }
+      legality = "illegal"
+    },
+    processed_item = {
+      name = "Concentrated Cannabis",
+      quantity = 1,
+      weight = 6.0,
+      type = "drug",
+      legality = "illegal"
+    },
+    reward_amount = 230
   }
 }
 
+-- todo: add required items like scissors, bags and butane/dishes for harvest / processing respectively
+
+RegisterServerEvent("HPS:checkItem")
+AddEventHandler("HPS:checkItem", function(job_name, process_time, stage)
+  print("stage: " .. stage)
+  local item_name = "undefined"
+  if stage == "Process" then
+    item_name = REWARDS[job_name].harvest_item.name
+  elseif stage == "Sale" then
+    item_name = REWARDS[job_name].processed_item.name
+  end
+  print("item_name: " .. item_name)
+  if item_name then
+    local userSource = tonumber(source)
+    TriggerEvent("es:getPlayerFromId", userSource, function(user)
+      local inventory = user.getActiveCharacterData("inventory")
+      for i = 1, #inventory do
+        local item = inventory[i]
+        if item then
+          if item.name == item_name then
+            if stage == "Process" then
+              TriggerClientEvent("HPS:continueProcessing", userSource, job_name, process_time)
+            elseif stage == "Sale" then
+              -- remove item:
+              if item.quantity > 1 then
+                inventory[i].quantity = inventory[i].quantity - 1
+              else
+                table.remove(inventory, i)
+              end
+              user.setActiveCharacterData("inventory", inventory)
+              -- give money:
+              local user_money = user.getActiveCharacterData("money")
+              user.setActiveCharacterData("money", user_money + REWARDS[job_name].reward_amount)
+              TriggerClientEvent("usa:notify", userSource, "Here is the cash!")
+            end
+            return
+          end
+        end
+      end
+      -- no materials:
+      if stage == "Process" then
+        TriggerClientEvent("usa:notify", userSource, "Don't have any " .. string.lower(job_name) .. " to process!")
+      elseif stage == "Sale" then
+        TriggerClientEvent("usa:notify", userSource, "Don't have any " .. string.lower(job_name) .. " to sell!")
+      end
+    end)
+  end
+end)
+
 RegisterServerEvent("HPS:rewardItem")
-AddEventHandler("HPS:rewardItem", function(item_name)
-  for job, item in pairs(REWARDS) do
-    if job == item_name then
-      print("giving player harvested item: " .. item.name)
-      TriggerEvent("usa:insertItem", item, item.quantity)
+AddEventHandler("HPS:rewardItem", function(job_name, stage)
+  local userSource = tonumber(source)
+  print("source: " .. userSource)
+  for job, data in pairs(REWARDS) do
+    if job == job_name then
+      if stage == "Harvest" then
+        print("giving player harvested item: " .. data.harvest_item.name)
+        TriggerEvent("usa:insertItem", data.harvest_item, data.harvest_item.quantity, userSource)
+      elseif stage == "Process" then
+        TriggerEvent("es:getPlayerFromId", userSource, function(user)
+          local inventory = user.getActiveCharacterData("inventory")
+          for i = 1, #inventory do
+            local item = inventory[i]
+            if item then
+              if item.name == data.harvest_item.name then
+                if item.quantity > 1 then
+                  inventory[i].quantity = inventory[i].quantity - 1
+                else
+                  table.remove(inventory, i)
+                end
+                user.setActiveCharacterData("inventory", inventory)
+                print("giving player processed item: " .. data.processed_item.name)
+                TriggerEvent("usa:insertItem", data.processed_item, data.processed_item.quantity, userSource)
+                return
+              end
+            end
+          end
+          -- at this point, item was not found
+          TriggerClientEvent("usa:notify", userSource, "You don't have enough " .. data.harvest_item.name .. " to process!")
+        end)
+      end
     end
   end
 end)
