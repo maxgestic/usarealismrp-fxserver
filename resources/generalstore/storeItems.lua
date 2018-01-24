432d6aee74167c8b1168b4e5d9f04b5acbdff1f9
Josh Yolles <josh@whaicehockey.com>
2018-01-23 23:26:07 -0800

    Increase food/drink substance

--- resources/generalstore/storeItems.lua
+++ resources/generalstore/storeItems.lua
@@ -1,19 +1,19 @@
 storeItems = {
     ["Food"] = {
-      {name = "Tuna Sandwich", price = 40, type = "food", substance = 35.0, quantity = 1, legality = "legal", weight = 4},
-      {name = "Chocolate Pizza", price = 25, type = "food", substance = 18.0, quantity = 1, legality = "legal", weight = 3},
-      {name = "Cheeseburger", price = 50, type = "food", substance = 35.0, quantity = 1, legality = "legal", weight = 4},
-      {name = "Kosher Hot Dog", price = 40, type = "food", substance = 31.5, quantity = 1, legality = "legal", weight = 4},
+      {name = "Tuna Sandwich", price = 40, type = "food", substance = 60.0, quantity = 1, legality = "legal", weight = 4},
+      {name = "Chocolate Pizza", price = 25, type = "food", substance = 25.0, quantity = 1, legality = "legal", weight = 3},
+      {name = "Cheeseburger", price = 50, type = "food", substance = 60.0, quantity = 1, legality = "legal", weight = 4},
+      {name = "Kosher Hot Dog", price = 40, type = "food", substance = 41.0, quantity = 1, legality = "legal", weight = 4},
       {name = "Walkers Milk Chocolate Toffee", price = 35, type = "food", substance = 25.0, quantity = 1, legality = "legal", weight = 3},
-      {name = "Salt & Vinegar Chips", price = 15, type = "food", substance = 15.0, quantity = 1, legality = "legal", weight = 3},
-      {name = "Fries", price = 20, type = "food", substance = 20.0, quantity = 1, legality = "legal", weight = 3}
+      {name = "Salt & Vinegar Chips", price = 15, type = "food", substance = 25.0, quantity = 1, legality = "legal", weight = 3},
+      {name = "Fries", price = 20, type = "food", substance = 40.0, quantity = 1, legality = "legal", weight = 3}
     },
     ["Drinks"] = {
-      {name = "Water", price = 30, type = "drink", substance = 30.0, quantity = 1, legality = "legal", weight = 4},
-      {name = "Cookies n Cream Milkshake", price = 35, type = "drink", substance = 34.0, quantity = 1, legality = "legal", weight = 5},
-      {name = "Orange Juice w/ Pulp", price = 35, type = "drink", substance = 30.0, quantity = 1, legality = "legal", weight = 4},
-      {name = "Soda", price = 40, type = "drink", substance = 28.0, quantity = 1, legality = "legal", weight = 4},
-      {name = "Four Loko", price = 50, type = "alcohol", substance = 10.0, quantity = 1, legality = "legal", weight = 4, strength = 0.02},
+      {name = "Water", price = 30, type = "drink", substance = 60.0, quantity = 1, legality = "legal", weight = 4},
+      {name = "Cookies n Cream Milkshake", price = 35, type = "drink", substance = 54.0, quantity = 1, legality = "legal", weight = 5},
+      {name = "Orange Juice w/ Pulp", price = 35, type = "drink", substance = 50.0, quantity = 1, legality = "legal", weight = 4},
+      {name = "Soda", price = 40, type = "drink", substance = 48.0, quantity = 1, legality = "legal", weight = 4},
+      {name = "Four Loko", price = 50, type = "alcohol", substance = 40.0, quantity = 1, legality = "legal", weight = 4, strength = 0.02},
       {name = "Smirnoff Vodka", price = 100, type = "alcohol", substance = 10.0, quantity = 1, legality = "legal", weight = 4, strength = 0.04}
       --{name = "Lite Beer 4 Pack", price = 100, type = "Drink", substance = 5.0, quantity = 4, legality = "legal", weight = 6},
       --{name = "Champagne", price = 200, type = "Drink", substance = 15.0, quantity = 1, legality = "legal", weight = 8}
