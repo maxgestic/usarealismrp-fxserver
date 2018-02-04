51bd5b981650a6b54c2e24678815a1d469509f71
Josh Yolles <josh@whaicehockey.com>
2018-02-04 14:35:52 -0800

    Add more food items

--- resources/generalstore/storeItems.lua
+++ resources/generalstore/storeItems.lua
@@ -3,20 +3,36 @@ storeItems = {
       {name = "Tuna Sandwich", price = 40, type = "food", substance = 60.0, quantity = 1, legality = "legal", weight = 4},
       {name = "Chocolate Pizza", price = 25, type = "food", substance = 25.0, quantity = 1, legality = "legal", weight = 3},
       {name = "Cheeseburger", price = 50, type = "food", substance = 60.0, quantity = 1, legality = "legal", weight = 4},
-      {name = "Kosher Hot Dog", price = 40, type = "food", substance = 41.0, quantity = 1, legality = "legal", weight = 4},
+      {name = "Kosher Hot Dog", price = 40, type = "food", substance = 55.0, quantity = 1, legality = "legal", weight = 4},
+      {name = "Microwave Burrito", price = 45, type = "food", substance = 52.0, quantity = 1, legality = "legal", weight = 4},
+      {name = "Peanut Butter Cups", price = 45, type = "food", substance = 40.0, quantity = 1, legality = "legal", weight = 4},
+      {name = "Taco", price = 58, type = "food", substance = 45.0, quantity = 1, legality = "legal", weight = 4},
+      {name = "Nachos", price = 30, type = "food", substance = 30.0, quantity = 1, legality = "legal", weight = 4},
       {name = "Walkers Milk Chocolate Toffee", price = 35, type = "food", substance = 25.0, quantity = 1, legality = "legal", weight = 3},
       {name = "Salt & Vinegar Chips", price = 15, type = "food", substance = 25.0, quantity = 1, legality = "legal", weight = 3},
+      {name = "Cheetos", price = 30, type = "food", substance = 30.0, quantity = 1, legality = "legal", weight = 4},
+      {name = "Hot Cheetos", price = 30, type = "food", substance = 30.0, quantity = 1, legality = "legal", weight = 4},
+      {name = "Doritos", price = 30, type = "food", substance = 30.0, quantity = 1, legality = "legal", weight = 4},
       {name = "Fries", price = 20, type = "food", substance = 40.0, quantity = 1, legality = "legal", weight = 3}
     },
     ["Drinks"] = {
       {name = "Water", price = 30, type = "drink", substance = 60.0, quantity = 1, legality = "legal", weight = 4},
+      {name = "Arizona Iced Tea", price = 30, type = "drink", substance = 49.0, quantity = 1, legality = "legal", weight = 3},
+      {name = "Caramel Iced Coffee", price = 40, type = "drink", substance = 49.0, quantity = 1, legality = "legal", weight = 3},
+      {name = "Slurpy", price = 45, type = "drink", substance = 50.0, quantity = 1, legality = "legal", weight = 3},
       {name = "Cookies n Cream Milkshake", price = 35, type = "drink", substance = 54.0, quantity = 1, legality = "legal", weight = 5},
+      {name = "Yoohoo Chocolate Milk", price = 35, type = "drink", substance = 50.0, quantity = 1, legality = "legal", weight = 4},
       {name = "Orange Juice w/ Pulp", price = 35, type = "drink", substance = 50.0, quantity = 1, legality = "legal", weight = 4},
-      {name = "Soda", price = 40, type = "drink", substance = 48.0, quantity = 1, legality = "legal", weight = 4},
+      {name = "Apple Juice", price = 35, type = "drink", substance = 50.0, quantity = 1, legality = "legal", weight = 4},
+      {name = "Pepsi", price = 40, type = "drink", substance = 48.0, quantity = 1, legality = "legal", weight = 4},
+      {name = "Diet Dr. Pepper", price = 40, type = "drink", substance = 48.0, quantity = 1, legality = "legal", weight = 4},
+      {name = "Strawberry Soda", price = 40, type = "drink", substance = 48.0, quantity = 1, legality = "legal", weight = 4},
+      {name = "Grape Soda", price = 40, type = "drink", substance = 48.0, quantity = 1, legality = "legal", weight = 4},
       {name = "Four Loko", price = 50, type = "alcohol", substance = 40.0, quantity = 1, legality = "legal", weight = 4, strength = 0.02},
-      {name = "Smirnoff Vodka", price = 100, type = "alcohol", substance = 10.0, quantity = 1, legality = "legal", weight = 4, strength = 0.04}
-      --{name = "Lite Beer 4 Pack", price = 100, type = "Drink", substance = 5.0, quantity = 4, legality = "legal", weight = 6},
-      --{name = "Champagne", price = 200, type = "Drink", substance = 15.0, quantity = 1, legality = "legal", weight = 8}
+      {name = "Corona Light Beer", price = 50, type = "alcohol", substance = 20.0, quantity = 4, legality = "legal", weight = 2, strength = 0.03},
+      {name = "Jack Daniels Whiskey", price = 105, type = "alcohol", substance = 10.0, quantity = 1, legality = "legal", weight = 4, strength = 0.06},
+      {name = "Champagne", price = 200, type = "alcohol", substance = 18.0, quantity = 1, legality = "legal", weight = 4, strength = 0.06},
+      {name = "Smirnoff Vodka", price = 100, type = "alcohol", substance = 10.0, quantity = 1, legality = "legal", weight = 4, strength = 0.05}
     },
     ["Vehicle"] = {
         { name = "Repair Kit", price = 1500, type = "vehicle", quantity = 1, legality = "legal", weight = 12}
