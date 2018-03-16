d0b11207f6ce1b89f8dbd9b5b25ba727d810815e
Josh Yolles <josh@whaicehockey.com>
2018-03-15 22:01:26 -0700

    Remove gtao scoreboard

--- resources/[system]/gtaoscoreboard/__resource.lua
+++
@@ -1,5 +0,0 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
-
--- You can choose from: 'left', 'center', 'right'.
-displayType 'left' -- default: 'left'.
-client_script 'GTAoScoreboard.net.dll'
