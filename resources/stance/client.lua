2cc1a45f816d01d2f3cc5317d69a437417352897
Josh Yolles <josh@whaicehockey.com>
2018-05-03 22:48:35 -0700

    Adjust stance

--- resources/stance/client.lua
+++ resources/stance/client.lua
@@ -13,7 +13,7 @@ Citizen.CreateThread( function()
 			DisableControlAction( 0, proneKey, true )
 			DisableControlAction( 0, crouchKey, true )
 			if ( not IsPauseMenuActive() ) then
-				if ( IsDisabledControlJustPressed( 0, crouchKey ) ) then
+				if IsDisabledControlJustPressed(0, crouchKey) and not IsPedInAnyVehicle(ped, true) then
 					if standing then
 						RequestAnimSet( "move_ped_crouched" )
 
@@ -21,16 +21,11 @@ Citizen.CreateThread( function()
 							Citizen.Wait( 100 )
 						end
 
-						--if ( crouched and not proned ) then
-							--ResetPedMovementClipset( ped )
-							--ResetPedStrafeClipset(ped)
-							--crouched = false
-						--elseif ( not crouched and not proned ) then
 							SetPedMovementClipset( ped, "move_ped_crouched", 0.55 )
 							SetPedStrafeClipset(ped, "move_ped_crouched_strafing")
 							crouched = true
 							standing = false
-						--end
+							
 					elseif crouched and not IsPedInAnyVehicle(ped, true) and not IsPedFalling(ped) and not IsPedDiving(ped) and not IsPedInCover(ped, false) and not IsPedInParachuteFreeFall(ped) and (GetPedParachuteState(ped) == 0 or GetPedParachuteState(ped) == -1)  then
 						RequestAnimSet( "move_crawl" )
 						while ( not HasAnimSetLoaded( "move_crawl" ) ) do
@@ -51,27 +46,7 @@ Citizen.CreateThread( function()
 						proned = false
 						standing = true
 					end
-				--[[
-				elseif ( IsDisabledControlJustPressed(0, proneKey) and not crouched and not IsPedInAnyVehicle(ped, true) and not IsPedFalling(ped) and not IsPedDiving(ped) and not IsPedInCover(ped, false) and not IsPedInParachuteFreeFall(ped) and (GetPedParachuteState(ped) == 0 or GetPedParachuteState(ped) == -1) ) then
-					if proned then
-						ClearPedTasksImmediately(ped)
-						proned = false
-					else
-						RequestAnimSet( "move_crawl" )
-						while ( not HasAnimSetLoaded( "move_crawl" ) ) do
-							Citizen.Wait( 100 )
-						end
-						ClearPedTasksImmediately(ped)
-						proned = true
-						if IsPedSprinting(ped) or IsPedRunning(ped) or GetEntitySpeed(ped) > 5 then
-							TaskPlayAnim(ped, "move_jump", "dive_start_run", 8.0, 1.0, -1, 0, 0.0, 0, 0, 0)
-							Wait(1000)
-						end
-						SetProned()
-					end
-					--]]
 				end
-				--]]
 			end
 		else
 			proned = false
