71e207d8c4491b12ede69959e9299a170ba7504d
mini punch <joshyolles@gmail.com>
2018-06-06 17:31:21 -0700

    loadscreen: Added more images to background

--- resources/[test]/keks-new/js/lj-safety-first.js
+++ resources/[test]/keks-new/js/lj-safety-first.js
@@ -149,6 +149,12 @@
         "img/bg-1.jpg"
       , "img/bg-2.jpg"
       , "img/bg-3.jpg"
+	  , "img/bg-4.jpg"
+	  , "img/bg-5.jpg"
+	  , "img/bg-6.jpg"
+	  , "img/bg-7.jpg"
+	  , "img/bg-8.jpg"
+	  , "img/bg-9.jpg"
     ],{duration: 4400, fade: 1500});
 
     // Header's wrapper vertical centering
