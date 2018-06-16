60bcb9da8fb365af53e475e80d6724b6e917ea6a
tleachman1234 <tleachman1234@gmail.com>
2018-06-16 11:32:36 -0500

    Taxi fixes (#239)
    * userSource bugfix and indentation
    
    * refactor code logic

--- resources/taxijob/sv_taxijob.lua
+++ resources/taxijob/sv_taxijob.lua
@@ -4,57 +4,64 @@ local DUTY_FEE = 300
 
 RegisterServerEvent("taxi:setJob")
 AddEventHandler("taxi:setJob", function(property)
-  TriggerEvent("es:getPlayerFromId", source, function(user)
-  
+	local userSource = source
+	local user = exports["essentialmode"]:getPlayerFromId(userSource)
 	local first, last = user.getActiveCharacterData("firstName"), user.getActiveCharacterData("lastName")
-	if not first then first = "" elseif not last then last = "" end
+	if not first then
+		first = ""
+	end
+	if not last then
+		last = ""
+	end
 	local name = first .. " " .. last
-    local user_licenses = user.getActiveCharacterData("licenses")
-    local user_money = user.getActiveCharacterData("money")
-    if user.getActiveCharacterData("job") == "taxi" then
-      print("user " .. GetPlayerName(source) .. " just went off duty for downtown taxi cab co.!")
-      user.setActiveCharacterData("job", "civ")
-      TriggerClientEvent("taxi:offDuty", source)
-    else
-      if user_money - DUTY_FEE >= 0 then
-        if not timeout then
-          print("user " .. name .. " just is trying to go on duty for downtown taxi cab co.!")
-          for i = 1, #user_licenses do
-            local item = user_licenses[i]
-            if string.find(item.name, "Driver") then
-              print("DL found! checking validity")
-              if item.status == "valid" then
-                user.setActiveCharacterData("job", "taxi")
-                TriggerClientEvent("taxi:onDuty", source, name)
-                -- take money --
-                user.setActiveCharacterData("money", user_money - DUTY_FEE)
-                -- give money to taxi shop owner --
-                if property then 
-                  TriggerEvent("properties:addMoney", property.name, round(0.20 * DUTY_FEE, 0))
-                end
-                  timeout = true
-                  SetTimeout(15000, function()
-                    timeout = false
-                  end)
-                  return
-              else 
-                TriggerClientEvent("usa:notify", source, "Your license is suspended!")
-                return
-              end
-            end
-          end
-          -- at this point, no valid DL was found
-          TriggerClientEvent("usa:notify", source, "You don't have a valid driver's license!")
-        else
-          print("player is on timeout and cannot go on duty for downtown taxi co!")
-          TriggerClientEvent("usa:notify", source, "Can't retrieve another car! Please wait a little.")
-        end
-      else 
-        print("player did not have enough money to go on duty for taxi!")
-        TriggerClientEvent("usa:notify", source, "You don't have enough money to pay the security fee!")
-      end
-    end
-  end)
+	local user_licenses = user.getActiveCharacterData("licenses")
+	local user_money = user.getActiveCharacterData("money")
+	if user.getActiveCharacterData("job") == "taxi" then
+		print("user " .. GetPlayerName(userSource) .. " just went off duty for downtown taxi cab co.!")
+		user.setActiveCharacterData("job", "civ")
+		TriggerClientEvent("taxi:offDuty", userSource)
+	else
+		if user_money < DUTY_FEE then
+			print("player did not have enough money to go on duty for taxi!")
+			TriggerClientEvent("usa:notify", userSource, "You don't have enough money to pay the security fee!")
+			return
+		end
+		
+		if timeout then
+			print("player is on timeout and cannot go on duty for downtown taxi co!")
+			TriggerClientEvent("usa:notify", userSource, "Can't retrieve another car! Please wait a little.")
+			return
+		end
+
+		print("user " .. name .. " just is trying to go on duty for downtown taxi cab co.!")
+		for i = 1, #user_licenses do
+			local item = user_licenses[i]
+			if string.find(item.name, "Driver") then
+				print("DL found! checking validity")
+				if item.status ~= "valid" then
+					TriggerClientEvent("usa:notify", userSource, "Your license is suspended!")
+					return
+				end
+
+				user.setActiveCharacterData("job", "taxi")
+				TriggerClientEvent("taxi:onDuty", userSource, name)
+				-- take money --
+				user.setActiveCharacterData("money", user_money - DUTY_FEE)
+				-- give money to taxi shop owner --
+				if property then
+					TriggerEvent("properties:addMoney", property.name, round(0.20 * DUTY_FEE, 0))
+				end
+
+				timeout = true
+				SetTimeout(15000, function()
+					timeout = false
+				end)
+				return
+			end
+		end
+		-- at this point, no valid DL was found
+		TriggerClientEvent("usa:notify", userSource, "You don't have a valid driver's license!")
+	end
 end)
 
 function round(num, numDecimalPlaces)
