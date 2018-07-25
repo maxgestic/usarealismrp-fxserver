5449d6a944171ebb60e6d0fcbca7fc6bdc77bdcd
minipunch <joshyolles@gmail.com>
2018-07-25 10:10:39 -0700

    Updated general store

--- resources/generalstore/cl_generalstore.lua
+++ resources/generalstore/cl_generalstore.lua
@@ -1,4 +1,8 @@
-local locations = {
+--# by minipunch
+--# for USA REALISM rp
+--# Made for the 24/7 stores as general stores for various items
+
+local GENERAL_STORE_LOCATIONS = {
     {x=1961.061, y=3741.604, z=31.343 },
     { x=374.586, y=326.037, z=102.566 },
     { x=1136.534, y=-971.131, z=45.415 },
@@ -20,247 +24,51 @@ local locations = {
     { x=-1487.241, y=379.843, z=39.163 },
 }
 
-function isPlayerAtGeneralStore()
-	local playerCoords = GetEntityCoords(GetPlayerPed(-1) --[[Ped]], false)
-	for i = 1, #locations do
-		if GetDistanceBetweenCoords(playerCoords.x,playerCoords.y,playerCoords.z,locations[i].x,locations[i].y,locations[i].z,false) < 1 then
+local GENERAL_STORE_ITEMS = {} -- loaded from server
+
+local created_menus = {}
+
+local MENU_KEY = 38
+
+-------------------
+-- utility funcs --
+-------------------
+function comma_value(amount)
+  local formatted = amount
+  while true do
+    formatted, k = string.gsub(formatted, "^(-?%d+)(%d%d%d)", '%1,%2')
+    if (k==0) then
+      break
+    end
+  end
+  return formatted
+end
+
+function IsAnyMenuVisible()
+	for i = 1, #created_menus do
+		if created_menus[i]:Visible() then
 			return true
 		end
 	end
 	return false
 end
 
-function buyItem(item)
-  local playerCoords = GetEntityCoords(GetPlayerPed(-1) --[[Ped]], false)
-  TriggerEvent("properties:getPropertyGivenCoords", playerCoords.x, playerCoords.y, playerCoords.z, function(property)
-    print("inside property coords callback!")
-    if not property then print("no property found!") end
-    TriggerServerEvent("generalStore:buyItem", item, property)
-  end)
+function CloseAllMenus()
+	for i = 1, #created_menus do
+		if created_menus[i]:Visible() then
+			created_menus[i]:Visible(false)
+		end
+	end
 end
 
-Citizen.CreateThread(function()
-
-  local menu = {
-    open = false,
-    page = "home",
-    title = "General Store",
-    key = 38 -- "E"
-  }
-
-	while true do
-		Citizen.Wait(0)
-    ------------------------------
-    -- DRAW MARKERS & HELP TEXT --
-    ------------------------------
-		for i = 1, #locations do
-			DrawMarker(27, locations[i].x, locations[i].y, locations[i].z, 0, 0, 0, 0, 0, 0, 2.0, 2.0, 1.0, 15, 0, 190, 90, 0, 0, 2, 0, 0, 0, 0)
-		end
-		if isPlayerAtGeneralStore() and not menu.open then
-			drawTxt("Press ~g~E~w~ to open the general store menu!",7,1,0.5,0.8,0.6,255,255,255,255)
-		end
-    -----------------------
-    -- OPEN / CLOSE MENU --
-    -----------------------
-		if IsControlJustPressed(1,menu.key) then
-			if isPlayerAtGeneralStore() then
-        menu.open = not menu.open
-			end
-		elseif not isPlayerAtGeneralStore() then
-      menu.open = false
-		end
-    --------------
-    -- THE MENU --
-    --------------
-    if menu.open then
-      TriggerEvent("GUI-general:Title", menu.title)
-      ---------------
-      -- HOME MENU --
-      ---------------
-      if menu.page == "home" then
-  			TriggerEvent("GUI-general:Option", "Drinks", function(cb)
-  				if cb then
-            menu.page = "drinks"
-            menu.title = "Drinks"
-  				end
-  			end)
-        TriggerEvent("GUI-general:Option", "Food", function(cb)
-  				if cb then
-            menu.page = "food"
-            menu.title = "Food"
-  				end
-  			end)
-        TriggerEvent("GUI-general:Option", "Vehicle", function(cb)
-  				if cb then
-            menu.page = "vehicle"
-            menu.title = "Vehicle"
-  				end
-  			end)
-        TriggerEvent("GUI-general:Option", "Electronics", function(cb)
-  				if cb then
-            menu.page = "electronics"
-            menu.title = "Electronics"
-  				end
-  			end)
-        TriggerEvent("GUI-general:Option", "Miscellaneous", function(cb)
-  				if cb then
-            menu.page = "misc"
-            menu.title = "Miscellaneous"
-  				end
-  			end)
-        TriggerEvent("GUI-general:Option", "~y~Close", function(cb)
-  				if cb then
-            menu.page = "home"
-            menu.open = false
-            menu.title = "General Store"
-  				end
-  			end)
-        ---------------
-        -- DRINK MENU --
-        ---------------
-      elseif menu.page == "drinks" then
-        for i = 1, #storeItems["Drinks"] do
-          item = storeItems["Drinks"][i]
-          TriggerEvent("GUI-general:Option", "(~g~$" .. item.price .. "~w~) " .. item.name, function(cb)
-            if cb then
-              --print("person wants to buy: " .. item.name)
-              buyItem(item)
-              --menu.page = "home"
-              --menu.title = "General Store"
-            end
-          end)
-        end
-        TriggerEvent("GUI-general:Option", "~y~Back", function(cb)
-          if cb then
-            menu.page = "home"
-            menu.title = "General Store"
-          end
-        end)
-        TriggerEvent("GUI-general:Option", "~y~Close", function(cb)
-          if cb then
-            menu.page = "home"
-            menu.open = false
-            menu.title = "General Store"
-          end
-        end)
-        ---------------
-        -- FOOD MENU --
-        ---------------
-      elseif menu.page == "food" then
-        for i = 1, #storeItems["Food"] do
-          item = storeItems["Food"][i]
-          TriggerEvent("GUI-general:Option", "(~g~$" .. item.price .. "~w~) " .. item.name, function(cb)
-            if cb then
-              --print("person wants to buy: " .. item.name)
-              buyItem(item)
-              --menu.page = "home"
-              --menu.title = "General Store"
-            end
-          end)
-        end
-        TriggerEvent("GUI-general:Option", "~y~Back", function(cb)
-          if cb then
-            menu.page = "home"
-            menu.title = "General Store"
-          end
-        end)
-        TriggerEvent("GUI-general:Option", "~y~Close", function(cb)
-          if cb then
-            menu.page = "home"
-            menu.open = false
-            menu.title = "General Store"
-          end
-        end)
-        ------------------
-        -- VEHICLE MENU --
-        ------------------
-      elseif menu.page == "vehicle" then
-        for i = 1, #storeItems["Vehicle"] do
-          item = storeItems["Vehicle"][i]
-          TriggerEvent("GUI-general:Option", "(~g~$" .. item.price .. "~w~) " .. item.name, function(cb)
-            if cb then
-              --print("person wants to buy: " .. item.name)
-              buyItem(item)
-              --menu.page = "home"
-              --menu.title = "General Store"
-            end
-          end)
-        end
-        TriggerEvent("GUI-general:Option", "~y~Back", function(cb)
-          if cb then
-            menu.page = "home"
-            menu.title = "General Store"
-          end
-        end)
-        TriggerEvent("GUI-general:Option", "~y~Close", function(cb)
-          if cb then
-            menu.page = "home"
-            menu.open = false
-            menu.title = "General Store"
-          end
-        end)
-      ----------------------
-      -- ElECTRONICS MENU --
-      ----------------------
-      elseif menu.page == "electronics" then
-        for i = 1, #storeItems["Electronics"] do
-          item = storeItems["Electronics"][i]
-          TriggerEvent("GUI-general:Option", "(~g~$" .. item.price .. "~w~) " .. item.name, function(cb)
-            if cb then
-              --print("person wants to buy: " .. item.name)
-              buyItem(item)
-              --menu.page = "home"
-              --menu.title = "General Store"
-            end
-          end)
-        end
-        TriggerEvent("GUI-general:Option", "~y~Back", function(cb)
-          if cb then
-            menu.page = "home"
-            menu.title = "General Store"
-          end
-        end)
-        TriggerEvent("GUI-general:Option", "~y~Close", function(cb)
-          if cb then
-            menu.page = "home"
-            menu.open = false
-            menu.title = "General Store"
-          end
-        end)
-      ----------------------
-      -- MISC MENU --
-      ----------------------
-      elseif menu.page == "misc" then
-        for i = 1, #storeItems["Misc"] do
-          item = storeItems["Misc"][i]
-          TriggerEvent("GUI-general:Option", "(~g~$" .. item.price .. "~w~) " .. item.name, function(cb)
-            if cb then
-              --print("person wants to buy: " .. item.name)
-              buyItem(item)
-              --menu.page = "home"
-              --menu.title = "General Store"
-            end
-          end)
-        end
-        TriggerEvent("GUI-general:Option", "~y~Back", function(cb)
-          if cb then
-            menu.page = "home"
-            menu.title = "General Store"
-          end
-        end)
-        TriggerEvent("GUI-general:Option", "~y~Close", function(cb)
-          if cb then
-            menu.page = "home"
-            menu.open = false
-            menu.title = "General Store"
-          end
-        end)
-      end
-      TriggerEvent("GUI-general:Update")
-    end
-	end
-
-end)
+function IsNearStore(store)
+  local mycoords = GetEntityCoords(GetPlayerPed(-1))
+  if Vdist(store.x, store.y, store.z, mycoords.x, mycoords.y, mycoords.z) < 3.0 then
+    return true
+  else
+    return false
+  end
+end
 
 function drawTxt(text,font,centre,x,y,scale,r,g,b,a)
 	SetTextFont(font)
@@ -277,41 +85,124 @@ function drawTxt(text,font,centre,x,y,scale,r,g,b,a)
 	DrawText(x , y)
 end
 
----------------
--- MENU CODE --
----------------
-RegisterNetEvent("GUI-general:Title")
-AddEventHandler("GUI-general:Title", function(title)
-	Menu.Title(title)
+function DrawGroundMarker(location)
+  DrawMarker(27, location.x, location.y, location.z, 0, 0, 0, 0, 0, 0, 2.0, 2.0, 1.0, 240, 230, 140, 90, 0, 0, 2, 0, 0, 0, 0)
+end
+
+----------------------
+-- Set up main menu --
+----------------------
+_menuPool = NativeUI.CreatePool()
+mainMenu = NativeUI.CreateMenu("General Store", "~b~Welcome!", 0 --[[X COORD]], 320 --[[Y COORD]])
+_menuPool:Add(mainMenu)
+
+table.insert(created_menus, mainMenu)
+
+--------------------------------
+-- Construct GUI menu buttons --
+--------------------------------
+function CreateGeneralStoreMenu(menu)
+  -----------------------------------
+  -- Adds button for each category --
+  -----------------------------------
+  for category, items in pairs(GENERAL_STORE_ITEMS) do
+    local submenu = _menuPool:AddSubMenu(menu, category, "See our selection of " .. category .. " items", true --[[KEEP POSITION]])
+		table.insert(created_menus, submenu)
+    for i = 1, #items do
+      ---------------------------------------------
+      -- Button for each weapon in each category --
+      ---------------------------------------------
+      local item = NativeUI.CreateItem(items[i].name, "Purchase price: $" .. comma_value(items[i].price))
+      item.Activated = function(parentmenu, selected)
+        local playerCoords = GetEntityCoords(GetPlayerPed(-1) --[[Ped]], false)
+        TriggerEvent("properties:getPropertyGivenCoords", playerCoords.x, playerCoords.y, playerCoords.z, function(property)
+          TriggerServerEvent("generalStore:buyItem", property, items[i])
+        end)
+      end
+      ----------------------------------------
+      -- add to sub menu created previously --
+      ----------------------------------------
+      submenu:AddItem(item)
+    end
+  end
+  ----------------------------
+  -- Close menu button --
+  ----------------------------
+  local item = NativeUI.CreateItem("Close", "Close the menu.")
+  item.Activated = function(parentmenu, selected)
+    CloseAllMenus()
+  end
+  menu:AddItem(item)
+end
+
+Citizen.CreateThread(function()
+  local closest_location = nil
+  while true do
+    Wait(1)
+    _menuPool:MouseControlsEnabled(false)
+    _menuPool:ControlDisablingEnabled(false)
+    _menuPool:ProcessMenus()
+    local mycoords = GetEntityCoords(GetPlayerPed(-1))
+    -- see if close to any stores --
+    for i = 1, #GENERAL_STORE_LOCATIONS do
+      if Vdist(mycoords.x, mycoords.y, mycoords.z, GENERAL_STORE_LOCATIONS[i].x, GENERAL_STORE_LOCATIONS[i].y, GENERAL_STORE_LOCATIONS[i].z) < 50.0 then
+        DrawGroundMarker(GENERAL_STORE_LOCATIONS[i])
+        if IsNearStore(GENERAL_STORE_LOCATIONS[i]) then
+          drawTxt("Press [~y~E~w~] to open the General Store menu",7,1,0.5,0.8,0.5,255,255,255,255)
+          if IsControlJustPressed(1, MENU_KEY) and not IsAnyMenuVisible() then
+            mainMenu:Visible(not mainMenu:Visible())
+            closest_location = GENERAL_STORE_LOCATIONS[i]
+          end
+        end
+      end
+    end
+    -- close menu when far away --
+    if closest_location then
+      if not IsNearStore(closest_location) then
+        if IsAnyMenuVisible() then
+          closest_location = nil
+          CloseAllMenus()
+        end
+      end
+    end
+  end
 end)
 
-RegisterNetEvent("GUI-general:Option")
-AddEventHandler("GUI-general:Option", function(option, cb)
-	cb(Menu.Option(option))
+--------------------
+-- Spawn job peds --
+--------------------
+local JOB_PEDS = {
+  --{x = -331.043, y = 6086.09, z = 30.40, heading = 180.0}
+  {x = 1727.7, y = 6415.6, z = 35.0, heading = 180.0}
+}
+Citizen.CreateThread(function()
+	for i = 1, #JOB_PEDS do
+		local hash = -840346158
+		--local hash = GetHashKey(data.ped.model)
+		RequestModel(hash)
+		while not HasModelLoaded(hash) do
+			RequestModel(hash)
+			Citizen.Wait(0)
+		end
+		local ped = CreatePed(4, hash, JOB_PEDS[i].x, JOB_PEDS[i].y, JOB_PEDS[i].z, JOB_PEDS[i].heading --[[Heading]], false --[[Networked, set to false if you just want to be visible by the one that spawned it]], true --[[Dynamic]])
+		SetEntityCanBeDamaged(ped,false)
+		SetPedCanRagdollFromPlayerImpact(ped,false)
+		TaskSetBlockingOfNonTemporaryEvents(ped,true)
+		SetPedFleeAttributes(ped,0,0)
+		SetPedCombatAttributes(ped,17,1)
+		SetPedRandomComponentVariation(ped, true)
+    TaskStartScenarioInPlace(ped, "WORLD_HUMAN_HANG_OUT_STREET", 0, true);
+	end
 end)
 
-RegisterNetEvent("GUI-general:Bool")
-AddEventHandler("GUI-general:Bool", function(option, bool, cb)
-	Menu.Bool(option, bool, function(data)
-		cb(data)
-	end)
+RegisterNetEvent("generalStore:loadItems")
+AddEventHandler("generalStore:loadItems", function(items)
+  GENERAL_STORE_ITEMS = items
+  -----------------
+  -- Create Menu --
+  -----------------
+  CreateGeneralStoreMenu(mainMenu)
+  _menuPool:RefreshIndex()
 end)
 
-RegisterNetEvent("GUI-general:Int")
-AddEventHandler("GUI-general:Int", function(option, int, min, max, cb)
-	Menu.Int(option, int, min, max, function(data)
-		cb(data)
-	end)
-end)
-
-RegisterNetEvent("GUI-general:StringArray")
-AddEventHandler("GUI-general:StringArray", function(option, array, position, cb)
-	Menu.StringArray(option, array, position, function(data)
-		cb(data)
-	end)
-end)
-
-RegisterNetEvent("GUI-general:Update")
-AddEventHandler("GUI-general:Update", function()
-	Menu.updateSelection()
-end)
+TriggerServerEvent("generalStore:loadItems")
--- resources/generalstore/sv_generalstore.lua
+++ resources/generalstore/sv_generalstore.lua
@@ -1,11 +1,82 @@
-function round(num, numDecimalPlaces)
-  return tonumber(string.format("%." .. (numDecimalPlaces or 0) .. "f", num))
+--# by minipunch
+--# for USA REALISM rp
+--# Made for the 24/7 stores as general stores for various items
+
+local GENERAL_STORE_ITEMS = {
+  ["Food"] = {
+    {name = "Tuna Sandwich", price = 5, type = "food", substance = 40.0, quantity = 1, legality = "legal", weight = 4},
+    {name = "Chocolate Pizza", price = 15, type = "food", substance = 25.0, quantity = 1, legality = "legal", weight = 5},
+    {name = "Cheeseburger", price = 12, type = "food", substance = 55.0, quantity = 1, legality = "legal", weight = 4},
+    {name = "Kosher Hot Dog", price = 9, type = "food", substance = 45.0, quantity = 1, legality = "legal", weight = 4},
+    {name = "Microwave Burrito", price = 4, type = "food", substance = 30.0, quantity = 1, legality = "legal", weight = 4},
+    {name = "Peanut Butter Cups", price = 5, type = "food", substance = 10.0, quantity = 1, legality = "legal", weight = 1},
+    {name = "Half Dozen Donuts", price = 12, type = "food", substance = 5.0, quantity = 6, legality = "legal", weight = 2},
+    {name = "Taco", price = 1, type = "food", substance = 10.0, quantity = 1, legality = "legal", weight = 1},
+    {name = "Nachos", price = 3, type = "food", substance = 12.0, quantity = 1, legality = "legal", weight = 2},
+    {name = "Walkers Milk Chocolate Toffee", price = 15, type = "food", substance = 15.0, quantity = 1, legality = "legal", weight = 2},
+    {name = "Sea Salt & Vinegar Chips", price = 2, type = "food", substance = 7.0, quantity = 1, legality = "legal", weight = 1},
+    {name = "Cheetos", price = 2, type = "food", substance = 6.0, quantity = 1, legality = "legal", weight = 1},
+    {name = "Flaming Hot Cheetos", price = 3, type = "food", substance = 6.0, quantity = 1, legality = "legal", weight = 1},
+    {name = "Doritos", price = 3, type = "food", substance = 6.0, quantity = 1, legality = "legal", weight = 1},
+    {name = "Curly Fries", price = 6, type = "food", substance = 12.0, quantity = 1, legality = "legal", weight = 2}
+  },
+  ["Drinks"] = {
+    {name = "Water", price = 5, type = "drink", substance = 70.0, quantity = 1, legality = "legal", weight = 4},
+    {name = "Arizona Iced Tea", price = 3, type = "drink", substance = 60.0, quantity = 1, legality = "legal", weight = 1},
+    {name = "Gatorade", price = 4, type = "drink", substance = 70.0, quantity = 1, legality = "legal", weight = 2},
+    {name = "Caramel Iced Coffee", price = 1, type = "drink", substance = 20.0, quantity = 1, legality = "legal", weight = 1},
+    {name = "Mocha Iced Coffee", price = 1, type = "drink", substance = 20.0, quantity = 1, legality = "legal", weight = 1},
+    {name = "Slurpee", price = 4, type = "drink", substance = 22.0, quantity = 1, legality = "legal", weight = 2},
+    {name = "Pepsi", price = 2, type = "drink", substance = 9.0, quantity = 1, legality = "legal", weight = 1},
+    {name = "Dr. Pepper", price = 2, type = "drink", substance = 8.0, quantity = 1, legality = "legal", weight = 1},
+    {name = "Grape Soda", price = 2, type = "drink", substance = 8.0, quantity = 1, legality = "legal", weight = 1},
+    {name = "Monster Energy Drink", price = 6, type = "drink", substance = 22.0, quantity = 1, legality = "legal", weight = 2},
+    {name = "Four Loko (8%)", price = 8, type = "alcohol", substance = 6.0, quantity = 1, legality = "legal", weight = 2, strength = 0.04},
+    {name = "Corona Light Beer (4%)", price = 10, type = "alcohol", substance = 8.0, quantity = 4, legality = "legal", weight = 1, strength = 0.01},
+    {name = "Jack Daniels Whiskey (40%)", price = 35, type = "alcohol", substance = 10.0, quantity = 1, legality = "legal", weight = 4, strength = 0.20},
+    {name = "Champagne (12.5%)", price = 60, type = "alcohol", substance = 18.0, quantity = 1, legality = "legal", weight = 6, strength = 0.06},
+    {name = "Everclear Vodka (90%)", price = 35, type = "alcohol", substance = 5.0, quantity = 1, legality = "legal", weight = 4, strength = 0.45}
+  },
+  ["Vehicle"] = {
+    { name = "Repair Kit", price = 500, type = "vehicle", quantity = 1, legality = "legal", weight = 8}
+  },
+  ["Electronics"] = {
+    { name = "Cell Phone", price = 900, type = "misc", quantity = 1, legality = "legal", contacts = {}, conversations = {}, weight = 1}
+  },
+  ["Misc"] = {
+    { name = "First Aid Kit", price = 100, type = "misc", quantity = 1, legality = "legal", weight = 5},
+    { name = "Large Scissors", price = 40, type = "misc", quantity = 1, legality = "legal", weight = 5},
+    { name = "Sturdy Rope", price = 10, type = "misc", quantity = 1, legality = "legal", weight = 4},
+    { name = "Bag", price = 70, type = "misc", quantity = 1, legality = "legal", weight = 3},
+    { name = "The Daily Weazel", price = 2, type = "misc", quantity = 1, legality = "legal", weight = 1},
+    { name = "Condoms", price = 3, type = "misc", quantity = 3, legality = "legal", weight = 1},
+    { name = "KY Intense Gel", price = 15, type = "misc", quantity = 1, legality = "legal", weight = 1},
+    { name = "Viagra", price = 100, type = "misc", quantity = 10, legality = "legal", weight = 1},
+    { name = "Tent", price = 100, type = "misc", quantity = 1, legality = "legal", weight = 20},
+    { name = "Wood", price = 10, type = "misc", quantity = 1, legality = "legal", weight = 30},
+    { name = "Chair", price = 25, type = "misc", quantity = 1, legality = "legal", weight = 20},
+    { name = "Vortex Optics Binoculars", price = 350, type = "misc", quantity = 1, legality = "legal", weight = 2},
+    { name = "Toolkit", price = 100, type = "misc", quantity = 1, legality = "legal", weight = 20}
+  }
+}
+
+function GetServerPrice(item)
+  for category, items in pairs(GENERAL_STORE_ITEMS) do
+    for i = 1, #items do
+      if items[i].name == item.name then
+        return items[i].price
+      end
+    end
+  end
 end
 
 RegisterServerEvent("generalStore:buyItem")
-AddEventHandler("generalStore:buyItem", function(item, property)
+AddEventHandler("generalStore:buyItem", function(property, item)
   local userSource = source
-  TriggerEvent('es:getPlayerFromId', userSource, function(user)
+  item.price = GetServerPrice(item)
+  --local item = GENERAL_STORE_ITEMS[category][index]
+  --TriggerEvent('es:getPlayerFromId', userSource, function(user)
+  local user = exports["essentialmode"]:getPlayerFromId(userSource)
     if user.getCanActiveCharacterHoldItem(item) then
       local user_money = user.getActiveCharacterData("money")
       if user_money >= item.price then
@@ -20,8 +91,10 @@ AddEventHandler("generalStore:buyItem", function(item, property)
                 TriggerClientEvent("usa:notify", userSource, "Purchased: ~y~" .. item.name)
                 if property then
                   -- give to owner of property
-                  print("adding money from general store to property: " .. property.name)
-                  TriggerEvent("properties:addMoney", property.name, round(0.12 * item.price, 0))
+                  --# by minipunch
+                  --# for USA REALISM rp
+                  --# Made for the 24/7 stores as general stores for various items("adding money from general store to property: " .. property.name)
+                  TriggerEvent("properties:addMoney", property.name, math.ceil(0.40 * item.price))
                 end
                 return
               end
@@ -34,7 +107,7 @@ AddEventHandler("generalStore:buyItem", function(item, property)
           if property then
             -- give to owner of property
             print("adding money from general store to property: " .. property.name)
-            TriggerEvent("properties:addMoney", property.name, round(0.40 * item.price, 0))
+            TriggerEvent("properties:addMoney", property.name, math.ceil(0.40 * item.price))
           end
         else
           --Generate number
@@ -51,7 +124,7 @@ AddEventHandler("generalStore:buyItem", function(item, property)
           if property then
             -- give to owner of property
             print("adding money from general store to property: " .. property.name)
-            TriggerEvent("properties:addMoney", property.name, round(0.12 * item.price, 0))
+            TriggerEvent("properties:addMoney", property.name, math.ceil(0.40 * item.price))
           end
         end
       else
@@ -61,10 +134,11 @@ AddEventHandler("generalStore:buyItem", function(item, property)
     else
       TriggerClientEvent("usa:notify", userSource, "Inventory full.")
     end
-  end)
+  --end)
 end)
 
-function round(num, numDecimalPlaces)
-	local mult = 5^(numDecimalPlaces or 0)
-	return math.floor(num * mult + 0.5) / mult
-end
+RegisterServerEvent("generalStore:loadItems")
+AddEventHandler("generalStore:loadItems", function()
+  print("**loading general store items!**")
+  TriggerClientEvent("generalStore:loadItems", source, GENERAL_STORE_ITEMS)
+end)
