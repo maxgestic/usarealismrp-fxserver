e37f43df4f3be1104c8d5830674fdcbaeacc4217
minipunch <joshyolles@gmail.com>
2018-10-21 12:06:42 -0700

    Updated auto repair blips, added spot

--- resources/CarFixGarage/garage.lua
+++ resources/CarFixGarage/garage.lua
@@ -21,17 +21,22 @@ vehicleRepairStation = {
 	--{-222.407,-1329.69,30.89},
 	{480.805,-1317.43,29.2},
 	{447.534, -997.033, 24.3635}, -- los santos police department on atlee & sinner st
-	{201.339, -1656.36, 27.2832} -- los santos fire department in davis
+	{201.339, -1656.36, 27.2832}, -- los santos fire department in davis
+	{1146.7, -775.8, 56.79}
 }
 
 blips = {
-	--{1150.1766,  -774.4962,  57.1689},
 	{536.1182,  -178.5338,  53.08846},
+	{2006.354,  3798.739,  30.83191},
+	--{128.6394,  6620.741,  30.43497}, -- OG paleto spot v1
+	{103.479,  6622.048,  30.8}, -- OG paleto spot v2
+	--{1150.1766,  -774.4962,  57.1689},
+	--{103.513,  6622.77,  30.1868}, -- paleto
 	--{-222.407,-1329.69,30.89},
-	{480.805,-1317.43,29.2}
+	{480.805,-1317.43,29.2},
+	{1146.7, -775.8, 56.79}
 }
 
---[[
 Citizen.CreateThread(function ()
 	Citizen.Wait(0)
 	for i = 1, #blips do
@@ -39,10 +44,13 @@ Citizen.CreateThread(function ()
 		stationBlip = AddBlipForCoord(garageCoords[1], garageCoords[2], garageCoords[3])
 		SetBlipSprite(stationBlip, 446) --446 = Tools
 		SetBlipAsShortRange(stationBlip, true)
+		SetBlipScale(stationBlip, 0.7)
+		BeginTextCommandSetBlipName("STRING")
+		AddTextComponentString("Auto Repair")
+		EndTextCommandSetBlipName(stationBlip)
 	end
 	return
 end)
---]]
 
 function DrawCoolLookingNotification(msg)
     SetNotificationTextEntry("STRING")
