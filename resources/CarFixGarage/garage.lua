c617da9f1bb551895fefb409af9b013bf3d17c75
minipunch <joshyolles@gmail.com>
2018-11-07 11:03:25 -0800

    add car fix garage to LS dealership

--- resources/CarFixGarage/garage.lua
+++ resources/CarFixGarage/garage.lua
@@ -22,7 +22,8 @@ vehicleRepairStation = {
 	{480.805,-1317.43,29.2},
 	{447.534, -997.033, 24.3635}, -- los santos police department on atlee & sinner st
 	{201.339, -1656.36, 27.2832}, -- los santos fire department in davis
-	{1146.7, -775.8, 56.79}
+	{1146.7, -775.8, 56.79},
+	{-35.4, -1088.7, 26.4}
 }
 
 blips = {
