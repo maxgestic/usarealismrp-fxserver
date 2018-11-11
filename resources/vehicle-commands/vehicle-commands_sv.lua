d047c40c627c48d46a60bbc4ee72bd50c43e70cf
minipunch <joshyolles@gmail.com>
2018-11-10 18:43:57 -0800

    Add police5 to spawn list

--- resources/vehicle-commands/vehicle-commands_sv.lua
+++ resources/vehicle-commands/vehicle-commands_sv.lua
@@ -32,11 +32,11 @@ local spawnableVehicles = {
 }
 
 local VEH_RANKS = {
-	[1] = {"police", "police2", "police3", "scorcher", "police7", "predator", "pbus", "policet", "sheriff", "sheriff2"},
-	[2] = {"police", "police2", "police3", "scorcher", "police7", "policeb", "police6", "unmarked7", "predator", "pbus", "policet", "sheriff", "sheriff2"},
-	[3] = {"police", "police2", "police3", "scorcher", "police7", "policeb", "police6", "unmarked7", "predator", "polmav", "pbus", "policet", "sheriff", "sheriff2", "fbi2"},
-	[4] = {"police", "police2", "police3", "scorcher", "police7", "policeb", "police6", "unmarked1", "unmarked7", "unmarked8", "fbi", "fbi2", "police4", "sheriff", "predator", "polmav", "pbus", "policet", "riot", "sheriff", "sheriff2"},
-	[5] = {"police", "police2", "police3", "scorcher", "police7", "policeb", "police6", "unmarked1", "unmarked6", "unmarked7", "unmarked8", "fbi", "fbi2", "police4", "sheriff", "unmarked3", "unmarked9", "predator", "polmav", "pbus", "policet", "riot", "sheriff", "sheriff2"},
+	[1] = {"police", "police2", "police3", "police5", "scorcher", "police7", "predator", "pbus", "policet", "sheriff", "sheriff2"},
+	[2] = {"police", "police2", "police3", "police5", "scorcher", "police7", "policeb", "police6", "unmarked7", "predator", "pbus", "policet", "sheriff", "sheriff2"},
+	[3] = {"police", "police2", "police3", "police5", "scorcher", "police7", "policeb", "police6", "unmarked7", "predator", "polmav", "pbus", "policet", "sheriff", "sheriff2", "fbi2"},
+	[4] = {"police", "police2", "police3", "police5", "scorcher", "police7", "policeb", "police6", "unmarked1", "unmarked7", "unmarked8", "fbi", "fbi2", "police4", "sheriff", "predator", "polmav", "pbus", "policet", "riot", "sheriff", "sheriff2"},
+	[5] = {"police", "police2", "police3", "police5", "scorcher", "police7", "policeb", "police6", "unmarked1", "unmarked6", "unmarked7", "unmarked8", "fbi", "fbi2", "police4", "sheriff", "unmarked3", "unmarked9", "predator", "polmav", "pbus", "policet", "riot", "sheriff", "sheriff2"},
 	[6] = {'policeb','sheriff','sheriff2','sheriff3','pbus','policet','police','police2','police3','police4','police5','police6','police7','police8','fbi','fbi2',	'riot','polmav','scorcher','predator','chpcvpi','unmarked1','unmarked3','unmarked6','unmarked7','unmarked8','unmarked9', "sheriff", "sheriff2"},
 	[7] = {'policeb','sheriff','sheriff2','sheriff3','pbus','policet','police','police2','police3','police4','police5','police6','police7','police8','fbi','fbi2',	'riot','polmav','scorcher','predator','chpcvpi','unmarked1','unmarked3', 'unmarked4', 'unmarked6','unmarked7','unmarked8','unmarked9', "sheriff", "sheriff2"},
 	[8] = {'policeb','sheriff','sheriff2','sheriff3','pbus','policet','police','police2','police3','police4','police5','police6','police7','police8','fbi','fbi2',	'riot','polmav','scorcher','predator','chpcvpi','unmarked1','unmarked3', 'unmarked4', 'unmarked6','unmarked7','unmarked8','unmarked9', "sheriff", "sheriff2"},
