1dca7b7804c6cf3689f3f6edbda73d810fc0ad06
minipunch <joshyolles@gmail.com>
2018-12-11 15:53:53 -0800

    Increase prices for black market items, update /info

--- resources/blackmarket/cl_blackmarket.lua
+++ resources/blackmarket/cl_blackmarket.lua
@@ -5,24 +5,24 @@ locations = {
     { x= -2166.786, y = 5197.684, z = 15.880} -- island north of map by paleto
 }
 
-local storeItems = {
+local sv_storeItems = {
     ["weapons"] = {
 		    { name = "Lock Pick", type = "misc", hash = 615608432, price = 1000, legality = "illegal", quantity = 1, weight = 5 },
         { name = "Molotov", type = "weapon", hash = 615608432, price = 650, legality = "illegal", quantity = 1, weight = 20 },
         { name = "Brass Knuckles", type = "weapon", hash = -656458692, price = 650, legality = "illegal", quantity = 1, weight = 5 },
-        { name = "Dagger", type = "weapon", hash = -1834847097, price = 750, legality = "illegal", quantity = 1, weight = 10 },
-        { name = "Switchblade", type = "weapon", hash = -538741184, price = 3000, legality = "illegal", quantity = 1, weight = 10 },
-        { name = "AP Pistol", type = "weapon", hash = 0x22D8FE39, price = 14550, legality = "illegal", quantity = 1, weight = 15 },
-        { name = "Sawn-off", type = "weapon", hash = 0x7846A318, price = 9000, legality = "illegal", quantity = 1, weight = 30 },
-        { name = "Micro SMG", type = "weapon", hash = 324215364, price = 12000, legality = "illegal", quantity = 1, weight = 30 },
-        { name = "SMG", type = "weapon", hash = 736523883, price = 16700, legality = "illegal", quantity = 1, weight = 45 },
-        { name = "Machine Pistol", type = "weapon", hash = -619010992, price = 15500, legality = "illegal", quantity = 1, weight = 20 },
-        { name = "Tommy Gun", type = "weapon", hash = 1627465347, price = 18750, legality = "illegal", quantity = 1, weight = 45 },
-        { name = "AK47", type = "weapon", hash = -1074790547, price = 23500, legality = "illegal", quantity = 1, weight = 45 },
-        { name = "Carbine", type = "weapon", hash = -2084633992, price = 24500, legality = "illegal", quantity = 1, weight = 45 },
+        { name = "Dagger", type = "weapon", hash = -1834847097, price = 1250, legality = "illegal", quantity = 1, weight = 10 },
+        { name = "Switchblade", type = "weapon", hash = -538741184, price = 5000, legality = "illegal", quantity = 1, weight = 10 },
+        { name = "AP Pistol", type = "weapon", hash = 0x22D8FE39, price = 38550, legality = "illegal", quantity = 1, weight = 15 },
+        { name = "Sawn-off", type = "weapon", hash = 0x7846A318, price = 25000, legality = "illegal", quantity = 1, weight = 30 },
+        { name = "Micro SMG", type = "weapon", hash = 324215364, price = 50000, legality = "illegal", quantity = 1, weight = 30 },
+        { name = "SMG", type = "weapon", hash = 736523883, price = 50700, legality = "illegal", quantity = 1, weight = 45 },
+        { name = "Machine Pistol", type = "weapon", hash = -619010992, price = 45500, legality = "illegal", quantity = 1, weight = 20 },
+        { name = "Tommy Gun", type = "weapon", hash = 1627465347, price = 95750, legality = "illegal", quantity = 1, weight = 45 },
+        { name = "AK47", type = "weapon", hash = -1074790547, price = 120500, legality = "illegal", quantity = 1, weight = 45 },
+        { name = "Carbine", type = "weapon", hash = -2084633992, price = 120500, legality = "illegal", quantity = 1, weight = 45 },
         --{ name = "Compact Rifle", type = "weapon", hash = 1649403952, price = 19550, legality = "illegal", quantity = 1, weight = 55 },
         --{ name = "MK2 Assault Rifle", type = "weapon", hash = 961495388, price = 19550, legality = "illegal", quantity = 1, weight = 45 },
-        { name = "Bullpup Rifle", type = "weapon", hash = 2132975508, price = 30000, legality = "illegal", quantity = 1, weight = 45 },
+        { name = "Bullpup Rifle", type = "weapon", hash = 2132975508, price = 150000, legality = "illegal", quantity = 1, weight = 45 },
         --{ name = "Advanced Rifle", type = "weapon", hash = -1357824103, price = 20550, legality = "illegal", quantity = 1, weight = 45 },
         --{ name = "Assault SMG", type = "weapon", hash = -270015777, price = 40500, legality = "illegal", quantity = 1, weight = 45 }
         --{ name = "MK2 Carbine Rifle", type = "weapon", hash = 4208062921, price = 20550, legality = "illegal", quantity = 1, weight = 45 }
