b88b01469082bcc1cc99884e6338939705ee04f7
minipunch <joshyolles@gmail.com>
2019-07-01 15:18:08 -0700

    Adjust /spawn and pdmenu for new vehicles

--- resources/[usa_overhaul]/usa_pdmenu/sv_menu.lua
+++ resources/[usa_overhaul]/usa_pdmenu/sv_menu.lua
@@ -1,40 +1,47 @@
+local VEHICLE_RANKS = {
+	["sheriff"] = {
+		["pdcvpi"] = 1,
+		["pdtau"] = 2,
+		["pdchrg"] = 2,
+		["pdexp"] = 2,
+		["pdtahoe"] = 2,
+		["pdchrgum"] = 4
+	},
+	["ems"] = {
+		["ambulance"] = 1,
+		["pdexp"] = 1,
+		["pranger"] = 2,
+		["firetruk"] = 1,
+		["polmav"] = 1
+	},
+	["dai"] = {
+		["pdchrgum"] = 1
+	}
+}
+
 RegisterServerEvent("pdmenu:checkWhitelist")
 AddEventHandler("pdmenu:checkWhitelist", function(clientevent)
   local char = exports["usa-characters"]:GetCharacter(source)
   local user_job = char.get("job")
   if user_job == "sheriff" or user_job == "cop" or user_job == "dai" then
-    TriggerClientEvent(clientevent, source)
+      TriggerClientEvent(clientevent, source)
   else
     TriggerClientEvent("usa:notify", source, "~y~You are not on-duty for POLICE.")
   end
 end)
 
-
----------- GARAGE MENU POLICE VEHICLES  ------------
-
-local VEH_RANKS = {
-	[1] = {'taurus16b', 'chrg18b', 'chrg14b', 'cvpi11b', 'bison19', 'sheriff2', 'scorcher'},
-	[2] = {'taurus16b', 'chrg18b', 'chrg14b', 'cvpi11b', 'bison19', 'sheriff2','scorcher', 'policet', 'tahoe'},
-	[3] = {'taurus16b', 'chrg14a', 'chrg14b', 'chrg18a', 'chrg18b', 'cvpi11a', 'cvpi11b', 'bison19', 'scorcher', 'policet', 'riot', 'policeb'},
-	[4] = {'taurus16b', 'chrg14a', 'chrg14b', 'chrg18a', 'chrg18b', 'cvpi11a', 'cvpi11b', 'bison19', 'scorcher', 'policet', 'riot', 'policeb', 'fbi', 'fbi2', 'schafter19', 'buffalo19', 'baller19', 'interceptor19', 'oracle19'},
-	[5] = {'taurus16b', 'chrg14a', 'chrg14b', 'chrg18a', 'chrg18b', 'cvpi11a', 'cvpi11b', 'bison19', 'scorcher', 'policet', 'riot', 'policeb', 'fbi', 'fbi2', 'schafter19', 'buffalo19', 'baller19', 'interceptor19', 'oracle19'},
-	[6] = {'taurus16b', 'chrg14a', 'chrg14b', 'chrg18a', 'chrg18b', 'cvpi11a', 'cvpi11b', 'bison19', 'scorcher', 'policet', 'riot', 'policeb', 'fbi', 'fbi2', 'schafter19', 'buffalo19', 'baller19', 'interceptor19', 'oracle19'},
-	[7] = {'taurus16b', 'chrg14a', 'chrg14b', 'chrg18a', 'chrg18b', 'cvpi11a', 'cvpi11b', 'bison19', 'scorcher', 'policet', 'riot', 'policeb', 'fbi', 'fbi2', 'schafter19', 'buffalo19', 'baller19', 'interceptor19', 'oracle19'},
-	[8] = {'taurus16b', 'chrg14a', 'chrg14b', 'chrg18a', 'chrg18b', 'cvpi11a', 'cvpi11b', 'bison19', 'scorcher', 'policet', 'riot', 'policeb', 'fbi', 'fbi2', 'schafter19', 'buffalo19', 'baller19', 'interceptor19', 'oracle19'},
-	[9] = {'taurus16b', 'chrg14a', 'chrg14b', 'chrg18a', 'chrg18b', 'cvpi11a', 'cvpi11b', 'bison19', 'scorcher', 'policet', 'riot', 'policeb', 'fbi', 'fbi2', 'schafter19', 'buffalo19', 'baller19', 'interceptor19', 'oracle19'},
-	[10] = {'taurus16b', 'chrg14a', 'chrg14b', 'chrg18a', 'chrg18b', 'cvpi11a', 'cvpi11b', 'bison19', 'scorcher', 'policet', 'riot', 'policeb', 'fbi', 'fbi2', 'schafter19', 'buffalo19', 'baller19', 'interceptor19', 'oracle19'},
-	['dai'] = {'schafter19', 'buffalo19', 'baller19', 'oracle19', 'taurus16a', 'chrg14a', 'chrg18a', 'cvpi11a'}
-}
-
 RegisterServerEvent('pdmenu:returnAllowedVehicles')
 AddEventHandler('pdmenu:returnAllowedVehicles', function()
 	local char = exports["usa-characters"]:GetCharacter(source)
 	local user_job = char.get("job")
-	if user_job == "dai" then
-		TriggerClientEvent('pdmenu:sendAllowedVehicles', source, VEH_RANKS['dai'])
-	else
-		local user_rank = tonumber(char.get("policeRank"))
-		local allowedVehicles = VEH_RANKS[user_rank]
-		TriggerClientEvent('pdmenu:sendAllowedVehicles', source, allowedVehicles)
-	end
+    local vehs = GetAllowedVehicles(user_job)
+	TriggerClientEvent('pdmenu:sendAllowedVehicles', source, vehs)
 end)
+
+function GetAllowedVehicles(job)
+    local vehs = {}
+    for veh, rank in pairs(VEHICLE_RANKS[job]) do
+        table.insert(vehs, veh)
+    end
+    return vehs
+end
--- resources/[usa_overhaul]/usa_vehcommands/vehicle-commands_sv.lua
+++ resources/[usa_overhaul]/usa_vehcommands/vehicle-commands_sv.lua
@@ -1,55 +1,74 @@
-local spawnableVehicles = {
-	['ems'] = {'maverick2', 'ambulance', 'firetruk'},
-	['sheriff'] = {'chrg14a', 'chrg14b', 'chrg18', 'cvpi11a', 'cvpi11b', 'bison19', 'sheriff2', 'scorcher', 'pbus', 'policet', 'tahoe', 'riot', 'policeb', 'maverick2', 'predator', 'fbi', 'fbi2', 'fbi3', 'maverick2'},
-	['dai'] = {'schafter19', 'buffalo19', 'baller19', 'oracle19', 'taurus16a', 'chrg14a', 'chrg18a', 'cvpi11a'}
-}
-
-local VEH_RANKS = {
-	[1] = {'taurus16b', 'chrg18b', 'chrg14b', 'cvpi11b', 'bison19', 'sheriff2', 'scorcher'},
-	[2] = {'taurus16b', 'chrg18b', 'chrg14b', 'cvpi11b', 'bison19', 'sheriff2','scorcher', 'policet', 'tahoe'},
-	[3] = {'taurus16b', 'chrg14a', 'chrg14b', 'chrg18a', 'chrg18b', 'cvpi11a', 'cvpi11b', 'bison19', 'scorcher', 'policet', 'riot', 'policeb', 'maverick2'},
-	[4] = {'taurus16b', 'chrg14a', 'chrg14b', 'chrg18a', 'chrg18b', 'cvpi11a', 'cvpi11b', 'bison19', 'scorcher', 'policet', 'riot', 'policeb', 'fbi', 'fbi2', 'schafter19', 'buffalo19', 'baller19', 'interceptor19', 'oracle19', 'maverick2'},
-	[5] = {'taurus16b', 'chrg14a', 'chrg14b', 'chrg18a', 'chrg18b', 'cvpi11a', 'cvpi11b', 'bison19', 'scorcher', 'policet', 'riot', 'policeb', 'fbi', 'fbi2', 'schafter19', 'buffalo19', 'baller19', 'interceptor19', 'oracle19', 'maverick2'},
-	[6] = {'taurus16b', 'chrg14a', 'chrg14b', 'chrg18a', 'chrg18b', 'cvpi11a', 'cvpi11b', 'bison19', 'scorcher', 'policet', 'riot', 'policeb', 'fbi', 'fbi2', 'schafter19', 'buffalo19', 'baller19', 'interceptor19', 'oracle19', 'maverick2'},
-	[7] = {'taurus16b', 'chrg14a', 'chrg14b', 'chrg18a', 'chrg18b', 'cvpi11a', 'cvpi11b', 'bison19', 'scorcher', 'policet', 'riot', 'policeb', 'fbi', 'fbi2', 'schafter19', 'buffalo19', 'baller19', 'interceptor19', 'oracle19', 'maverick2'},
-	[8] = {'taurus16b', 'chrg14a', 'chrg14b', 'chrg18a', 'chrg18b', 'cvpi11a', 'cvpi11b', 'bison19', 'scorcher', 'policet', 'riot', 'policeb', 'fbi', 'fbi2', 'schafter19', 'buffalo19', 'baller19', 'interceptor19', 'oracle19', 'maverick2'},
-	[9] = {'taurus16b', 'chrg14a', 'chrg14b', 'chrg18a', 'chrg18b', 'cvpi11a', 'cvpi11b', 'bison19', 'scorcher', 'policet', 'riot', 'policeb', 'fbi', 'fbi2', 'schafter19', 'buffalo19', 'baller19', 'interceptor19', 'oracle19', 'maverick2'},
-	[10] = {'taurus16b', 'chrg14a', 'chrg14b', 'chrg18a', 'chrg18b', 'cvpi11a', 'cvpi11b', 'bison19', 'scorcher', 'policet', 'riot', 'policeb', 'fbi', 'fbi2', 'schafter19', 'buffalo19', 'baller19', 'interceptor19', 'oracle19', 'maverick2'},
+local VEHICLE_RANKS = {
+	["sheriff"] = {
+		["pdcvpi"] = 1,
+		["pdtau"] = 2,
+		["pdchrg"] = 2,
+		["pdexp"] = 2,
+		["pdtahoe"] = 2,
+		["pdchrgum"] = 4
+	},
+	["ems"] = {
+		["ambulance"] = 1,
+		["paraexp"] = 1,
+		["pranger"] = 2,
+		["firetruk"] = 1,
+		["polmav"] = 1
+	},
+	["dai"] = {
+		"pdchrgum"
+	}
 }
 
 TriggerEvent('es:addJobCommand', 'spawn', { "police", "sheriff", "ems", "fire", "dai" }, function(source, args, char)
-		local vehicleRequested = args[2]
-		local job = char.get('job')
+	local job = char.get('job')
+	if not args[2] then
+		local rank = 0
 		if job == "sheriff" then
-			local user_police_rank = tonumber(char.get("policeRank"))
-			if vehicleRequested ~= nil and vehicleRequested ~= '' and IsVehicleSpawnable(vehicleRequested) then
-				if IsHighEnoughRank(vehicleRequested, user_police_rank) then
-					TriggerClientEvent("vehicleCommands:spawnVehicle", source, vehicleRequested, job)
-					return
-				else
-					TriggerClientEvent("usa:notify", "You are not a high enough rank for that vehicle option.")
-					return
-				end
-			else
-				DisplaySpawnOptionsBasedOnRank(source, user_police_rank)
-			end
+			rank = char.get("policeRank")
 		elseif job == "ems" then
-			for _, vehicle in pairs(spawnableVehicles[job]) do
-				if vehicle == vehicleRequested then
-					TriggerClientEvent("vehicleCommands:spawnVehicle", source, vehicleRequested, job)
-					return
-				end
-			end
-			TriggerClientEvent("chatMessage", source, "", {0,0,0}, "^3^*[SPAWN] ^r^0Options: maverick2, ambulance, firetruk")
+			rank = char.get("emsRank")
 		elseif job == "dai" then
-			for _, vehicle in pairs(spawnableVehicles[job]) do
-				if vehicle == vehicleRequested then
-					TriggerClientEvent("vehicleCommands:spawnVehicle", source, vehicleRequested, job)
-					return
-				end
-			end
-			TriggerClientEvent("chatMessage", source, "", {0,0,0}, "^3^*[SPAWN] ^r^0Options: schafter19, buffalo19, baller19", "oracle19", "taurus16a", "chrg14a", "chrg18a", "cvpi11a")
+			rank = char.get("daiRank")
 		end
+		DisplaySpawnOptionsBasedOnRank(source, job, rank)
+		return
+	end
+	local vehicleRequested = string.lower(args[2])
+	if job == "sheriff" then
+		local rank = char.get("policeRank")
+		local neededRank = VEHICLE_RANKS[job][vehicleRequested]
+		if  neededRank then
+			if rank >= neededRank then
+				TriggerClientEvent("vehicleCommands:spawnVehicle", source, vehicleRequested, job)
+				return
+			else
+				TriggerClientEvent("usa:notify", "You are not a high enough rank for that vehicle option.")
+				return
+			end
+		else
+			DisplaySpawnOptionsBasedOnRank(source, job, rank)
+		end
+	elseif job == "ems" then
+		local rank = char.get("emsRank")
+		local neededRank = VEHICLE_RANKS[job][vehicleRequested]
+		if neededRank then
+			if rank >= neededRank then
+				TriggerClientEvent("vehicleCommands:spawnVehicle", source, vehicleRequested, job)
+				return
+			end
+		end
+		DisplaySpawnOptionsBasedOnRank(source, job, rank)
+	elseif job == "dai" then
+		local rank = char.get("emsRank")
+		local neededRank = VEHICLE_RANKS[job][vehicleRequested]
+		if neededRank then
+			if rank >= neededRank then
+				TriggerClientEvent("vehicleCommands:spawnVehicle", source, vehicleRequested, job)
+				return
+			end
+		end
+		DisplaySpawnOptionsBasedOnRank(source, job, rank)
+	end
 end, {
 	help = "Spawn an exclusive emergency vehicle.",
 	params = {
@@ -83,40 +102,12 @@ end, {
 	}
 })
 
-function IsHighEnoughRank(model, user_rank)
-	for rank, vehicles in pairs(VEH_RANKS) do
-		if user_rank >= rank then
-			for k = 1, #vehicles do
-				if model == vehicles[k] then
-					return true
-				end
-			end
+function DisplaySpawnOptionsBasedOnRank(src, job, rank)
+	local msg = ""
+	for veh, neededRank in pairs(VEHICLE_RANKS[job]) do
+		if rank >= neededRank then
+			msg = msg .. veh .. ", "
 		end
 	end
-	return false
-end
-
-function IsVehicleSpawnable(modelName)
-	for i = 1, #spawnableVehicles["sheriff"] do
-		if spawnableVehicles["sheriff"][i] == modelName then
-			return true
-		end
-	end
-	return false
-end
-
-function DisplaySpawnOptionsBasedOnRank(source, user_rank)
-	local string = ""
-	for rank, vehicles in pairs(VEH_RANKS) do
-		if rank == user_rank then
-			for k = 1, #vehicles do
-				if k == 1 then
-					string = vehicles[k]
-				end
-				string = string .. ", " .. vehicles[k]
-			end
-			break
-		end
-	end
-	TriggerClientEvent("chatMessage", source, "", {0,0,0}, "^3^*[SPAWN] ^r^0Options: " .. string)
+	TriggerClientEvent("chatMessage", src, "", {0,0,0}, "^3SPAWN OPTIONS:^0 " .. msg )
 end
