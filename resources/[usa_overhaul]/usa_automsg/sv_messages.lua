f332e2008e6e4dff8ab29ceb7c35ecdb48636a14
minipunch <joshyolles@gmail.com>
2019-07-07 13:39:53 -0700

    automsg: remove join delay

--- resources/[usa_overhaul]/usa_automsg/sv_messages.lua
+++ resources/[usa_overhaul]/usa_automsg/sv_messages.lua
@@ -1,21 +1,3 @@
-------------------------------------------
--- Server join delay after start up --
-------------------------------------------
-local SECONDS_TO_DELAY = 120
-local ABLE_TO_JOIN = false
-
-Citizen.CreateThread(function()
-  Wait(SECONDS_TO_DELAY * 1000)
-  ABLE_TO_JOIN = true
-end)
-
-AddEventHandler('playerConnecting', function(name, setReason)
-  if not ABLE_TO_JOIN then
-    setReason("Please wait " .. SECONDS_TO_DELAY .. " seconds before joining.")
-    CancelEvent()
-  end
-end)
-
 local CHECK_TIME = 10000 -- every 10 secs
 ------------------------------
 -- Send restart message --
@@ -27,7 +9,7 @@ Citizen.CreateThread(function()
         if date.hour == 3 and date.min == 20 then
             TriggerClientEvent('restart:notify', -1, 10)
             print('SERVER: Restarting in 10 minutes!')
-        elseif date.hour == 3 and date.min == 29 then
+        elseif date.hour == 3 and date.min == 28 then
             for i = 1, 4 do
                 TriggerClientEvent('chatMessage', -1, '^8^*[SERVER] ^rServer is restarting, disconnect now or risk data loss!')
                 print("SERVER: Restarting!")
