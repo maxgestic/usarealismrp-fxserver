480a109da808ce69d5b5f443c31a4ddaebf73c08
minipunch <joshyolles@gmail.com>
2019-07-15 10:37:20 -0700

    Fix jail bug

---
+++ resources/[usa_overhaul]/tokovoip_script/s_config.lua
@@ -0,0 +1,10 @@
+TokoVoipConfig = {
+	channels = {
+		{name = "TEST", subscribers = {}},
+		{name = "Call PD Radio", subscribers = {}},
+		{name = "EMS Radio", subscribers = {}},
+		{name = "PD/SO/EMS Shared Radio", subscribers = {}},
+		{name = "SO Radio", subscribers = {}},
+		{name = "PD/SO Shared Radio", subscribers = {}}
+	}
+};
