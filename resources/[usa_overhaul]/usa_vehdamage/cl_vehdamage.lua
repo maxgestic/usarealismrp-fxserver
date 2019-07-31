6f7f42b91f3d73056c9f103acadf1c38dca11a74
minipunch <joshyolles@gmail.com>
2019-07-30 17:57:52 -0700

    vehdamage: reduce tire burst chance

--- resources/[usa_overhaul]/usa_vehdamage/cl_vehdamage.lua
+++ resources/[usa_overhaul]/usa_vehdamage/cl_vehdamage.lua
@@ -67,13 +67,11 @@ Citizen.CreateThread(function()
                if damagedEngine < 0 then damagedEngine = -4000.0 end
                if GetPedInVehicleSeat(car, -1) == ped then
                 SetVehicleEngineHealth(car, damagedEngine)
-                if math.random() > 0.2 then
+                if math.random() > 0.7 then
                     local tyreBursted = math.random(1, 2)
                     SetVehicleTyreBurst(car, tyreBursted, true, 1000.0)
-                    print('tyre: '..tyreBursted)
                 end
                end
-               print('Engine: '..damagedEngine)
                if not beltOn and speedBuffer[1] > MinSpeed then
                     ShakeStrength = GetEntitySpeed(car)/15
 
