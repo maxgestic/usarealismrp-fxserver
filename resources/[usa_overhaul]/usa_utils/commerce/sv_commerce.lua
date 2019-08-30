c58e34105321b83af3c9b52adaf3ec8ec7a2938c
minipunch <joshyolles@gmail.com>
2019-08-29 21:14:16 -0700

    commerce: completed initial UI, need to wire up functionality for the two items

--- resources/[usa_overhaul]/usa_utils/commerce/sv_commerce.lua
+++ resources/[usa_overhaul]/usa_utils/commerce/sv_commerce.lua
@@ -1,10 +1,22 @@
+TriggerEvent("es:addCommand", "store", function(source, char, args, location)
+    if CanPlayerStartCommerceSession(source) then 
+        TriggerClientEvent("commerce:openStore", source)
+    else 
+        TriggerClientEvent("usa:notify", source, "Please sign into your FiveM account!", "^3USARRP STORE: ^0Please sign into your FiveM account! You can do this right after FiveM starts (see the top) or by clicking 'Settings' at the top.")
+    end
+end, {
+	help = "Open the in-game market place!"
+})
+
 TriggerEvent("es:addCommand", "commerce", function(source, char, args, location)
     if CanPlayerStartCommerceSession(source) then 
         TriggerClientEvent("commerce:openStore", source)
     else 
-        TriggerClientEvent("usa:notify", source, "Please sign into your FiveM account!", "^0Please sign into your FiveM account! You can do this right after FiveM starts (see the top) or by clicking 'Settings' at the top.")
+        TriggerClientEvent("usa:notify", source, "Please sign into your FiveM account!", "^3USARRP STORE: ^0Please sign into your FiveM account! You can do this right after FiveM starts (see the top) or by clicking 'Settings' at the top.")
     end
-end)
+end, {
+	help = "Open the in-game market place!"
+})
 
 RegisterServerEvent("commerce:purchase")
 AddEventHandler("commerce:purchase", function(id)
@@ -17,9 +29,10 @@ AddEventHandler("commerce:purchase", function(id)
         if not DoesPlayerOwnSku(usource, id) then
             print("COMMERCE: starting commerce session for id " .. id .. ", type is " .. type(id))
             RequestPlayerCommerceSession(usource, id)
+            TriggerClientEvent("usa:notify", usource, "A new web browser tab should open after selecting \"Yes\", check your web browser!", "^3USARRP STORE: ^0A new web browser tab should open after selecting \"Yes\", check your web browser!")
         else 
             print("COMMERCE: user already had SKU " .. id .. "!")
-            TriggerClientEvent("usa:notify", usource, "You already own that item!", "^0You already own that item!")
+            TriggerClientEvent("usa:notify", usource, "You already own that item!", "^3USARRP STORE: ^0You already own that item!")
         end
     end)
 end)
--- resources/[usa_overhaul]/usa_utils/commerce/ui/js/components/CHome.js
+++ resources/[usa_overhaul]/usa_utils/commerce/ui/js/components/CHome.js
@@ -1,28 +1,42 @@
 const CHome = Vue.component("CHome", {
     template: 
     `
-    <div id="background" class="w3-main w3-content w3-padding w3-light-grey">
+    <div id="background" class="w3-main w3-content w3-padding">
         <div id="logo-container"><img src="https://fontmeme.com/permalink/190204/49bb075d378719bbd20b0040eef07af3.png" alt="USARRP"></div>
-        <div class="items-container w3-row">
-            <div class="item w3-half w3-container">
-                <div class="item-content">
-                    <p>ITEM NAME HERE</p>
-                    <img src="" alt="">
-                    <p>Description of the item here</p>
-                    <button>BUY NOW</button>
+        <section class="grid-container">
+            <div v-for="item in items" class="item w3-card">
+                <p>{{item.name}}</p>
+                <img v-bind:src="item.img" v-bind:alt="item.name" height="160px">
+                <p class="item-price"><span>€</span>{{item.price}}</p>
+                <div style="height: 70px; align-content: center;">
+                    <p class="item-description">{{item.description}}</p>
                 </div>
+                <button class="w3-btn w3-green" @click="purchase(item.sku)">Buy Now</button>
             </div>
-            <div class="item w3-half w3-container">
-                <div class="item-content">
-                    <p>ITEM NAME HERE</p>
-                    <img src="" alt="">
-                    <p>Description of the item here</p>
-                    <button>BUY NOW</button>
-                </div>
-            </div>
-        </div>
+        </section>
     </div>
     `,
+    data: function() {
+        return {
+            items: [
+                {
+                    name: "Unlimited Screenshots",
+                    img: "https://www.rawshorts.com/freeicons/wp-content/uploads/2017/01/media-pict-camera.png",
+                    description: "One time purchase for unlimited access to the /screenshot command! Use it whenever, forever!",
+                    price: 5,
+                    sku: 15
+                },
+                {
+                    name: "LS Customs Coupons (25% off)",
+                    img: "http://vaultguides.com/wp-content/uploads/2015/10/gta-5-online-original-motorworks.jpg",
+                    description: "Want to upgrade your cars? Get a coupon for 25% off your entire purchase at ANY LS Customs or Benny's Garage in San Andreas!",
+                    price: 15,
+                    sku: 16
+                }
+                
+            ]
+        }
+    },
     methods: {
         purchase: function(id) {
             console.log("attempting purchase of item: " + id);
