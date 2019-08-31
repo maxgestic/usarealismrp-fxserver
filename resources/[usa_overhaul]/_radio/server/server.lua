13bf66df860c276ba87f8db1e436c9de49690108
minipunch <joshyolles@gmail.com>
2019-08-30 17:09:10 -0700

    Let doctors use radio (actually)

--- resources/[usa_overhaul]/_radio/server/server.lua
+++ resources/[usa_overhaul]/_radio/server/server.lua
@@ -27,7 +27,7 @@ RegisterServerEvent("radio:accessCheck")
 AddEventHandler("radio:accessCheck", function()
     local char = exports["usa-characters"]:GetCharacter(source)
     local cjob = char.get("job")
-    local hasNotAllowedJob = (cjob ~= "sheriff" and cjob ~= "corrections" and cjob ~= "ems" and cjob ~= "tow")
+    local hasNotAllowedJob = (cjob ~= "sheriff" and cjob ~= "corrections" and cjob ~= "ems" and cjob ~= "tow" and cjob ~= "doctor")
     if not CAN_CIVS_USE and hasNotAllowedJob then
         TriggerClientEvent("usa:notify", source, "Only for public servants")
         return
