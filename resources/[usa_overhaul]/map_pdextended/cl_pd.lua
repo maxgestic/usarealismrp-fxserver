35e2664ca32f835e20b21b9282b45eecd5d2731e
minipunch <joshyolles@gmail.com>
2019-09-10 20:37:26 -0700

    Added new MRPD interior

--- resources/[usa_overhaul]/map_pdextended/cl_pd.lua
+++
@@ -1,7 +0,0 @@
-print('continuing...')
-Citizen.CreateThread(function() -- remove the peds which spawn at PD
-    while true do
-        ClearAreaOfPeds(469.85, -989.9, 24.91, 30, 0)
-        Wait(1000)
-    end
-end)
