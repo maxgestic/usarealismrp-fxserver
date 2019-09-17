b1b6f6b0d65af35dd8b1e9f7522425f1b27ead95
minipunch <joshyolles@gmail.com>
2019-09-17 10:13:16 -0700

    append /givecash fixes

--- resources/[usa_overhaul]/usa_hud/server.lua
+++ resources/[usa_overhaul]/usa_hud/server.lua
@@ -16,7 +16,9 @@ end, {
 TriggerEvent('es:addCommand','cash', function(source, args, char)
 	TriggerEvent('display:shareDisplayBySource', source, 'counts cash', 2, 370, 10, 3000, true)
 	Citizen.Wait(200)
-	TriggerClientEvent("es:setMoneyDisplay", source, 1, char.get("money"))
+	local amount = char.get("money")
+	if amount == 0 then amount = "0" end
+	TriggerClientEvent("es:setMoneyDisplay", source, 1, amount)
 	Citizen.Wait(3000)
 	TriggerClientEvent("es:setMoneyDisplay", source, 0)
 end, {
