1dc756357076161812dea53baaa9711708ba13d4
minipunch <joshyolles@gmail.com>
2019-10-03 17:30:59 -0700

    chopshop: able to take any car in for reward

--- resources/[usa_overhaul]/usa_chopshop/cl_chopshop.lua
+++ resources/[usa_overhaul]/usa_chopshop/cl_chopshop.lua
@@ -4,8 +4,6 @@
 
 local KEY = 38 -- "E"
 
-local JOB_TIME = 3600000 -- in ms (one hour)
-
 local peds = {
   {x = 2346.036, y = 3045.159, z = 47.16, hash = 846439045, heading = 180.0},
   {x = -470.84, y = -1708.76, z = 17.91, hash = 846439045, heading = 270.0}
@@ -16,32 +14,7 @@ local drop_offs = {
   {x = -465.21, y = -1718.34, z = 18.68}
 }
 
-local current_job = {
-  active = false,
-  vehicles = {},
-  start_time = 0
-}
-
-local chop_time = 30000 -- in ms
-
---------------------
--- Event Handlers --
---------------------
-RegisterNetEvent("chopshop:startJob")
-AddEventHandler("chopshop:startJob", function(wanted_vehicles)
-  if not current_job.active then
-    current_job.active = true
-    current_job.vehicles = wanted_vehicles
-    current_job.start_time = GetGameTimer()
-  end
-  TriggerEvent("chatMessage", "", { 0, 0, 0 }, "^3^*[CHOP SHOP] ^r^7Please bring me these models  (don\'t get caught):")
-  local msg = ""
-  for i = 1, #wanted_vehicles do
-    --print("wanted: " .. wanted_vehicles[i].name)
-    TriggerEvent("chatMessage", "", { 0, 0, 0 }, "^0⠀• " .. ConvertRealCarToGtaCar(wanted_vehicles[i].name))
-    msg = msg .. ConvertRealCarToGtaCar(wanted_vehicles[i].name) .. "\n"
-  end
-end)
+local chop_time = 60000 -- in ms
 
 -----------------------------------------------
 -- Watches for entering the chopping circle  --
@@ -50,60 +23,49 @@ Citizen.CreateThread(function()
   local handle = 0
   local display_name = "Undefined"
   while true do
-    Citizen.Wait(0)
-    if current_job.active then
-      local playerPed = PlayerPedId()
-      local playerCoords = GetEntityCoords(playerPed)
-      for i = 1, #drop_offs do
-        DrawText3D(drop_offs[i].x, drop_offs[i].y, drop_offs[i].z, 10, '[E] - Drop-off Vehicle')
-        if IsControlJustPressed(1,KEY) and Vdist(playerCoords, drop_offs[i].x, drop_offs[i].y, drop_offs[i].z) < 3.0 then
-          local found = false
+    Wait(0)
+    local playerPed = PlayerPedId()
+    local playerCoords = GetEntityCoords(playerPed)
+    for i = 1, #drop_offs do
+      DrawText3D(drop_offs[i].x, drop_offs[i].y, drop_offs[i].z, 10, '[E] - Drop-off Vehicle')
+      if IsControlJustPressed(1,KEY) and Vdist(playerCoords, drop_offs[i].x, drop_offs[i].y, drop_offs[i].z) < 3.0 then
+        if IsPedInAnyVehicle(playerPed, true) then
           local handle = GetVehiclePedIsIn(playerPed, true)
           local display_name = string.lower(GetDisplayNameFromVehicleModel(GetEntityModel(handle)))
-          for x = 1, #current_job.vehicles do
-            if display_name == string.lower(current_job.vehicles[x].name) then
-              found = true
-              if math.random() > 0.8 then
-                local x, y, z = table.unpack(playerCoords)
-                local lastStreetHASH = GetStreetNameAtCoord(x, y, z)
-                local lastStreetNAME = GetStreetNameFromHashKey(lastStreetHASH)
-                TriggerServerEvent('911:ChopShop', x, y, z, lastStreetNAME, IsPedMale(playerPed))
-              end
-              OpenAllDoors(handle)
-              TaskLeaveVehicle(playerPed, handle, 256)
-              SetVehicleDoorsLockedForAllPlayers(handle, true)
-              SetVehicleDoorsLocked(handle, true)
-              TriggerEvent('usa:notify', 'Wait here while the vehicle is being chopped up.')
-              local chopped = true
-              local beginTime = GetGameTimer()
-              while GetGameTimer() - beginTime < chop_time do
-                playerCoords = GetEntityCoords(playerPed)
-                if Vdist(playerCoords, drop_offs[i].x, drop_offs[i].y, drop_offs[i].z) < 10.0 then
-                  DrawTimer(beginTime, chop_time, 1.42, 1.475, 'CHOPPING')
-                  Citizen.Wait(0)
-                else
-                  TriggerEvent('usa:notify', 'You went too far away while the vehicle was being chopped!')
-                  chopped = false
-                  break
-                end
-              end
-              if chopped then
-                RemoveFromWantedVehicles(display_name)
-                NotifyOfUpdatedWantedVehicles()
-    					  TriggerServerEvent("chopshop:reward", display_name, GetVehicleBodyDamage(handle))
-                SetEntityAsMissionEntity(handle, true, true)
-                DeleteCar(handle)
-                if #current_job.vehicles <= 0 then
-                  ResetJob()
-                end
-                break
-              end
+          if math.random() < 0.20 then
+            local x, y, z = table.unpack(playerCoords)
+            local lastStreetHASH = GetStreetNameAtCoord(x, y, z)
+            local lastStreetNAME = GetStreetNameFromHashKey(lastStreetHASH)
+            TriggerServerEvent('911:ChopShop', x, y, z, lastStreetNAME, IsPedMale(playerPed))
+          end
+          OpenAllDoors(handle)
+          TaskLeaveVehicle(playerPed, handle, 256)
+          SetVehicleDoorsLockedForAllPlayers(handle, true)
+          SetVehicleDoorsLocked(handle, true)
+          TriggerEvent('usa:notify', 'Wait here while the vehicle is being chopped up.')
+          local chopped = true
+          local beginTime = GetGameTimer()
+          while GetGameTimer() - beginTime < chop_time do
+            playerCoords = GetEntityCoords(playerPed)
+            if Vdist(playerCoords, drop_offs[i].x, drop_offs[i].y, drop_offs[i].z) < 15.0 then
+              DrawTimer(beginTime, chop_time, 1.42, 1.475, 'CHOPPING')
+              Citizen.Wait(0)
+            else
+              TriggerEvent('usa:notify', 'You went too far away while the vehicle was being chopped!')
+              chopped = false
+              SetVehicleDoorsLockedForAllPlayers(handle, false)
+              SetVehicleDoorsLocked(handle, false)
+              break
             end
           end
-          if not found then
-            TriggerEvent("usa:notify", "I didn't ask for this vehicle!")
+          if chopped then
+            TriggerServerEvent("chopshop:reward", display_name, GetVehicleBodyDamage(handle))
+            SetEntityAsMissionEntity(handle, true, true)
+            DeleteCar(handle)
+            break
           end
-          found = false
+        else 
+          exports.globals:notify("Must be in a vehicle!")
         end
       end
     end
@@ -116,32 +78,15 @@ end)
 Citizen.CreateThread(function()
   addBlips()
   while true do
-    Citizen.Wait(0)
-    if not current_job.active then
-      local playerPed = PlayerPedId()
-      local playerCoords = GetEntityCoords(playerPed)
-      for i = 1, #peds do
-        DrawText3D(peds[i].x, peds[i].y, peds[i].z + 1.0, 5, '[E] - Chop Shop')
-        if IsControlJustPressed(1, KEY) and Vdist(playerCoords, peds[i].x, peds[i].y, peds[i].z) < 3.0 then
-            TriggerServerEvent("chopshop:startJob")
-        end
+    Wait(0)
+    local playerPed = PlayerPedId()
+    local playerCoords = GetEntityCoords(playerPed)
+    for i = 1, #peds do
+      DrawText3D(peds[i].x, peds[i].y, peds[i].z + 1.0, 5, '[E] - Chop Shop')
+      if IsControlJustPressed(1, KEY) and Vdist(playerCoords, peds[i].x, peds[i].y, peds[i].z) < 3.0 then
+          TriggerEvent("usa:notify", "Bring me some vehicles!", "^3Pedro:^0 You can bring me any vehicle and we can hook you up with some cash!")
       end
     end
-  end
-end)
-
--------------------------------------
--- Watch for job timer expiration  --
--------------------------------------
-Citizen.CreateThread(function()
-  while true do
-    if current_job.active then
-      if GetGameTimer() >= current_job.start_time + JOB_TIME then
-        ResetJob()
-        TriggerEvent("usa:notify", "You took too long to bring me the vehicles I wanted! Hit me up next time.")
-      end
-    end
-    Wait(1000)
   end
 end)
 
@@ -219,37 +164,6 @@ function GetVehicleBodyDamage(veh)
   return damage
 end
 
-function ResetJob()
-  TriggerServerEvent('chopshop:resetJob')
-  current_job.active = false
-  current_job.vehicles = {}
-  current_job.start_time = 0
-end
-
-function NotifyOfUpdatedWantedVehicles()
-  if #current_job.vehicles > 0 then
-    local msg = "Vehicles still needed:\n"
-    for k = 1, #current_job.vehicles do
-      --print("wanted: " .. wanted_vehicles[i].name)
-      msg = msg .. ConvertRealCarToGtaCar(current_job.vehicles[k].name) .. "\n"
-    end
-    TriggerEvent("usa:notify", msg)
-  else
-    TriggerEvent("usa:notify", "Thanks! Let me know if you can help some more!")
-  end
-end
-
-function RemoveFromWantedVehicles(name)
-  if current_job.vehicles then
-    for i = 1, #current_job.vehicles do
-      if string.lower(current_job.vehicles[i].name) == string.lower(name) then
-        table.remove(current_job.vehicles, i)
-        return
-      end
-    end
-  end
-end
-
 function DeleteCar(entity)
   Citizen.InvokeNative( 0xEA386986E786A54F, Citizen.PointerValueIntInitialized( entity ) )
 end
@@ -323,8 +237,3 @@ function DrawTimer(beginTime, duration, x, y, text)
     Set_2dLayer(3)
     DrawText(x - 0.06, y - 0.012)
 end
-
-RegisterNetEvent('character:setCharacter')
-AddEventHandler('character:setCharacter', function()
-    ResetJob()
-end)
--- resources/[usa_overhaul]/usa_chopshop/sv_chopshop.lua
+++ resources/[usa_overhaul]/usa_chopshop/sv_chopshop.lua
@@ -1,80 +1,13 @@
-local VEHICLES = {
-  --{name = "ORACLE", reward = 5000},
-  {name = "ORACLE2", reward = 600},
-  {name = "FLATBED", reward = 700},
-  {name = "SADLER", reward = 700},
-  {name = "EMPEROR", reward = 500},
-  {name = "BFINJECT", reward = 450},
-  {name = "SANCHEZ02", reward = 500},
-  {name = "REBEL02", reward = 600},
-  {name = "SEMINOLE", reward = 550},
-  {name = "BOBCATXL", reward = 600},
-  {name = "BUFFALO", reward = 700},
-  {name = "RUINER", reward = 550},
-  {name = "BLISTA2", reward = 600},
-  {name = "TORNADO3", reward = 450},
-  {name = "SABREGT", reward = 700},
-  {name = "FUGITIVE", reward = 700},
-  {name = "DUBSTA", reward = 600},
-  {name = "PEYOTE", reward = 550},
-  {name = "SANDKING", reward = 600}
-}
-
-local vehiclesWanted = {}
-
-RegisterServerEvent("chopshop:startJob")
-AddEventHandler("chopshop:startJob", function()
-  for player, vehicles in pairs(vehiclesWanted) do
-    if player == source then
-      return
-    end
-  end
-  local wanted_vehicles = {}
-  for i = 1, 5 do
-    local random_vehicle = VEHICLES[math.random(#VEHICLES)]
-    table.insert(wanted_vehicles, random_vehicle)
-    print("inserted: " .. random_vehicle.name)
-  end
-  vehiclesWanted[source] = wanted_vehicles
-  TriggerClientEvent("chopshop:startJob", source, wanted_vehicles)
-end)
-
 RegisterServerEvent("chopshop:reward")
 AddEventHandler("chopshop:reward", function(veh_name, damage, property)
   local char = exports["usa-characters"]:GetCharacter(source)
-  for player, vehicles in pairs(vehiclesWanted) do
-    if player == source then
-      for i = #vehicles, 1, -1 do
-        if string.lower(vehicles[i].name) == string.lower(veh_name) then
-          local reward = GetRewardFromName(veh_name)
-          if (reward - damage) >= 0 then
-            char.giveMoney((reward - damage))
-            print('CHOPSHOP: Player '..GetPlayerName(source)..'['..GetPlayerIdentifier(source)..'] has received money['..reward..'] minus damage['..damage..'] after chopping vehicle!')
-            TriggerClientEvent("usa:notify", source, "~y~Reward:~w~ $" .. (reward - damage) .. "\nThere was $" .. damage .. " in damages.")
-          else
-            TriggerClientEvent("usa:notify", source, "This vehicle is too damaged. I am not giving you any money for this!")
-          end
-          table.remove(vehiclesWanted[source], i)
-        end
-      end
-    end
+  local reward = math.random(200, 1500)
+  local numTroopers = exports["usa-characters"]:GetNumCharactersWithJob("sheriff")
+  if numTroopers == 0 then
+    damage = math.ceil(0.10 * damage)
+    reward = math.ceil(0.05 * reward) -- only give 20% of regular reward if no cops
   end
-end)
-
-RegisterServerEvent('chopshop:resetJob')
-AddEventHandler('chopshop:resetJob', function()
-  vehiclesWanted[source] = nil
-end)
-
-function GetRewardFromName(name)
-  for i = 1, #VEHICLES do
-    if string.lower(name) == string.lower(VEHICLES[i].name) then
-      return VEHICLES[i].reward
-    end
-  end
-  return 0
-end
-
-AddEventHandler('playerDropped', function()
-  vehiclesWanted[source] = nil
+  char.giveMoney(reward - damage)
+  print("damage was: " .. damage)
+  TriggerClientEvent("usa:notify", source, "Thanks! Here is $" .. reward - damage .. "!", "^0Thanks! Here is $" .. reward - damage .. "!")
 end)
