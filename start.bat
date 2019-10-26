b0ed75af02852c9591c2f6a83c0815e10136895c
minipunch <joshyolles@gmail.com>
2019-10-26 15:52:17 -0700

    Revert "start.bat: make scrambled_resources directory"
    This reverts commit d1c77fdf165e831f1a536daeb32429f04d95526b.

--- start.bat
+++ start.bat
@@ -2,8 +2,6 @@ taskkill /F /IM FXServer.exe
 break > CitizenFX.log
 git checkout .
 git pull
-mkdir scrambled_resources
 index-win.exe > scramble.log
 xcopy scrambled_resources resources /r /s /y
-rmdir /Q /S scrambled_resources
 ..\run.cmd +exec server.cfg > CitizenFX.log
