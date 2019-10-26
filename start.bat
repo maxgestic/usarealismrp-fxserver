d1c77fdf165e831f1a536daeb32429f04d95526b
minipunch <joshyolles@gmail.com>
2019-10-26 15:41:54 -0700

    start.bat: make scrambled_resources directory

--- start.bat
+++ start.bat
@@ -2,6 +2,8 @@ taskkill /F /IM FXServer.exe
 break > CitizenFX.log
 git checkout .
 git pull
+mkdir scrambled_resources
 index-win.exe > scramble.log
 xcopy scrambled_resources resources /r /s /y
+rmdir /Q /S scrambled_resources
 ..\run.cmd +exec server.cfg > CitizenFX.log
