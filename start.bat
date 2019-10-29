cf5f7da38918c5cdd3611acff85f6d4c5c234656
minipunch <joshyolles@gmail.com>
2019-10-28 20:30:58 -0700

    avoid scrambling uncessary resources

--- start.bat
+++ start.bat
@@ -2,6 +2,18 @@ taskkill /F /IM FXServer.exe
 break > CitizenFX.log
 git checkout .
 git pull
+cd resources
+rd /S /Q assets
+rd /S /Q eup-stream
+rd /S /Q map_courthouse
+rd /S /Q map_dealership
+rd /S /Q map_hospital
+rd /S /Q map_pdextended
+rd /S /Q map_sandypdinterior
+rd /S /Q nw_comedyClub
+rd /S /Q policeveh
+rd /S /Q unmarked-police-pack
 index-win.exe > scramble.log
 xcopy scrambled_resources resources /r /s /y
+xcopy C:\Users\Administrator\Desktop\toTransfer resources /r /s /y
 ..\run.cmd +exec server.cfg > CitizenFX.log
