88965021a863092c52eb03ac16bbb8ced7c034cc
minipunch <joshyolles@gmail.com>
2019-10-28 20:33:32 -0700

    start.bat: add missed cd ..

--- start.bat
+++ start.bat
@@ -13,6 +13,7 @@ rd /S /Q map_sandypdinterior
 rd /S /Q nw_comedyClub
 rd /S /Q policeveh
 rd /S /Q unmarked-police-pack
+cd ..
 index-win.exe > scramble.log
 xcopy scrambled_resources resources /r /s /y
 xcopy C:\Users\Administrator\Desktop\toTransfer resources /r /s /y
