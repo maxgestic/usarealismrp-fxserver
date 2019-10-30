8bf6f87d6ca1140c089e58765ef7144d54b370c4
minipunch <joshyolles@gmail.com>
2019-10-30 10:57:04 -0700

    drugEffects: don't use globbing in __resource.lua to make scrambler happy

--- resources/usa_drugEffects/__resource.lua
+++ resources/usa_drugEffects/__resource.lua
@@ -2,10 +2,10 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 client_scripts {
     "cl_*.lua",
-    "**/cl_*.lua"
+    "cannabis/cl_cannabis.lua"
 }
 
 server_scripts {
     "sv_*.lua",
-    "**/sv_*.lua"
+    "cannabis/sv_cannabis.lua"
 }
