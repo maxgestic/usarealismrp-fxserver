d779eabfb60427551ef49a23a028dd0a287f7d25
minipunch <joshyolles@gmail.com>
2019-12-10 11:24:13 -0800

    robnpc: prevent robbing with snowballs

--- resources/robnpc/cl_robnpc.lua
+++ resources/robnpc/cl_robnpc.lua
@@ -12,13 +12,13 @@ local CALL_DELAY = 7000 -- 7s
 local previousPed = nil
 local targetPed = nil
 
-local weapons = { "WEAPON_PISTOL", "WEAPON_KNIFE" }
-
-
 local emote = {
     dict = "random@mugging3",
     name = "handsup_standing_base"
 }
+
+local prohibitedWeapons = {}
+prohibitedWeapons[`WEAPON_SNOWBALL`] = true
 
 function ShowHelp(text, bleep)
     BeginTextCommandDisplayHelp("STRING")
@@ -26,33 +26,38 @@ function ShowHelp(text, bleep)
     EndTextCommandDisplayHelp(0, false, bleep, -1)
 end
 
+function canRobOtherPed(me, targetPed)
+    -- valid other ped? --
+    if not DoesEntityExist(targetPed) or not IsEntityAPed(targetPed) or IsPedInAnyVehicle(targetPed, true) or IsPedDeadOrDying(targetPed, true) or IsPedAPlayer(targetPed) then
+        return false
+    end
+    
+    -- is player robbing with a valid weapon? --
+    local selectedPedWeapon = GetSelectedPedWeapon(me)
+    if prohibitedWeapons[selectedPedWeapon] then
+        return false
+    else 
+        return true
+    end
+end
+
 Citizen.CreateThread(function()
     while true do
         ::top::
         if IsControlJustPressed(0, KEYS.E) and IsPedArmed(GetPlayerPed(-1), 7) then
+            local me = PlayerPedId()
             aiming, targetPed = GetEntityPlayerIsFreeAimingAt(PlayerId(-1))
             if targetPed ~= previousPed and targetPed then
-                local PID = GetPlayerPed(-1)
-                local p = GetEntityCoords(PID, true)
-                local t = GetEntityCoords(targetPed, true)
-                local canContinue = DoesEntityExist(targetPed) and IsEntityAPed(targetPed) and not IsPedInAnyVehicle(targetPed, true) and not IsPedDeadOrDying(targetPed, true) and not IsPedAPlayer(targetPed)
-                if canContinue and not robbing and isValidPedModel(targetPed) then
-                    if GetDistanceBetweenCoords(p.x, p.y, p.z, t.x, t.y, t.z, true) < DISTANCE_FROM_PED then
+                local mycoords = GetEntityCoords(me, true)
+                local otherPedCoords = GetEntityCoords(targetPed, true)
+                if canRobOtherPed(me, targetPed) and not robbing and isValidPedModel(targetPed) then
+                    if GetDistanceBetweenCoords(mycoords.x, mycoords.y, mycoords.z, otherPedCoords.x, otherPedCoords.y, otherPedCoords.z, true) < DISTANCE_FROM_PED then
                         local fleeChance = math.random()
-                        --local attackChance = math.random()
                         previousPed = targetPed
                         if fleeChance <= 0.15 then
-                            TaskReactAndFleePed(targetPed, PID)
+                            TaskReactAndFleePed(targetPed, me)
                             goto top
                         end
-                        --To come back to at a later date.  Fighting script.
-                        --[[if attackChance >= 0.15 then
-                            print("fighting")
-                            SetPedFleeAttributes(targetPed, 46, true)
-                            GiveWeaponToPed(targetPed, GetHashKey("WEAPON_KNIFE"), 1, false, true)
-                            TaskCombatPed(targetPed, PID, 0, 16)
-                            goto top
-                        end--]]
                         ClearPedTasksImmediately(targetPed)
                         FreezeEntityPosition(targetPed, true)
                         robbing = true
@@ -71,10 +76,8 @@ Citizen.CreateThread(function()
                         StopAnimTask(targetPed, emote.dict, emote.name, 1.0)
                         ClearPedTasksImmediately(targetPed)
                         TaskSetBlockingOfNonTemporaryEvents(targetPed, false)
-                        TaskReactAndFleePed(targetPed, PID)
-                        local playerPed = PlayerPedId()
-                        local playerCoords = GetEntityCoords(playerPed)
-                        local x, y, z = table.unpack(playerCoords)
+                        TaskReactAndFleePed(targetPed, me)
+                        local x, y, z = table.unpack(mycoords)
                         local lastStreetHASH = GetStreetNameAtCoord(x, y, z)
                         local lastStreetNAME = GetStreetNameFromHashKey(lastStreetHASH)
                         TriggerServerEvent("911:MuggingNPC", x, y, z, lastStreetNAME)
