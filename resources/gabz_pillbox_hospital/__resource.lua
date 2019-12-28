3abcfb072ab5d3600e996879cede9c0b9fb1a8a3
minipunch <joshyolles@gmail.com>
2019-12-27 23:40:23 -0800

    pillbox: experimental fix(?) for player flickering

--- resources/gabz_pillbox_hospital/__resource.lua
+++ resources/gabz_pillbox_hospital/__resource.lua
@@ -2,9 +2,13 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 this_is_a_map 'yes'
 
-file 'gabz_timecycle_mods_1.xml'
+files {
+    'gabz_timecycle_mods_1.xml',
+    'interiorproxies.meta'
+}
 data_file 'TIMECYCLEMOD_FILE' 'gabz_timecycle_mods_1.xml'
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
 
-client_script {
+client_scripts {
     "main.lua"
 }
