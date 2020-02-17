162a20f077c25057c4c52f0de5656ac783cc9670
goryholes <60585938+goryholes@users.noreply.github.com>
2020-02-17 18:59:57 +0000

    Fix headpopping and increase lock distance (#340)
    * Fix headpopping and increase lock distance
    
    * mrpd: small adjustments
    
    Co-authored-by: minipunch

--- resources/map_mrpd/client/main.lua
+++ resources/map_mrpd/client/main.lua
@@ -2,10 +2,28 @@ Citizen.CreateThread(function()
 	LoadInterior(GetInteriorAtCoords(440.84, -983.14, 30.69))
 end)
 
+--Citizen.CreateThread(function()
+--    while (true) do
+--        Citizen.Wait(1)
+--
+--        ClearAreaOfPeds(468.2, -984.65, 24.91, 600, 1)
+--        Citizen.Wait(0)
+--    end
+--end)
+
+
 Citizen.CreateThread(function()
-    while (true) do
-        ClearAreaOfPeds(440.84, -983.14, 30.69, 300, 1)
-        ClearAreaOfPeds(467.96, -988.84, 24.91, 317.6, 1)
-        Citizen.Wait(0)
+    while true do
+        local myCoords = myCoords
+        if GetDistanceBetweenCoords(myCoords, 975.76763916016,-120.56567382813,74.223541259766, true ) < 80 then
+            ClearAreaOfPeds(975.76763916016,-120.56567382813,74.223541259766, 58.0, 0)
+        elseif GetDistanceBetweenCoords(myCoords, 471.90618896484,-989.65802001953,24.914854049683, true ) < 20 then
+            ClearAreaOfPeds(471.90618896484,-989.65802001953,24.914854049683, 20.0, 0)
+        elseif GetDistanceBetweenCoords(myCoords, 243.72465515137,-1096.3624267578,29.30615234375, true ) < 30 then
+            ClearAreaOfPeds(243.72465515137,-1096.3624267578,29.30615234375, 20.0, 0)
+        elseif GetDistanceBetweenCoords(myCoords, 445.8, -981.62, 26.67, true ) < 30 then
+            ClearAreaOfPeds(445.8, -981.62, 26.67, 20.0, 0)
+        end
+        Wait(1)
     end
 end)
