82ffbf603f3135bbe4b041c8f2c36e1b220bc517
Goryholes <aaronjake0191@gmail.com>
2020-03-08 21:09:24 +0000

    Place all construction works into 1 resource (disregard the others)

---
+++ resources/CustomDesigns/__resource.lua
@@ -0,0 +1,19 @@
+--ch6_03_radio.ytd - Aces Rebel Radio
+--ch6_03_radio_sta_dec+hidr.ytd - Aces Rebel Radio
+
+--sc1_24_res.ytd - Swedes House paint change
+-- prop_flag_uk.yft - Swedes Flag (changed to sweden)
+-- Swede Flag.ymap - Swede Custom map changes via codewalker
+
+-- vb_33_detail.ytd - Crew (VA graffiti)
+
+-- hei_hw1_16_stm_0.ymap - Jose Guzman more doors
+-- hw1_16_build02.ydr - Jose Guzman more doors
+-- hw1_16_build02b_dcl2_sc.ydr - Jose Guzman more doors
+-- hw1_16_strm_0.ymap - Jose Guzman more doors (codewalker)
+
+-- Galaxy_FightClub.ymap - Fight Club
+
+-- johnson.ymap - Scott Johnson helipad
+-- ch2_roadsa_critical_1.ymap - Scott Johnson helipad
+-- ch2_roadsa_long_0.ymap - Scott Johnson helipad
