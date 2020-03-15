c70e4829a73a3ce2603da5cbb1a941759abb2db2
Josh Yolles <joshyolles@gmail.com>
2020-03-14 19:15:38 -0700

    Custom designs (#353)
    * Place all construction works into 1 resource (disregard the others)
    
    * Coords added to resource file and player / crew names beside each file for ease of determining what does what and for whom
    
    * rename folder, add to server.cfg
    
    * Gloryholes Construction - Blood Banners
    
    * Jet Ski race course
    
    * Add coords to resorces file

--- resources/map_customDesigns/__resource.lua
+++ resources/map_customDesigns/__resource.lua
@@ -22,3 +22,10 @@ this_is_a_map 'yes'
 
 -- Galaxy_FightClub.ymap - Fight Club
 -- Fight Club COORDS: 260.17, -269.47, 60.55
+
+-- sc1_17_cloth.ytd - Bloods banners
+-- BLOOD BANNER COORDS: -164.24, -1697.29, 31.77
+
+
+--jet_ski.ymap - JET SKI EVENT
+-- JET SKI COORDS - -265.92, 783.41, 200.62
