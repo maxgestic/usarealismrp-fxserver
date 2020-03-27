6eada75fa1b005a44bb3a2584021bbf118a6214d
minipunch <joshyolles@gmail.com>
2020-03-27 00:32:05 -0700

    start.bat: add map_glory

--- start.bat
+++ start.bat
@@ -21,6 +21,7 @@ rd /S /Q map_sandypdinterior
 rd /S /Q gabz_pillbox_hospital
 rd /S /Q map_burgershot
 rd /S /Q map_customDesigns
+rd /S /Q map_glory
 rd /S /Q map_mosleys
 rd /S /Q map_luxury-autos
 rd /S /Q paletopd
