063c3e29e7b0aadfaa7908e7ee3cad50edf90587
Goryholes <aaronjake0191@gmail.com>
2020-04-08 20:57:33 +0100

    missed files oopsie

---
+++ resources/usa_realestate/client/client.lua
@@ -0,0 +1,52 @@
+local SignIn = "[E] - Sign In"
+local SignOut = "[E] - Sign Out"
+local SigninCoords = {-148.55, -579.82, 48.24}
+local working = "no"
+
+Citizen.CreateThread(function()
+    while true do
+        local x,y,z = table.unpack(SigninCoords)
+        if nearMarker(x,y,z) then
+            promptJob(SigninCoords)
+        end
+        Wait(0)
+    end
+end)
+
+function nearMarker(x, y, z)
+    local mycoords = GetEntityCoords(GetPlayerPed(-1))
+    return GetDistanceBetweenCoords(x, y, z, mycoords.x, mycoords.y, mycoords.z, true) < 3
+end
+
+function promptJob(location)
+    local x,y,z = table.unpack(location)
+    if working == 'no' then
+        DrawText3D(x,y,z, 8, SignIn)
+        if IsControlJustPressed(0, 38) then
+            TriggerServerEvent('realty:duty')
+            working = 'yes'
+        end
+    else
+        DrawText3D(x,y,z, 8, SignOut)
+        if IsControlJustPressed(0, 38) then
+            TriggerServerEvent('realty:duty')
+            working = 'no'
+        end
+    end
+end
+
+function DrawText3D(x, y, z, distance, text)
+    if Vdist(GetEntityCoords(PlayerPedId()), x, y, z) < distance then
+        local onScreen,_x,_y=World3dToScreen2d(x,y,z)
+        SetTextScale(0.35, 0.35)
+        SetTextFont(4)
+        SetTextProportional(1)
+        SetTextColour(255, 255, 255, 215)
+        SetTextEntry("STRING")
+        SetTextCentre(1)
+        AddTextComponentString(text)
+        DrawText(_x,_y)
+        local factor = (string.len(text)) / 470
+        DrawRect(_x,_y+0.0125, 0.015+factor, 0.03, 41, 11, 41, 68)
+    end
+end
