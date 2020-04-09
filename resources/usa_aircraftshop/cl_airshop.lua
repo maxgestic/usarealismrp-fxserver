0ce383394a8294717ca71818f1ea2c8ab7db99f5
minipunch <joshyolles@gmail.com>
2020-04-08 19:14:57 -0700

    remove comment

--- resources/usa_aircraftshop/cl_airshop.lua
+++ resources/usa_aircraftshop/cl_airshop.lua
@@ -220,7 +220,7 @@ Citizen.CreateThread(function()
             local shopDist = GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), data.menu.x, data.menu.y, data.menu.z, true)
 			if data.private and shopDist < 70 then
 				DrawText3D(data.menu.x, data.menu.y, data.menu.z, 2, '[E] - Private Hanger')
-				if IsControlPressed(0, KEYS.E) then -- holding E
+				if IsControlPressed(0, KEYS.E) then
 					TriggerServerEvent('aircraft:requestOpenPrivateMenu')
 				end
 			elseif shopDist < 70 then
