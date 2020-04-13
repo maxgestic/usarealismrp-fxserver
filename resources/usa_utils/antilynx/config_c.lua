a1630c9b51ba89071123491023366322e059a333
Goryholes <aaronjake0191@gmail.com>
2020-04-13 21:20:26 +0100

    final commits

--- resources/usa_utils/antilynx/config_c.lua
+++ resources/usa_utils/antilynx/config_c.lua
@@ -194,10 +194,10 @@ LXC.ABWeaponsKick = false -- if the player get detected, kick him (NOT RECOMMEND
 
 ---------------Blacklisted Models-------------------------
 
-LXC.AVD = true -- This bool deletes automatically blacklisted vehicles (may cause lag!! use as your risk)
+LXC.AVD = false -- This bool deletes automatically blacklisted vehicles (may cause lag!! use as your risk)
 LXC.AED = true -- This bool deletes automatically blacklisted entities (may cause lag!! use as your risk)
 LXC.APD = true -- This bool deletes automatically blacklisted peds (may cause lag!! use as your risk)
-LXC.AutomaticDeleteTimeout = 6000 -- This timeout is how often the server checks for blacklisted stuff! (1000 = 1s) (Lower = lag)
+LXC.AutomaticDeleteTimeout = 10000 -- This timeout is how often the server checks for blacklisted stuff! (1000 = 1s) (Lower = lag)
 
 -- For string models use "" example : "cargoplane","adder","lol"
 -- For Hash models DON'T USE "" example : -12345,-98712,13371337
