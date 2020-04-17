bdf4a02babe048780ef07fb9b86d3aa6f7680c30
minipunch <joshyolles@gmail.com>
2020-04-16 19:52:24 -0700

    adjust file names, remove old prison spawning stuff

--- resources/usa_prison_new_map/client.lua
+++
@@ -1,9 +0,0 @@
-Citizen.CreateThread(function()
-    while true do
-        local myCoords = GetEntityCoords(PlayerPedId())
-        if GetDistanceBetweenCoords(myCoords, 1787.91,2590.53,45.8, true ) < 200 then
-            ClearAreaOfPeds(1787.91,2590.53,45.8, 200.0, 0)
-        end
-        Wait(1)
-    end
-end)
