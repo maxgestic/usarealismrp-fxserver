386592830c456e799c5496a242a49e5db1e5ffd4
minipunch <joshyolles@gmail.com>
2020-04-26 17:39:10 -0700

    remove extra thread

--- resources/usa_fishing/cl_anchor.lua
+++ resources/usa_fishing/cl_anchor.lua
@@ -1,4 +1,5 @@
 local ANIMATION_TIME_SECONDS = 10
+local anchored = 
 
 Citizen.CreateThread(function()
     while true do
@@ -9,8 +10,8 @@ Citizen.CreateThread(function()
         local vehicleClass = GetVehicleClass(fishingBoat)
         local speedCheck = GetEntitySpeed(fishingBoat)
 
-        if IsPedSittingInAnyVehicle( ped ) and IsEntityInWater(fishingBoat) and vehicleClass == 14  then
-            if IsControlPressed(0, 187) then -- down arrow
+        if IsControlPressed(0, 187) and  then -- down arrow
+            if IsPedSittingInAnyVehicle(ped) and IsEntityInWater(fishingBoat) and vehicleClass == 14  then
                 if speedCheck < 1 then
                     while GetGameTimer() - beginTime < ANIMATION_TIME_SECONDS * 1000 do
                         DrawTimer(beginTime, ANIMATION_TIME_SECONDS * 1000, 1.42, 1.475, 'Lowering Anchor')
@@ -20,6 +21,17 @@ Citizen.CreateThread(function()
                 else
                     exports.globals:notify('You cannot lower the anchor whilst moving!')
                 end
+            end
+        end
+
+       
+        if IsControlPressed(0, 188) then -- up arrow
+            if IsPedSittingInAnyVehicle(ped) and vehicleClass == 14  then -- unable to check if in water as entity is frozen
+                while GetGameTimer() - beginTime < ANIMATION_TIME_SECONDS * 1000 do
+                    DrawTimer(beginTime, ANIMATION_TIME_SECONDS * 1000, 1.42, 1.475, 'Raising Anchor')
+                    Wait(0)
+                end
+                FreezeEntityPosition(fishingBoat, false)
             end
         end
     end
