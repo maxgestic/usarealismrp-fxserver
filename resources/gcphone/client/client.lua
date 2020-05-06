96b16674a6d123e73bf28de5d8a0f59b581f1a2c
minipunch <joshyolles@gmail.com>
2020-05-06 00:04:00 -0700

    use other ringtone for real this time

--- resources/gcphone/client/client.lua
+++ resources/gcphone/client/client.lua
@@ -199,7 +199,7 @@ Citizen.CreateThread(function ()
               PhonePlayCall(true)
               TakeAppel(PhoneInCall[i])
               PhoneInCall = {}
-              StopSoundJS('ring.ogg')
+              StopSoundJS('ring2.ogg')
             end
           end
           break
@@ -209,17 +209,17 @@ Citizen.CreateThread(function ()
       showFixePhoneHelper(coords)
     end
     if inRangeToActivePhone == true and currentPlaySound == false then
-      PlaySoundJS('ring.ogg', 0.2 + (inRangedist - soundDistanceMax) / -soundDistanceMax * 0.8 )
+      PlaySoundJS('ring2.ogg', 0.2 + (inRangedist - soundDistanceMax) / -soundDistanceMax * 0.8 )
       currentPlaySound = true
     elseif inRangeToActivePhone == true then
       mod = mod + 1
       if (mod == 15) then
         mod = 0
-        SetSoundVolumeJS('ring.ogg', 0.2 + (inRangedist - soundDistanceMax) / -soundDistanceMax * 0.8 )
+        SetSoundVolumeJS('ring2.ogg', 0.2 + (inRangedist - soundDistanceMax) / -soundDistanceMax * 0.8 )
       end
     elseif inRangeToActivePhone == false and currentPlaySound == true then
       currentPlaySound = false
-      StopSoundJS('ring.ogg')
+      StopSoundJS('ring2.ogg')
     end
     Citizen.Wait(0)
   end
