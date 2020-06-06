376b3689ff41ebf2dccaf638a5cd2fa1d0b59e4e
Goryholes <aaronjake0191@gmail.com>
2020-06-06 23:30:50 +0100

    Remove prints

--- resources/usa_harvest-process-sell/sv_harvest-process-sell.lua
+++ resources/usa_harvest-process-sell/sv_harvest-process-sell.lua
@@ -32,10 +32,8 @@ RegisterServerEvent("HPS:checkCriminalHistory")
 AddEventHandler("HPS:checkCriminalHistory", function(job_name, process_time, stage)
   local char = exports["usa-characters"]:GetCharacter(source)
   local criminal_history = char.get("criminalHistory")
-  print(#criminal_history)
   if #criminal_history > 0 then
     for i = 1, #criminal_history do
-      print(criminal_history[i].charges)
       if hasCriminalRecord(criminal_history[i].charges) then
         TriggerClientEvent("usa:notify", source, "Due to your serious criminal background, we cannot hire you to work in our sandpit!")
         return
@@ -84,14 +82,13 @@ function hasCriminalRecord(charges)
   local numbers = {
     '187', '192', '206', '207', '211', '245', '459', '600', '646.9', '16590', '18720', '29800', '30605', '33410', '2331', '2800.2', '2800.3', '2800.4', '51-50', '5150'
   }
-
-  for _, code in pairs(numbers) do
-    if charges then
+  if charges then
+    for _, code in pairs(numbers) do
       if string.find(charges, code) then
         return true
       end
+      return false
     end
-    return false
   end
 end
 
