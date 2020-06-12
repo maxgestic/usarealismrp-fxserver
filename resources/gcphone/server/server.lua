191749d4c5047d6d16d45a4b8b05470163c5c3e3
minipunch <joshyolles@gmail.com>
2020-06-12 00:51:02 -0700

    gcphone: fix unread messages issue

--- resources/gcphone/server/server.lua
+++ resources/gcphone/server/server.lua
@@ -345,14 +345,36 @@ end
 
 function setReadMessageNumber(identifier, num)
     getNumberPhone(identifier, function(mePhoneNumber)
-        local query = {
-            ["receiver"] = mePhoneNumber,
-            ["transmitter"] = num
-        }
+        local endpoint = "/phone-messages/_design/messageFilters/_view/getReceivedMessagesByNum"
+        local url = "http://" .. exports["essentialmode"]:getIP() .. ":" .. exports["essentialmode"]:getPort() .. endpoint
+        PerformHttpRequest(url, function(err, responseText, headers)
+            if responseText then
+                local data = json.decode(responseText)
+                if data.rows then
+                    local msgs = arrayifyDBDocsResponse(data.rows)
+                    for i = 1, #msgs do
+                        if msgs[i].transmitter == num then -- only set messages as read for the person the user clicked on
+                            if msgs[i].isRead == 0 then
+                                msgs[i]._rev = nil -- prevent document conflict
+                                msgs[i].isRead = 1
+                                db.updateDocument("phone-messages", msgs[i]._id, msgs[i], function(doc, err, rText) end)
+                            end
+                        end
+                    end
+                end
+            end
+        end, "POST", json.encode({
+            keys = { mePhoneNumber }
+        }), { ["Content-Type"] = 'application/json', ['Authorization'] = "Basic " .. exports["essentialmode"]:getAuth() })
+        --[[
+        -- ! not working for some reason, returning code "0" ??
         db.getDocumentsByRows("phone-messages", query, function(docs)
+            print("docs: " .. type(docs))
             if docs then
+                print("docs existed")
                 for i = 1, #docs do
                     if docs[i].isRead == 0 then
+                        print("was not marked as read, setting message with id " .. docs[i]._id .. " as read")
                         docs[i]._rev = nil
                         docs[i].isRead = 1
                         db.updateDocument("phone-messages", docs[i]._id, docs[i], function(doc, err, rText)
@@ -363,6 +385,7 @@ function setReadMessageNumber(identifier, num)
                 end
             end
         end)
+        --]]
     end)
 end
 
