00f29efeba298de91285885f88000a50f48a7382
minipunch <joshyolles@gmail.com>
2020-07-11 20:19:27 -0700

    add server

---
+++ resources/ws_server/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  //-- [REQUIRED] IPv4 Address of your teamspeak 3 server
+  TSServer: "147.135.104.174",
+
+  //-- [REQUIRED] Port of the ws_server
+  WSServerPort: 3000,
+
+  //-- [OPTIONAL] IPv4 Address of the ws_server
+  //-- Set by autoconfig
+  // WSServerIP: "127.0.0.1",
+
+  //-- [OPTIONAL] IPv4 Adress of your FiveM server
+  //-- Set by autoconfig if you run ws_server as FXServer resource or standalone on the same machine
+  // FivemServerIP: "127.0.0.1",
+
+  //-- [OPTIONAL] Port of your FiveM Server
+  //-- Set by autoconfig if you run ws_server as FXServer resource
+  // FivemServerPort: 32000,
+};
