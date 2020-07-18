a72604283eea19d4014dbfb857724aae5922608a
minipunch <joshyolles@gmail.com>
2020-07-17 20:30:12 -0700

    UC police lights

--- resources/usa_policelights/sv_policelights.lua
+++ resources/usa_policelights/sv_policelights.lua
@@ -8,8 +8,26 @@ AddEventHandler('policelights:requestHeadlightColor', function(netVeh, value)
 	TriggerClientEvent('policelights:setHeadlightColor', -1, netVeh, value)
 end)
 
-TriggerEvent('es:addJobCommand', 'uclights', {'sheriff', 'police'}, function(source, args, user)
-	TriggerClientEvent('policelights:enableLightsOnVehicle', source)
+TriggerEvent('es:addJobCommand', 'uclights', {'sheriff', 'corrections'}, function(src, args, char)
+	local job = char.get("job")
+	if job == "sheriff" then -- SASP
+		print("police rank: " .. char.get("policeRank"))
+		if char.get("policeRank") >= 4 then
+			TriggerClientEvent('policelights:enableLightsOnVehicle', src)
+		else 
+			TriggerClientEvent("usa:notify", src, "Must be rank 4+")
+		end
+	elseif job == "corrections" then -- BCSO
+		exports.usa_prison:getBCSORank(src, function(rank)
+			if rank then 
+				if rank >= 6 then
+					TriggerClientEvent('policelights:enableLightsOnVehicle', src)
+				else
+					TriggerClientEvent("usa:notify", src, "Must be rank 6+")
+				end
+			end
+		end)
+	end
 end, {
 	help = "Enable undercover lights on non-police vehicles",
 })
