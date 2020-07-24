00770307112495dea2e604b116803ea2921b8342
minipunch <joshyolles@gmail.com>
2020-07-24 15:01:07 -0700

    adjust .gitignore, commit handling/interiorproxies meta files

---
+++ resources/assets/__resource.lua
@@ -0,0 +1,9 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+files {
+	'handling.meta',
+	"interiorproxies.meta"
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
