e787ad87b09b71618410e00635dfe26004c05cea
minipunch <joshyolles@gmail.com>
2020-08-21 10:38:27 -0700

    coord-saver: fix format, added more types

--- resources/coord-saver/client.lua
+++ resources/coord-saver/client.lua
@@ -1,7 +1,21 @@
 
-RegisterCommand('coords', function(source, args, rawCommand)
+RegisterCommand('coords1', function(source, args, rawCommand)
 	local coords = GetEntityCoords(PlayerPedId())
 	SendNUIMessage({
-		coords = "x = "..coords.x..",y = "..coords.y..", z = "..coords.z..""
+		coords = "x = " .. coords.x .. ", y = " .. coords.y ..", z = " .. coords.z
+	})
+end)
+
+RegisterCommand('coords2', function(source, args, rawCommand)
+	local coords = GetEntityCoords(PlayerPedId())
+	SendNUIMessage({
+		coords = "['x'] = " .. coords.x .. ", ['y'] = " .. coords.y ..", ['z'] = " .. coords.z
+	})
+end)
+
+RegisterCommand('coords3', function(source, args, rawCommand)
+	local coords = GetEntityCoords(PlayerPedId())
+	SendNUIMessage({
+		coords = coords.x .. ", " .. coords.y ..", " .. coords.z
 	})
 end)
