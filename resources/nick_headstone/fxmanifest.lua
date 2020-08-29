0d67e6b7a6cc185d89f5fac760dbc639e319923f
minipunch <joshyolles@gmail.com>
2020-08-29 12:29:38 -0700

    map: added custom headstone

---
+++ resources/nick_headstone/fxmanifest.lua
@@ -0,0 +1,8 @@
+description 'PixelWorld Mission Row PD Garage'
+name 'PixelWorld pw-mrpd-garage'
+author 'PixelWorldRP [Ultrunz] - https://pixelworldrp.com'
+
+this_is_a_map 'yes'
+
+fx_version 'adamant'
+games {'gta5'}
