1fd343e64d97126a6c64ef875eadbb5b7500449e
minipunch <joshyolles@gmail.com>
2020-09-07 10:41:50 -0700

    update ws_server

--- resources/ws_server/config.js
+++ resources/ws_server/config.js
@@ -3,20 +3,13 @@ module.exports = {
   TSServer: "147.135.104.174",
 
   //-- [REQUIRED] Port of the ws_server
-	//-- Make sure you open the port you specify below
-  WSServerPort: 3000,
+  //-- Make sure you open the port you specify below
+  //-- Please use a port above 30k as some networks block those below it
+  WSServerPort: 33251,
 
   //-- [OPTIONAL] IPv4 Address of the ws_server
   //-- Set by autoconfig
   // WSServerIP: "127.0.0.1",
-
-  //-- [OPTIONAL] IPv4 Adress of your FiveM server
-  //-- Set by autoconfig if you run ws_server as FXServer resource or standalone on the same machine
-   FivemServerIP: "147.135.104.174",
-
-  //-- [OPTIONAL] Port of your FiveM Server
-  //-- Set by autoconfig if you run ws_server as FXServer resource
-   FivemServerPort: 30120,
 
   //-- [OPTIONAL] Enable connection/disconnection logs
   enableLogs: false
