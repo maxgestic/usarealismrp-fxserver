40060477dfcf5c26eb4f691e195c86c6208c6b68
minipunch <joshyolles@gmail.com>
2020-09-20 20:44:38 -0700

    lockpicks: play animation

--- resources/usa_lockpick/client.lua
+++ resources/usa_lockpick/client.lua
@@ -1,5 +1,11 @@
 local loc = nil
 local bank = nil
+local shouldBePlayingAnim = false
+local dictLoaded = false
+local clearedAnim = false
+
+local LOCKPICK_ANIM_DICT = "anim@amb@clubhouse@tutorial@bkr_tut_ig3@"
+local LOCKPICK_ANIM_NAME = "machinic_loop_mechandplayer"
 
 RegisterNetEvent('lockpick:openlockpick')
 AddEventHandler('lockpick:openlockpick', function(location, bankLoc)
@@ -12,6 +18,7 @@ AddEventHandler('lockpick:openlockpick', function(location, bankLoc)
 	SendNUIMessage({
 		showPlayerMenu = 'open'
 	})
+	shouldBePlayingAnim = true
 	return
 end)
 
@@ -28,6 +35,7 @@ RegisterNUICallback('win', function()
 	SendNUIMessage({
 		showPlayerMenu = 'close'
 	})
+	shouldBePlayingAnim = false
 	if loc then
 		TriggerServerEvent('properties:lockpickSuccessful', loc)
 	else
@@ -41,4 +49,28 @@ AddEventHandler('lockpick:closehtml', function()
 	SendNUIMessage({
 		showPlayerMenu = 'close'
 	})
+	shouldBePlayingAnim = false
+end)
+
+Citizen.CreateThread(function()
+	while true do
+		if shouldBePlayingAnim then
+			if not dictLoaded then
+				exports.globals:loadAnimDict(LOCKPICK_ANIM_DICT)
+				dictLoaded = true
+			end
+			local myped = PlayerPedId()
+			if not IsEntityPlayingAnim(myped, LOCKPICK_ANIM_DICT, LOCKPICK_ANIM_NAME, 3) then
+				TaskPlayAnim(myped, LOCKPICK_ANIM_DICT, LOCKPICK_ANIM_NAME, 2.0, 2.0, -1, 51, 0, false, false, false)
+				clearedAnim = false
+            end
+		else
+			if not clearedAnim then
+				local myped = PlayerPedId()
+				ClearPedTasksImmediately(myped)
+				clearedAnim = true
+			end
+		end
+		Wait(0)
+	end
 end)
