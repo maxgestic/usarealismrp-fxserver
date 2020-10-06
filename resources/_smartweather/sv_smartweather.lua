21555e5b00a846a0dc792d637fba9eb202fe5490
minipunch <joshyolles@gmail.com>
2020-10-05 22:51:53 -0700

    smartweather: remove duplicate return

--- resources/_smartweather/sv_smartweather.lua
+++ resources/_smartweather/sv_smartweather.lua
@@ -102,7 +102,6 @@ currentWeatherData = {
 function getRandomWeatherDuration(weatherName)
 	if weatherName == "RAIN" or weatherName == "THUNDER" then
 		return math.random(15, 40) * 60000
-		return 15000
 	else
 		return math.random(DURATION_LOWER_BOUND_IN_MINUTES, DURATION_UPPER_BOUND_IN_MINUTES) * 60000
 	end
