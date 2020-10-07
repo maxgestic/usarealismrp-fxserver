7d72928f2115fc86d4a5a08eb003d9df05309018
minipunch <joshyolles@gmail.com>
2020-10-06 21:37:29 -0700

    added police scene manager menu (F6 hotkey)

---
+++ resources/scenemenu/__resource.lua
@@ -0,0 +1,17 @@
+-- Resource Metadata
+resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
+
+author 'KJ Studios'
+description 'Classic Scene Menu Script for traffic management.'
+version '3.1.0'
+
+client_scripts {
+    'warmenu.lua',
+    'config.lua',
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua',
+    'versioncheck.lua'
+}
---
+++ resources/scenemenu/server.lua
@@ -0,0 +1,45 @@
+local ALLOWED_JOBS = {
+    sheriff = true,
+    corrections = true,
+    ems = true
+}
+
+RegisterServerEvent('ZoneActivated')
+AddEventHandler('ZoneActivated', function(message, speed, radius, x, y, z)
+    if message and TrafficAlert then
+        TriggerClientEvent('chat:addMessage', -1, { color = { 255, 255, 255 }, multiline = false, args = {"Scene Menu", message} })
+    end
+    TriggerClientEvent('Zone', -1, speed, radius, x, y, z)
+end)
+
+RegisterServerEvent('Disable')
+AddEventHandler('Disable', function(blip)
+    TriggerClientEvent('RemoveBlip', -1)
+end)
+
+RegisterServerEvent('GetData')
+AddEventHandler('GetData', function(mode)
+local identifiers = GetPlayerIdentifiers(source)
+    if mode == "IP" then
+        for k,v in pairs(identifiers) do
+            if string.sub(v, 0, 3) == "ip:" then
+                TriggerClientEvent('ReturnData', source, v)
+            end
+        end
+    else
+        for k,v in pairs(identifiers) do
+            if string.sub(v, 0, 6) == "steam:" then
+                TriggerClientEvent('ReturnData', source, v)
+            end
+        end
+    end
+end)
+
+RegisterServerEvent("scenemenu:checkJobForMenuOpen")
+AddEventHandler("scenemenu:checkJobForMenuOpen", function()
+    local char = exports["usa-characters"]:GetCharacter(source)
+    local job = char.get("job")
+    if ALLOWED_JOBS[job] then
+        TriggerClientEvent("scenemenu:open", source)
+    end
+end)
