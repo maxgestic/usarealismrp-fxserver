cab8a03f27c9ffec12b5295b47c64b9a939e9293
minipunch <joshyolles@gmail.com>
2020-11-05 19:15:02 -0800

    essentialmode: fatty style optimization

--- resources/essentialmode/client/main.lua
+++ resources/essentialmode/client/main.lua
@@ -170,17 +170,6 @@ end)
 
 RegisterNetEvent("es:enablePvp")
 AddEventHandler("es:enablePvp", function()
-	Citizen.CreateThread(function()
-		while true do
-			Citizen.Wait(0)
-			for i = 0,255 do
-				if NetworkIsPlayerConnected(i) then
-					if NetworkIsPlayerConnected(i) and GetPlayerPed(i) ~= nil then
-						SetCanAttackFriendly(GetPlayerPed(i), true, true)
-						NetworkSetFriendlyFireOption(true)
-					end
-				end
-			end
-		end
-	end)
+	NetworkSetFriendlyFireOption(true)
+	SetCanAttackFriendly(PlayerPedId(), true, true)
 end)
