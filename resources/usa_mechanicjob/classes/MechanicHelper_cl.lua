e124b8839d48e00a6ebe6558cafc7f95e1df57b3
minipunch <joshyolles@gmail.com>
2020-11-09 11:27:25 -0800

    mechanic: make engine repairs go from red -> orange, orange -> green

--- resources/usa_mechanicjob/classes/MechanicHelper_cl.lua
+++ resources/usa_mechanicjob/classes/MechanicHelper_cl.lua
@@ -45,28 +45,35 @@ MechanicHelper.getClosestVehicle = function(maxRange)
 end
 
 MechanicHelper.repairVehicle = function(veh, repairCount, cb)
+    local beforeRepairHealth = GetVehicleEngineHealth(veh)
     local success = false
-    SetVehicleDoorOpen(veh, 4, false, false)
-    local me = PlayerPedId()
-    local beginTime = GetGameTimer()
-    while GetGameTimer() - beginTime < MechanicHelper.REPAIR_TIME do
-        exports.globals:DrawTimerBar(beginTime, MechanicHelper.REPAIR_TIME, 1.42, 1.475, 'REPAIRING')
-        if not IsEntityPlayingAnim(me, MechanicHelper.animations.repair.dict, MechanicHelper.animations.repair.name, 3) then
-            RequestAnimDict(MechanicHelper.animations.repair.dict)
-            TaskPlayAnim(me, MechanicHelper.animations.repair.dict, MechanicHelper.animations.repair.name, 8.0, 1.0, -1, 15, 1.0, 0, 0, 0)
+    if beforeRepairHealth < 800 then
+        SetVehicleDoorOpen(veh, 4, false, false)
+        local me = PlayerPedId()
+        local beginTime = GetGameTimer()
+        while GetGameTimer() - beginTime < MechanicHelper.REPAIR_TIME do
+            exports.globals:DrawTimerBar(beginTime, MechanicHelper.REPAIR_TIME, 1.42, 1.475, 'REPAIRING')
+            if not IsEntityPlayingAnim(me, MechanicHelper.animations.repair.dict, MechanicHelper.animations.repair.name, 3) then
+                RequestAnimDict(MechanicHelper.animations.repair.dict)
+                TaskPlayAnim(me, MechanicHelper.animations.repair.dict, MechanicHelper.animations.repair.name, 8.0, 1.0, -1, 15, 1.0, 0, 0, 0)
+            end
+            Wait(1)
         end
-        Wait(1)
+        local failChance = 0.5 - (0.005 * repairCount) -- larger successful repair count = smaller fail chance
+        if math.random() > failChance then
+            if not IsVehicleDriveable(veh, true) then -- damaged and red
+                SetVehicleUndriveable(veh, false)
+                SetVehicleEngineHealth(veh, 500.0)
+            else -- damaged but not red, so prob orange
+                SetVehicleEngineHealth(veh, 800.0)
+            end
+            FixAllTires(veh)
+            success = true
+        end
+        ClearPedTasks(me)
+        Wait(500)
+        SetVehicleDoorShut(veh, 4, false)
     end
-    local failChance = 0.5 - (0.005 * repairCount) -- larger successful repair count = smaller fail chance
-    if math.random() > failChance then
-        SetVehicleUndriveable(veh, false)
-        SetVehicleEngineHealth(veh, 800.0)
-        FixAllTires(veh)
-        success = true
-    end
-    ClearPedTasks(me)
-    Wait(500)
-    SetVehicleDoorShut(veh, 4, false)
     cb(success)
 end
 
