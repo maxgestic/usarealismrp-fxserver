7e336b1b9ba23b27d610040c17b83455c19744e0
minipunch <joshyolles@gmail.com>
2021-01-03 16:45:02 -0800

    radio: restrict freq. 1 - 14 by job

--- resources/rp-radio/server.lua
+++ resources/rp-radio/server.lua
@@ -1,8 +1,8 @@
 
-    --local radioItem = { name = "Radio", price = 350, type = "misc", quantity = 1, legality = "legal", weight = 7, objectModel = "prop_cs_hand_radio", blockedInPrison = true}
+--local radioItem = { name = "Radio", price = 350, type = "misc", quantity = 1, legality = "legal", weight = 7, objectModel = "prop_cs_hand_radio", blockedInPrison = true}
 --exports["usa_stores"]:AddGeneralStoreItem("Misc", radioItem)
 
-local EARPIECE_JOBS = {
+local EMERGENCY_JOBS = {
     sheriff = true,
     ems = true,
     corrections = true
@@ -12,28 +12,17 @@ RegisterServerEvent("rp-radio:checkForRadioItem")
 AddEventHandler("rp-radio:checkForRadioItem", function()
     local char = exports["usa-characters"]:GetCharacter(source)
     local charJob = char.get("job")
-    local civRadio = char.hasItem("Radio")
-    local policeRadio = char.hasItem("Police Radio")
-    local emsRadio = char.hasItem("EMS Radio")
-    if policeRadio or emsRadio then
-        TriggerClientEvent("Radio.Set", source, true, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13})
-        if canJobUseEarpiece(charJob) then
+    local hasAnyRadio = char.hasItem("Radio")
+    if hasAnyRadio then
+        if EMERGENCY_JOBS[charJob] then
+            TriggerClientEvent("Radio.Set", source, true, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13})
             TriggerClientEvent("rp-radio:toggleEarpiece", source, true)
         else
+            TriggerClientEvent("Radio.Set", source, true, {})
             TriggerClientEvent("rp-radio:toggleEarpiece", source, false)
         end
-    elseif civRadio then
-        TriggerClientEvent("Radio.Set", source, true, {})
-    else
+    else 
         TriggerClientEvent("Radio.Set", source, false, {})
         TriggerClientEvent("usa:notify", source, "You have no radio!")
     end
 end)
-
-function canJobUseEarpiece(job)
-    if EARPIECE_JOBS[job] then
-        return true
-    else
-        return false
-    end
-end
