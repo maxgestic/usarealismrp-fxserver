6444a3df04bcf5e182b6e580d93e557b27b1be1c
minipunch <joshyolles@gmail.com>
2021-01-28 19:07:00 -0800

    properties: fix spawn bug

--- resources/usa-characters/cl_characters.lua
+++ resources/usa-characters/cl_characters.lua
@@ -3,5 +3,12 @@ AddEventHandler("character:setCoords", function(coords)
     if coords.x and coords.y and coords.z then
         local myped = PlayerPedId()
         SetEntityCoords(myped, coords.x, coords.y, coords.z, 0, 0, 0, 0)
+        RequestCollisionAtCoord(coords.x, coords.y, coords.z)
+        Wait(10)
+        SetEntityCoordsNoOffset(myped, coords.x, coords.y, coords.z, false, false, false, true)
+        while not HasCollisionLoadedAroundEntity(myped) do
+            Wait(100)
+            SetEntityCoords(myped, coords.x, coords.y, coords.z, 1, 0, 0, 1)
+        end
     end
 end)
