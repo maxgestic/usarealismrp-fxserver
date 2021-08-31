601151c81e8b99adbe1e3085ffa308e2454f0ddb
minipunch <joshyolles@gmail.com>
2021-08-30 22:13:27 -0700

    vehshop: fix impala spelling

--- resources/usa_carshop/vehicle-shop_sv.lua
+++ resources/usa_carshop/vehicle-shop_sv.lua
@@ -374,7 +374,7 @@ local vehicleShopItems = {
 			{make = "Chevrolet", model = "Camaro (1969)", price = 70000, hash = "camaro69", storage_capacity = 180.0},
 			{make = "Chevrolet", model = "Camaro SS (1970)", price = 80000, hash = "camaro70", storage_capacity = 180.0},
 			{make = "Volkswagon", model = "Golf GTI", price = 90000, hash = "golfgti", storage_capacity = 175.0},
-			{make = "Chevrolet", model = "Imapala (1959)", price = 70000, hash = "impala59c", storage_capacity = 250.0},
+			{make = "Chevrolet", model = "Impala (1959)", price = 70000, hash = "impala59c", storage_capacity = 250.0},
 			{make = "Volkswagon", model = "Golf (MK3)", price = 60000, hash = "mk3", storage_capacity = 200.0},
 			{make = "Porsche", model = "911 (1973)", price = 60000, hash = "porrs73", storage_capacity = 165.0},
 			{make = "Volkswagon", model = "Type 2 (1962)", price = 50000, hash = "type262", storage_capacity = 165.0},
