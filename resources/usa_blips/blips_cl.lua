497b87b60b00bec586782502be23913e474f0018
Rezitoo <85491174+Rezitoo@users.noreply.github.com>
2021-12-27 13:45:25 -0600

    Added Garage for LS Fight Club (#634)
    * Added Garage for LS Fight Club
    
    * Removed the Blip for the garage
    
    * also added blip
    
    Co-authored-by: minipunch <joshyolles@gmail.com>

--- resources/usa_blips/blips_cl.lua
+++ resources/usa_blips/blips_cl.lua
@@ -95,6 +95,7 @@ local blips = {
 	{ title = "Best Buds", colour = 2, id = 140, x = 377.75665283203, y = -829.908203125, z = 29.302627563477, scale = 0.6 },
 	{ title = "LS Car Meet", x = 784.30633544922, y = -1868.2249755859, id = 38, colour = 62, scale = 0.8},
 	{ title = "Untamed Autos", x = 1149.8494, y =  -781.1504, id = 402, colour = 5, scale = 0.8},
+	{ title = "Fight Club", x = 1059.5687255859, y = -2409.5798339844, id = 210, colour = 0, scale = 0.4},
 	{ title = "RPG", x = -795.98742675781, y = -219.36700439453, id = 402, colour = 5, scale = 0.8},
 }
 
