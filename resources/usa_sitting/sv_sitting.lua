d54d31756165be4ddc91ba89109977b6889f4eb0
minipunch <joshyolles@gmail.com>
2022-01-16 14:34:04 -0800

    added blacklisted /sit locations

--- resources/usa_sitting/sv_sitting.lua
+++ resources/usa_sitting/sv_sitting.lua
@@ -1,4 +1,28 @@
-TriggerEvent('es:addCommand', 'sit', function(source, args, char)
+local BLACKLISTED_SIT_LOCATIONS = {
+	{
+		coords = vector3(4982.228515625, -5710.6870117188, 19.886962890625),
+		distance = 100
+	},
+	{
+		coords = vector3(257.16165161133, 220.75233459473, 106.28523254395),
+		distance = 100
+	}
+}
+
+local function isAtBlacklistedLocation(coords)
+	for i = 1, #BLACKLISTED_SIT_LOCATIONS do
+		if #(coords - BLACKLISTED_SIT_LOCATIONS[i].coords) < BLACKLISTED_SIT_LOCATIONS[i].distance then
+			return true
+		end
+	end
+	return false
+end
+
+TriggerEvent('es:addCommand', 'sit', function(src, args, char)
 	if char.get("jailTime") > 0 then return end
-	TriggerClientEvent('sit:sitOnNearest', source)
+	if isAtBlacklistedLocation(GetEntityCoords(GetPlayerPed(src))) then
+		TriggerClientEvent("usa:notify", src, "Can't sit here")
+		return
+	end
+	TriggerClientEvent('sit:sitOnNearest', src)
 end, { help = "Sit on the nearest chair, seat, or bench" })
