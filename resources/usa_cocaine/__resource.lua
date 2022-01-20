f5374e231199963fc62a2e3a9f4fcc962c59db78
minipunch <joshyolles@gmail.com>
2022-01-19 17:16:18 -0800

    mod mod patch #2

--- resources/usa_cocaine/__resource.lua
+++ resources/usa_cocaine/__resource.lua
@@ -1,4 +1,11 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-client_script 'cl_coke.lua'
-server_script 'sv_coke.lua'
+client_scripts {
+    'cl_coke.lua',
+    '@salty_tokenizer/init.lua'
+}
+
+server_scripts {
+    'sv_coke.lua',
+    '@salty_tokenizer/init.lua'
+}
