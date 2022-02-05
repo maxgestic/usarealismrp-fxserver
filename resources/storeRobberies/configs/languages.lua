dc27c5f3a53488787a39d7945da51307dedbea47
Josh Yolles <joshyolles@gmail.com>
2022-02-04 17:05:18 -0800

    Updated store robberies (#701)
    * ensure storeRobberies
    
    * initial commit after converting server functions to our framework and it seems to work decently
    
    * made sure lock picking registers works
    
    * disable old robbery script for conflicting stores and remove old peds
    
    * longer ped register opening
    
    * change key to F
    
    * adjust cops needed
    
    * update help text

---
+++ resources/storeRobberies/configs/languages.lua
@@ -0,0 +1,22 @@
+Languages = {}
+
+Languages = {
+    ['en'] = {
+
+        --Robbing--
+        ['robbingInitialMessage'] = "What do you want?",
+        ['robbingInitialMessage-noSurv'] = "[F] Empty the register",
+        ['robbingInitialMessage-Surv'] = "[F] Empty the register - [H] Disable store cameras",
+        ['secondaryMessage'] = "Okay, okay. Wait.",
+        ['disablingCameras']  = "Disabling cameras",
+        ['unlockRegister']  = "Unlocking register...",
+        ['crackRegister']  = "[F] Lockpick the register",
+        ['crackSafe']  = "[F] Crack the safe",
+        ['safecode']  = "The code for the safe is:",
+        ['safeCrack'] = "Press E left | F right | H to crack",
+        ['robbed'] = "Mate, I was robbed a while ago. I don't have any money for ya.",
+        ['safeOpened'] = "Already robbed",
+        ['registerOpened'] = "Already robbed",
+        ['no_cops'] = "You're messing with the wrong person, pal!",
+    }
+}
