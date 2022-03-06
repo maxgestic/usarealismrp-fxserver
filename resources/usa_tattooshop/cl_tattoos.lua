76b12e05f905f5138281107ea4339d48d4905b01
minipunch <joshyolles@gmail.com>
2022-03-06 11:54:46 -0800

    added Chamberlain Hills Recreational Center interior

--- resources/usa_tattooshop/cl_tattoos.lua
+++ resources/usa_tattooshop/cl_tattoos.lua
@@ -4,7 +4,8 @@ local SHOPS = {
   {x = 323.947, y = 179.87, z = 103.586}, -- los santos, vinewood area
   {x = 1321.550, y = -1653.529, z = 52.27}, -- los santos, east side
   {x = -1155.323, y = -1426.81, z = 4.9}, -- los santos, west side vespucci beach
-  {x = -3169.1, y = 1076.8, z = 20.8} -- ls, west coast
+  {x = -3169.1, y = 1076.8, z = 20.8}, -- ls, west coast
+  {x = -255.64067077637, y = -1530.5904541016, z = 32.483879089355} -- chamberlain hills community center (addon MLO)
 }
 
 local TATTOOS = nil
