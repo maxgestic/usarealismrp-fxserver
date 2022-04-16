4f0fe9277d9acba1f470e7aaaf581d26cf696a09
minipunch <joshyolles@gmail.com>
2022-04-15 22:01:19 -0700

    tokenize event

--- resources/_anticheese/__resource.lua
+++ resources/_anticheese/__resource.lua
@@ -2,11 +2,13 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 server_scripts {
 	"sv_anticheese.lua",
-	"sv_anti-modder.lua"
+	"sv_anti-modder.lua",
+	'@salty_tokenizer/init.lua'
 }
 
 client_scripts {
-	"cl_anticheese.lua"
+	"cl_anticheese.lua",
+	'@salty_tokenizer/init.lua'
 }
 
 exports {
