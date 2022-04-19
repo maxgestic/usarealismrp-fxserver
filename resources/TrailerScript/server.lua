db829046da5f6d467053f1dccace3698c6a02a30
Urbaniized <urbanizedkiller@live.com>
2022-04-18 20:15:30 -0700

    Deleted Trailerscript

--- resources/TrailerScript/server.lua
+++
@@ -1,5 +0,0 @@
-TriggerEvent('es:addCommand', 'trailer', function(source, args, char)
-    TriggerClientEvent("trailer:toggle", source)
-end, {
-    help = "Attach or detach a vehicle from a trailer",
-})
