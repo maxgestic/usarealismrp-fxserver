0f72f619e5e96d3ddf0881d700ff4743eaa2d847
minipunch <joshyolles@gmail.com>
2022-04-20 00:01:26 -0700

    append last commit

--- resources/usa_rp2/client/cl_weaponDamageModifier.lua
+++ resources/usa_rp2/client/cl_weaponDamageModifier.lua
@@ -62,8 +62,8 @@ Citizen.CreateThread(function()
       local currentGlobalModifier = GetPlayerWeaponDamageModifier(PlayerId())
       currentGlobalModifier = tonumber(string.format("%.3f", currentGlobalModifier))
       if currentGlobalModifier ~= whatItShouldBeModifier then
+        SetPlayerWeaponDamageModifier(PlayerId(), whatItShouldBeModifier)
         if GetSelectedPedWeapon(PlayerPedId()) ~= `WEAPON_UNARMED` then
-          SetPlayerWeaponDamageModifier(PlayerId(), whatItShouldBeModifier)
           TriggerServerEvent("usa:notifyStaff", "ANTICHEAT: Player with ID #" .. GetPlayerServerId(PlayerId()) .. " has modified their weapon damage! It was reset to what it should be.")
         end
       end
