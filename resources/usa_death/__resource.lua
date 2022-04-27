aa0e1644669e62c58b84ccdf90d19a57361c4b34
minipunch <joshyolles@gmail.com>
2022-04-26 18:30:28 -0700

    added /whodownedme command

--- resources/usa_death/__resource.lua
+++ resources/usa_death/__resource.lua
@@ -1,4 +1,8 @@
 description 'Prevent Deaths until /respawn or /revive for RP purposes'
 
+shared_scripts {
+    "@pmc-callbacks/import.lua"
+}
+
 client_script "client.lua"
 server_script "server.lua"
