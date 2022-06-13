34dc3a0e8e890a64d0cb140e13e42c93de87cbb6
Chang <77425784+ChangyBoi@users.noreply.github.com>
2022-06-13 22:20:26 +0200

    potential fix for random weapon camo (#972)

--- resources/gunmetas/__resource.lua
+++ resources/gunmetas/__resource.lua
@@ -2,12 +2,7 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 files {
-	'weaponcomponents.meta',
-	'weaponanimations.meta',
-	'weapons.meta'
+	'weaponanimations.meta'
 }
 
-	data_file 'WEAPONCOMPONENTSINFO_FILE' 'weaponcomponents.meta'
 	data_file 'WEAPON_ANIMATIONS_FILE' 'weaponanimations.meta'
-	data_file 'WEAPONINFO_FILE' 'weapons.meta'
-	data_file 'WEAPONINFO_FILE_PATCH' 'weapons.meta'
