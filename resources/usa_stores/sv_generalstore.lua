4ca2c15c61a92004f1f1f836e72fcff2b1d363d3
Chang <77425784+ChangyBoi@users.noreply.github.com>
2022-08-06 02:39:15 +0200

    Removed beer pong from prison (#1103)

--- resources/usa_stores/sv_generalstore.lua
+++ resources/usa_stores/sv_generalstore.lua
@@ -75,7 +75,7 @@ local GENERAL_STORE_ITEMS = {
     { name = "RAW Papers", price = 10, type = "misc", quantity = 5, legality = "legal", weight = 1, objectModel = "prop_cs_pills", blockedInPrison = true},
     { name = "Bic Lighter", price = 10, type = "misc", quantity = 1, legality = "legal", weight = 1, objectModel = "prop_cs_pills", blockedInPrison = true},
     { name = "Sign Kit", price = 200, type = "misc", quantity = 1, legality = "legal", weight = 5, objectModel = "prop_cd_paper_pile1"},
-    { name = "Beer Pong Kit", price = 300, type = "misc", quantity = 1, legality = "legal", weight = 5, objectModel = "apa_prop_cs_plastic_cup_01"}
+    { name = "Beer Pong Kit", price = 300, type = "misc", quantity = 1, legality = "legal", weight = 5, objectModel = "apa_prop_cs_plastic_cup_01", blockedInPrison = true}
   }
 }
 
