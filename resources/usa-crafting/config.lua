cc018a92a2d92421f9ecd9bb10cda1b4d836c5f8
minipunch <joshyolles@gmail.com>
2022-09-21 11:05:14 -0700

    adjust crafting fail chance

--- resources/usa-crafting/config.lua
+++ resources/usa-crafting/config.lua
@@ -435,7 +435,7 @@ Config.Keys = {
 
 Config.DEFAULT_CRAFT_DURATION_SECONDS = 10 * 60
 
-Config.MAX_FAIL_CHANCE = 0.72 -- max (and initial) chance to fail when crafting
+Config.MAX_FAIL_CHANCE = 0.60 -- max (and initial) chance to fail when crafting
 
 Config.FAILURE_COEFFICIENT = 0.0058 -- the higher the number, the less chance of failing when crafting (should be < MAX_FAIL_CHANCE)
 
