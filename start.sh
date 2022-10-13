2a84b243a32618cd396115f820462fb9a3fcc9a1
minipunch <joshyolles@gmail.com>
2022-10-13 08:18:58 -0700

    update start.sh

--- start.sh
+++ start.sh
@@ -14,24 +14,11 @@ git pull --recurse-submodules
 git submodule init
 git submodule update
 
-# avoid scrambling node_modules within resources
-#cp -r resources/[system]/[builders] .
-#cp -r resources/ws_server/node_modules .
-#rm -f -r resources/[system]/[builders]
-#rm -f -r resources/ws_server/node_modules
-
-# scramble resources
-#java -jar ResourceEventScrambler.jar
-
-# move node_modules back into place
-#mv [builders] resources/[system]
-#mv node_modules resources/ws_server
-
 # save last log file, create new one
-mv CitizenFX.log CitizenFX.log.2
+#mv CitizenFX.log CitizenFX.log.2
 
 # clear chat log file
-> /var/www/html/log.txt
+#> /var/www/html/log.txt
 
 # run server
-tmux new -d -s fxserver "bash $pathToServerFiles/run.sh +set onesync on > CitizenFX.log"
+tmux new -d -s fxserver "bash $pathToServerFiles/run.sh +set onesync on"
