bd15adc8ae302848ba03a4e1cb93b54b474fada2
minipunch <joshyolles@gmail.com>
2022-12-23 14:24:06 -0800

    update update.sh

--- update.sh
+++ update.sh
@@ -5,3 +5,9 @@ git pull --recurse-submodules
 # init submodules if ncessary
 git submodule init
 git submodule update
+
+# idk why we need to do this manually every time or if we really need to do but i guess for now whatever if it works
+cd resources/[custom_maps]
+git checkout master
+git pull
+cd ../../
