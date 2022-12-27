4973ba7bf87c530a054089e6ef70dc3236a3fb6d
Savage912 <55938321+Savage912-Official@users.noreply.github.com>
2022-12-26 19:35:55 -0500

    feat(Main): Ai Population Controller (#1592)
    Adjust the density of Ai Population around San Andreas!
    
    Co-authored-by: minipunch <joshyolles@gmail.com>

---
+++ resources/usa_rp2/client/cl_aipopulation..lua
@@ -0,0 +1,20 @@
+AiPopulation = {}
+
+AiPopulation.Main = {
+    VehicleDensity = 0.5,
+    PedDensity = 0.5,
+    RandomVehicleDensity = 0.5,
+    ParkedVehicleDensity = 0.5,
+    ScenarioPedDensity = 0.5
+}
+
+Citizen.CreateThread(function()
+	while true do
+	    Citizen.Wait(0)
+	    SetVehicleDensityMultiplierThisFrame(AiPopulation.Main.VehicleDensity)
+	    SetPedDensityMultiplierThisFrame(AiPopulation.Main.PedDensity)
+	    SetRandomVehicleDensityMultiplierThisFrame(AiPopulation.Main.RandomVehicleDensity)
+	    SetParkedVehicleDensityMultiplierThisFrame(AiPopulation.Main.ParkedVehicleDensity)
+	    SetScenarioPedDensityMultiplierThisFrame(AiPopulation.Main.ScenarioPedDensity)
+	end
+end)
