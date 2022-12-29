0c29438a82a653e265773d2f47db52e4b12a3d1d
minipunch <joshyolles@gmail.com>
2022-12-28 18:33:29 -0800

    tweak ped multiplier

--- resources/usa_rp2/client/cl_aipopulation..lua
+++ resources/usa_rp2/client/cl_aipopulation..lua
@@ -2,7 +2,7 @@ AiPopulation = {}
 
 AiPopulation.Main = {
     VehicleDensity = 0.5,
-    PedDensity = 0.5,
+    PedDensity = 0.8,
     RandomVehicleDensity = 0.5,
     ParkedVehicleDensity = 0.5,
     ScenarioPedDensity = 0.5
