af25699b2c2039c9b6b136a8c1c4d1a776d78fde
minipunch <joshyolles@gmail.com>
2022-12-29 11:04:48 -0800

    added NY rooftop mlo

--- resources/usa_garage/client.lua
+++ resources/usa_garage/client.lua
@@ -80,7 +80,8 @@ locations = {
     {x = 234.1309, y = -2492.6128, z = 6.5814, noBlip = true}, -- Train Clock in
 	{x = -935.0384, y = -2316.2700, z =  6.7091, noBlip = true}, -- metro Clock in
 	{x = -2315.1484375, y = 283.1760559082, z = 169.46704101563, noBlip = true}, -- plasma kart 
-	{x = 897.21588134766, y = -1019.8331298828, z = 34.966979980469, noBlip = true} -- Auto Repair Place
+	{x = 897.21588134766, y = -1019.8331298828, z = 34.966979980469, noBlip = true}, -- Auto Repair Place
+	{x = -1669.1839599609, y = -541.4306640625, z = 35.10620880127, noBlip = true}
 }
 
 local VEHICLE_DAMAGES = {}
