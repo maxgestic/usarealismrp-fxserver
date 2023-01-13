cac4095c0164a0ff609e7797657d0f4f2b1c4f41
Savage912 <55938321+Savage912-Official@users.noreply.github.com>
2023-01-13 15:32:35 -0500

    fix: Animals not spawning (#1690)

--- resources/usa_rp2/client/cl_aipopulation..lua
+++ resources/usa_rp2/client/cl_aipopulation..lua
@@ -2,19 +2,19 @@ AiPopulation = {}
 
 AiPopulation.Main = {
     VehicleDensity = 0.5,
-    PedDensity = 1.0,
+    --PedDensity = 1.0,
     RandomVehicleDensity = 0.5,
     ParkedVehicleDensity = 0.5,
-    ScenarioPedDensity = 1.0
+    --ScenarioPedDensity = 1.0
 }
 
 Citizen.CreateThread(function()
 	while true do
 	    Citizen.Wait(0)
 	    SetVehicleDensityMultiplierThisFrame(AiPopulation.Main.VehicleDensity)
-	    SetPedDensityMultiplierThisFrame(AiPopulation.Main.PedDensity)
+	    --SetPedDensityMultiplierThisFrame(AiPopulation.Main.PedDensity)
 	    SetRandomVehicleDensityMultiplierThisFrame(AiPopulation.Main.RandomVehicleDensity)
 	    SetParkedVehicleDensityMultiplierThisFrame(AiPopulation.Main.ParkedVehicleDensity)
-	    SetScenarioPedDensityMultiplierThisFrame(AiPopulation.Main.ScenarioPedDensity)
+	    --SetScenarioPedDensityMultiplierThisFrame(AiPopulation.Main.ScenarioPedDensity)
 	end
 end)
