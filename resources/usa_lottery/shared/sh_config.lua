111b63c3b31593306b232a18862f97ccc1319ae8
minipunch <joshyolles@gmail.com>
2023-02-17 16:43:48 -0800

    perhaps fix lotto auto update

--- resources/usa_lottery/shared/sh_config.lua
+++ resources/usa_lottery/shared/sh_config.lua
@@ -8,4 +8,4 @@ Config.TicketPrice = 2000 -- Total amount the tickets cost.
 
 Config.TargetLocation = vector3(-1083.13, -245.57, 37.76) -- This is where the target location is.
 
-Config.Reset = '4:00' -- When the resource adds a day/chooses a winner. The time you set will be relative to your Server Box. Make sure this is shortly after a restart.
+Config.Reset = '04:00' -- When the resource adds a day/chooses a winner. The time you set will be relative to your Server Box. Make sure this is shortly after a restart.
