e3320da34f2b68408f40c583b5fe6798b0192f2d
maxgestic <3323241+maxgestic@users.noreply.github.com>
2023-03-01 19:29:02 +0000

    Added ability for doctor to see pillbox, viceroy and sandy hospital cams

--- resources/_securitycams/server.lua
+++ resources/_securitycams/server.lua
@@ -1,6 +1,16 @@
-TriggerEvent('es:addJobCommand', 'camera', { 'sheriff', 'police' , 'judge', "corrections"}, function(source, args, user)
+TriggerEvent('es:addJobCommand', 'camera', { 'sheriff', 'police' , 'judge', "corrections", "doctor"}, function(source, args, user)
 	local cameraID = args[2]
-	TriggerClientEvent('cameras:activateCamera', source, cameraID)
+	local char = exports["usa-characters"]:GetCharacter(source)
+	local job = char.get("job")
+	if job == "doctor" then
+		if cameraID == "pillbox" or cameraID == "viceroy" or cameraID == "sandyhospital" then
+			TriggerClientEvent('cameras:activateCamera', source, cameraID)
+		else
+			TriggerClientEvent("usa:notify", source, "You do not have access to that camera system!")
+		end
+	else
+		TriggerClientEvent('cameras:activateCamera', source, cameraID)
+	end
 end, {
 	help = "Activate a security camera",
 	params = {
