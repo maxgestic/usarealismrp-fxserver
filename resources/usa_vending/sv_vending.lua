bb7fa48efcbe44ceea2da344ee406791a0623921
minipunch <joshyolles@gmail.com>
2023-04-28 18:06:11 -0700

    vending machine update

--- resources/usa_vending/sv_vending.lua
+++ resources/usa_vending/sv_vending.lua
@@ -1,36 +1,36 @@
 local ITEMS = {
     ["Food"] = {
-        {name = "Peanut Butter Cups", price = 10, type = "food", substance = 10.0, quantity = 1, legality = "legal", weight = 5, objectModel = "ng_proc_food_chips01a"},
-        {name = "Sea Salt & Vinegar Chips", price = 10, type = "food", substance = 7.0, quantity = 1, legality = "legal", weight = 7, objectModel = "ng_proc_food_chips01a"},
-        {name = "Cheetos", price = 10, type = "food", substance = 6.0, quantity = 1, legality = "legal", weight = 7, objectModel = "ng_proc_food_chips01a"},
-        {name = "Flaming Hot Cheetos", price = 12, type = "food", substance = 6.0, quantity = 1, legality = "legal", weight = 7, objectModel = "ng_proc_food_chips01c"},
-        {name = "Funyuns", price = 13, type = "food", substance = 6.0, quantity = 1, legality = "legal", weight = 7, objectModel = "ng_proc_food_chips01b"},
-        {name = "Cool Ranch Doritos", price = 13, type = "food", substance = 6.0, quantity = 1, legality = "legal", weight = 7, objectModel = "ng_proc_food_chips01b"},
-        {name = "Gummy Worms", price = 13, type = "food", substance = 6.0, quantity = 1, legality = "legal", weight = 7, objectModel = "ng_proc_food_chips01b"},
-        {name = "Sour Gummy Worms", price = 13, type = "food", substance = 6.0, quantity = 1, legality = "legal", weight = 7, objectModel = "ng_proc_food_chips01b"}
+        {name = "Peanut Butter Cups", price = 50, type = "food", substance = 10.0, quantity = 1, legality = "legal", weight = 5, objectModel = "ng_proc_food_chips01a"},
+        {name = "Sea Salt & Vinegar Chips", price = 50, type = "food", substance = 7.0, quantity = 1, legality = "legal", weight = 7, objectModel = "ng_proc_food_chips01a"},
+        {name = "Cheetos", price = 50, type = "food", substance = 6.0, quantity = 1, legality = "legal", weight = 7, objectModel = "ng_proc_food_chips01a"},
+        {name = "Flaming Hot Cheetos", price = 50, type = "food", substance = 6.0, quantity = 1, legality = "legal", weight = 7, objectModel = "ng_proc_food_chips01c"},
+        {name = "Funyuns", price = 50, type = "food", substance = 6.0, quantity = 1, legality = "legal", weight = 7, objectModel = "ng_proc_food_chips01b"},
+        {name = "Cool Ranch Doritos", price = 50, type = "food", substance = 6.0, quantity = 1, legality = "legal", weight = 7, objectModel = "ng_proc_food_chips01b"},
+        {name = "Gummy Worms", price = 50, type = "food", substance = 6.0, quantity = 1, legality = "legal", weight = 7, objectModel = "ng_proc_food_chips01b"},
+        {name = "Sour Gummy Worms", price = 50, type = "food", substance = 6.0, quantity = 1, legality = "legal", weight = 7, objectModel = "ng_proc_food_chips01b"}
     },
     ["Water"] = {
-        {name = "Fiji Water", price = 25, type = "drink", substance = 40.0, quantity = 1, legality = "legal", weight = 10, objectModel = "ba_prop_club_water_bottle"},
-        {name = "Value Water", price = 20, type = "drink", substance = 40.0, quantity = 1, legality = "legal", weight = 10, objectModel = "ba_prop_club_water_bottle"},
-        {name = "Smart Water", price = 25, type = "drink", substance = 40.0, quantity = 1, legality = "legal", weight = 10, objectModel = "ba_prop_club_water_bottle"},
-        {name = "Dasani Water", price = 25, type = "drink", substance = 40.0, quantity = 1, legality = "legal", weight = 10, objectModel = "ba_prop_club_water_bottle"}
+        {name = "Fiji Water", price = 75, type = "drink", substance = 40.0, quantity = 1, legality = "legal", weight = 10, objectModel = "ba_prop_club_water_bottle"},
+        {name = "Value Water", price = 75, type = "drink", substance = 40.0, quantity = 1, legality = "legal", weight = 10, objectModel = "ba_prop_club_water_bottle"},
+        {name = "Smart Water", price = 75, type = "drink", substance = 40.0, quantity = 1, legality = "legal", weight = 10, objectModel = "ba_prop_club_water_bottle"},
+        {name = "Dasani Water", price = 75, type = "drink", substance = 40.0, quantity = 1, legality = "legal", weight = 10, objectModel = "ba_prop_club_water_bottle"}
     },
     ["Coffee"] = {
-        {name = "Caramel Iced Coffee", price = 20, type = "drink", substance = 35.0, quantity = 1, legality = "legal", weight = 10, objectModel = "ba_prop_club_water_bottle"},
-        {name = "Mocha Iced Coffee", price = 20, type = "drink", substance = 35.0, quantity = 1, legality = "legal", weight = 10, objectModel = "ba_prop_club_water_bottle"},
-        {name = "Vanilla Iced Coffee", price = 20, type = "drink", substance = 35.0, quantity = 1, legality = "legal", weight = 10, objectModel = "ba_prop_club_water_bottle"},
-        {name = "Original Iced Coffee", price = 20, type = "drink", substance = 35.0, quantity = 1, legality = "legal", weight = 10, objectModel = "ba_prop_club_water_bottle"},
-        {name = "Verana Blend Hot Coffee", price = 20, type = "drink", substance = 35.0, quantity = 1, legality = "legal", weight = 10, objectModel = "ba_prop_club_water_bottle"}
+        {name = "Caramel Iced Coffee", price = 100, type = "drink", substance = 35.0, quantity = 1, legality = "legal", weight = 10, objectModel = "ba_prop_club_water_bottle"},
+        {name = "Mocha Iced Coffee", price = 100, type = "drink", substance = 35.0, quantity = 1, legality = "legal", weight = 10, objectModel = "ba_prop_club_water_bottle"},
+        {name = "Vanilla Iced Coffee", price = 100, type = "drink", substance = 35.0, quantity = 1, legality = "legal", weight = 10, objectModel = "ba_prop_club_water_bottle"},
+        {name = "Original Iced Coffee", price = 100, type = "drink", substance = 35.0, quantity = 1, legality = "legal", weight = 10, objectModel = "ba_prop_club_water_bottle"},
+        {name = "Verana Blend Hot Coffee", price = 100, type = "drink", substance = 35.0, quantity = 1, legality = "legal", weight = 10, objectModel = "ba_prop_club_water_bottle"}
     },
     ["Soda"] = {
-        {name = "Coca Cola", price = 25, type = "drink", substance = 35.0, quantity = 1, legality = "legal", weight = 10, objectModel = "ng_proc_sodacan_01b"},
-        {name = "Diet Cola", price = 25, type = "drink", substance = 35.0, quantity = 1, legality = "legal", weight = 10, objectModel = "ng_proc_sodacan_01b"},
-        {name = "Root Beer", price = 25, type = "drink", substance = 35.0, quantity = 1, legality = "legal", weight = 10, objectModel = "ng_proc_sodacan_01b"},
-        {name = "Peach Fanta", price = 25, type = "drink", substance = 8.0, quantity = 1, legality = "legal", weight = 10, objectModel = "ng_proc_sodacan_01b"},
-        {name = "Pineapple Fanta", price = 25, type = "drink", substance = 35.0, quantity = 1, legality = "legal", weight = 10, objectModel = "ng_proc_sodacan_01b"},
-        {name = "Monster Energy Drink", price = 26, type = "drink", substance = 22.0, quantity = 1, legality = "legal", weight = 10, objectModel = "prop_energy_drink"},
-        {name = "Redbull Energy Drink", price = 26, type = "drink", substance = 22.0, quantity = 1, legality = "legal", weight = 10, objectModel = "prop_energy_drink"},
-        {name = "Rockstar Energy Drink", price = 26, type = "drink", substance = 22.0, quantity = 1, legality = "legal", weight = 10, objectModel = "prop_energy_drink"}
+        {name = "Coca Cola", price = 100, type = "drink", substance = 35.0, quantity = 1, legality = "legal", weight = 10, objectModel = "ng_proc_sodacan_01b"},
+        {name = "Diet Cola", price = 100, type = "drink", substance = 35.0, quantity = 1, legality = "legal", weight = 10, objectModel = "ng_proc_sodacan_01b"},
+        {name = "Root Beer", price = 100, type = "drink", substance = 35.0, quantity = 1, legality = "legal", weight = 10, objectModel = "ng_proc_sodacan_01b"},
+        {name = "Peach Fanta", price = 100, type = "drink", substance = 8.0, quantity = 1, legality = "legal", weight = 10, objectModel = "ng_proc_sodacan_01b"},
+        {name = "Pineapple Fanta", price = 150, type = "drink", substance = 35.0, quantity = 1, legality = "legal", weight = 10, objectModel = "ng_proc_sodacan_01b"},
+        {name = "Monster Energy Drink", price = 150, type = "drink", substance = 22.0, quantity = 1, legality = "legal", weight = 10, objectModel = "prop_energy_drink"},
+        {name = "Redbull Energy Drink", price = 150, type = "drink", substance = 22.0, quantity = 1, legality = "legal", weight = 10, objectModel = "prop_energy_drink"},
+        {name = "Rockstar Energy Drink", price = 150, type = "drink", substance = 22.0, quantity = 1, legality = "legal", weight = 10, objectModel = "prop_energy_drink"}
     }
 }
 
