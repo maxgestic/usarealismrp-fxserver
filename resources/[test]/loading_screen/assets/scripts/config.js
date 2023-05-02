a603d1d01fabd750796f0022b5b1cde179832c1a
Crownapple <49931922+Crownapple@users.noreply.github.com>
2023-05-01 23:42:22 -0400

    New Loading screen (#2057)
    * New Loading screen
    
    * Update Config
    
    * Update
    
    * HTML Update
    
    * adjust text, remove buttons, adjust song
    
    ---------
    
    Co-authored-by: minipunch <joshyolles@gmail.com>

---
+++ resources/[test]/loading_screen/assets/scripts/config.js
@@ -0,0 +1,24 @@
+Config = {}; // Don't touch
+
+Config.ServerIP = "usarrp.gg";
+
+// Social media buttons on the left side
+Config.Socials = [
+    {name: "discord", label: "Discord", description: "Click here to copy the link and join our Discord server!", icon: "assets/media/icons/discord.png", link: "https://discord.gg/usarrp"},
+    {name: "instagram", label: "Instagram", description: "Make sure you follow us on instgram", icon: "assets/media/icons/tiktok.png", link: "https://www.instagram.com/usarealismrp/"},
+    {name: "Website", label: "Website", description: "Click here to copy the link to the website", icon: "assets/media/icons/tebex.png", link: "https://usarrp.gg/forums/"},
+];
+
+Config.HideoverlayKeybind = 112 // JS key code https://keycode.info
+Config.CustomBindText = "F1"; // leave as "" if you don't want the bind text in html to be statically set
+
+
+
+// Categories
+Config.Categories = [
+    {label: "Social Media", default: true},
+    
+];
+
+// Music
+Config.Song = "song.mp3";
